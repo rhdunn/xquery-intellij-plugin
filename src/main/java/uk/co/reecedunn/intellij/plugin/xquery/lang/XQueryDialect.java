9a147e6eafebafb307ec1a4fc39f6a1f3434f5dc
Reece H. Dunn <msclrhd@gmail.com>
2016-07-05 21:23:46 +0100

    Rename and group the dialect strings.

--- src/main/java/uk/co/reecedunn/intellij/plugin/xquery/lang/XQueryDialect.java
+++ src/main/java/uk/co/reecedunn/intellij/plugin/xquery/lang/XQueryDialect.java
@@ -20,14 +20,14 @@ import org.jetbrains.annotations.Nullable;
 import uk.co.reecedunn.intellij.plugin.xquery.XQueryBundle;
 
 public enum XQueryDialect {
-    XQUERY_0_9_MARKLOGIC_3_2("0.9-ml/3.2", XQueryVersion.XQUERY_0_9_MARKLOGIC, XQueryVendor.MARKLOGIC, XQueryBundle.message("xquery.vendor.description.ml/3.2"), "https://docs.marklogic.com/guide/xquery/dialects#id_65735"),
-    XQUERY_1_0_W3C("1.0/W3C", XQueryVersion.XQUERY_1_0, XQueryVendor.W3C, XQueryBundle.message("xquery.variant.description.1.0/W3C"), "https://www.w3.org/TR/2007/REC-xquery-20070123/"),
-    XQUERY_1_0_MARKLOGIC_5("1.0-ml/5", XQueryVersion.XQUERY_1_0_MARKLOGIC, XQueryVendor.MARKLOGIC, XQueryBundle.message("xquery.vendor.description.ml/5"), "https://docs.marklogic.com/5.0/guide/xquery/dialects"),
-    XQUERY_1_0_MARKLOGIC_6("1.0-ml/6", XQueryVersion.XQUERY_1_0_MARKLOGIC, XQueryVendor.MARKLOGIC, XQueryBundle.message("xquery.vendor.description.ml/6"), "https://docs.marklogic.com/6.0/guide/xquery/dialects"),
-    XQUERY_1_0_MARKLOGIC_7("1.0-ml/7", XQueryVersion.XQUERY_1_0_MARKLOGIC, XQueryVendor.MARKLOGIC, XQueryBundle.message("xquery.vendor.description.ml/7"), "https://docs.marklogic.com/7.0/guide/xquery/dialects"),
-    XQUERY_1_0_MARKLOGIC_8("1.0-ml/8", XQueryVersion.XQUERY_1_0_MARKLOGIC, XQueryVendor.MARKLOGIC, XQueryBundle.message("xquery.vendor.description.ml/8"), "https://docs.marklogic.com/8.0/guide/xquery/dialects"),
-    XQUERY_3_0_W3C("3.0/W3C", XQueryVersion.XQUERY_3_0, XQueryVendor.W3C, XQueryBundle.message("xquery.variant.description.3.0/W3C"), "https://www.w3.org/TR/2014/REC-xquery-30-20140408/"),
-    XQUERY_3_1_W3C("3.1/W3C", XQueryVersion.XQUERY_3_1, XQueryVendor.W3C, XQueryBundle.message("xquery.variant.description.3.1/W3C"), "https://www.w3.org/TR/2015/CR-xquery-31-20151217/");
+    XQUERY_0_9_MARKLOGIC_3_2("0.9-ml/3.2", XQueryVersion.XQUERY_0_9_MARKLOGIC, XQueryVendor.MARKLOGIC, XQueryBundle.message("xquery.dialect.description.0.9-ml/3.2"), "https://docs.marklogic.com/guide/xquery/dialects#id_65735"),
+    XQUERY_1_0_W3C("1.0/W3C", XQueryVersion.XQUERY_1_0, XQueryVendor.W3C, XQueryBundle.message("xquery.dialect.description.1.0/W3C"), "https://www.w3.org/TR/2007/REC-xquery-20070123/"),
+    XQUERY_1_0_MARKLOGIC_5("1.0-ml/5", XQueryVersion.XQUERY_1_0_MARKLOGIC, XQueryVendor.MARKLOGIC, XQueryBundle.message("xquery.dialect.description.1.0-ml/5"), "https://docs.marklogic.com/5.0/guide/xquery/dialects"),
+    XQUERY_1_0_MARKLOGIC_6("1.0-ml/6", XQueryVersion.XQUERY_1_0_MARKLOGIC, XQueryVendor.MARKLOGIC, XQueryBundle.message("xquery.dialect.description.1.0-ml/6"), "https://docs.marklogic.com/6.0/guide/xquery/dialects"),
+    XQUERY_1_0_MARKLOGIC_7("1.0-ml/7", XQueryVersion.XQUERY_1_0_MARKLOGIC, XQueryVendor.MARKLOGIC, XQueryBundle.message("xquery.dialect.description.1.0-ml/7"), "https://docs.marklogic.com/7.0/guide/xquery/dialects"),
+    XQUERY_1_0_MARKLOGIC_8("1.0-ml/8", XQueryVersion.XQUERY_1_0_MARKLOGIC, XQueryVendor.MARKLOGIC, XQueryBundle.message("xquery.dialect.description.1.0-ml/8"), "https://docs.marklogic.com/8.0/guide/xquery/dialects"),
+    XQUERY_3_0_W3C("3.0/W3C", XQueryVersion.XQUERY_3_0, XQueryVendor.W3C, XQueryBundle.message("xquery.dialect.description.3.0/W3C"), "https://www.w3.org/TR/2014/REC-xquery-30-20140408/"),
+    XQUERY_3_1_W3C("3.1/W3C", XQueryVersion.XQUERY_3_1, XQueryVendor.W3C, XQueryBundle.message("xquery.dialect.description.3.1/W3C"), "https://www.w3.org/TR/2015/CR-xquery-31-20151217/");
 
     private final String mName;
     private final XQueryVersion mVersion;
