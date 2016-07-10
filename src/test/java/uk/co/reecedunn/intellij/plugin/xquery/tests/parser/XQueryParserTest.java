fdf0172ad0d57a36542483f9ed64ec97172cabcc
Reece H. Dunn <msclrhd@gmail.com>
2016-07-10 20:21:16 +0100

    XQueryParserTest: Provide a programmatical way to test the parsed AST nodes like the API for testing Lexer tokens.

--- src/test/java/uk/co/reecedunn/intellij/plugin/xquery/tests/parser/XQueryParserTest.java
+++ src/test/java/uk/co/reecedunn/intellij/plugin/xquery/tests/parser/XQueryParserTest.java
@@ -23,6 +23,9 @@ import com.intellij.openapi.extensions.Extensions;
 import com.intellij.openapi.progress.ProgressManager;
 import com.intellij.openapi.progress.impl.ProgressManagerImpl;
 import com.intellij.openapi.util.Disposer;
+import com.intellij.openapi.util.Pair;
+import com.intellij.psi.impl.source.tree.CompositeElement;
+import com.intellij.psi.tree.IElementType;
 import com.intellij.psi.tree.TokenSet;
 import com.intellij.testFramework.PlatformLiteFixture;
 import org.jetbrains.annotations.NotNull;
@@ -34,11 +37,15 @@ import org.picocontainer.defaults.AbstractComponentAdapter;
 import uk.co.reecedunn.intellij.plugin.xquery.lexer.XQueryTokenType;
 import uk.co.reecedunn.intellij.plugin.xquery.parser.XQueryParserDefinition;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.hamcrest.CoreMatchers.is;
-import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class XQueryParserTest extends PlatformLiteFixture {
+    // region IntelliJ Platform Infrastructure
+
     protected @Override boolean shouldContainTempFiles() {
         return false;
     }
@@ -69,17 +76,45 @@ public class XQueryParserTest extends PlatformLiteFixture {
         registerApplicationService(DefaultASTFactory.class, new DefaultASTFactoryImpl());
     }
 
-    private @NotNull ASTNode parseText(@NotNull String text) {
+    // endregion
+    // region Parser Test Helpers
+
+    private void serializeASTNode(@NotNull List<Pair<Integer, ASTNode>> nodes, ASTNode node, int depth) {
+        nodes.add(Pair.create(depth, node));
+        for (ASTNode child : node.getChildren(null)) {
+            serializeASTNode(nodes, child, depth + 1);
+        }
+    }
+
+    private @NotNull List<Pair<Integer, ASTNode>> parseText(@NotNull String text, int size) {
         ParserDefinition parserDefinition = new XQueryParserDefinition();
         TokenSet whitespaces = parserDefinition.getWhitespaceTokens();
         TokenSet comments = parserDefinition.getCommentTokens();
         Lexer lexer = parserDefinition.createLexer(null);
         PsiBuilder builder = new PsiBuilderImpl(null, null, whitespaces, comments, lexer, null, text, null, null);
-        return parserDefinition.createParser(null).parse(XQueryTokenType.WHITE_SPACE, builder);
+
+        ASTNode node = parserDefinition.createParser(null).parse(XQueryTokenType.WHITE_SPACE, builder);
+        List<Pair<Integer, ASTNode>> nodes = new ArrayList<>();
+        serializeASTNode(nodes, node, 0);
+        assertThat(nodes.size(), is(size));
+        return nodes;
     }
 
+    private void matchASTNode(@NotNull final Pair<Integer, ASTNode> node, int depth, Class nodeClass, IElementType type, int start, int end, String text) {
+        assertThat(node.getFirst(), is(depth));
+
+        assertThat(node.getSecond().getText(), is(text));
+        assertThat(node.getSecond().getElementType(), is(type));
+        assertThat(node.getSecond().getClass().getName(), is(nodeClass.getName()));
+
+        assertThat(node.getSecond().getTextRange().getStartOffset(), is(start));
+        assertThat(node.getSecond().getTextRange().getEndOffset(), is(end));
+    }
+
+    // endregion
+
     public void testEmptyBuffer() {
-        final ASTNode node = parseText("");
-        assertThat(node, is(notNullValue()));
+        final List<Pair<Integer, ASTNode>> nodes = parseText("", 1);
+        matchASTNode(nodes.get(0), 0, CompositeElement.class, XQueryTokenType.WHITE_SPACE, 0, 0, "");
     }
 }
