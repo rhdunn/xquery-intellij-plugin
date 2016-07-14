2573a42e5a902857a9ae121dccd1b9f01c8db529
Reece H. Dunn <msclrhd@gmail.com>
2016-07-14 23:30:37 +0100

    Use a mock VirtualFile implementation in the filetype tests as LightVirtualFile does not work well with multiple ParserTestCase classes.

---
+++ src/test/java/uk/co/reecedunn/intellij/plugin/xquery/tests/filetypes/MockVirtualFile.java
@@ -0,0 +1,102 @@
+/*
+ * Copyright (C) 2016 Reece H. Dunn
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package uk.co.reecedunn.intellij.plugin.xquery.tests.filetypes;
+
+import com.intellij.openapi.vfs.VirtualFile;
+import com.intellij.openapi.vfs.VirtualFileSystem;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+public class MockVirtualFile extends VirtualFile {
+    @NotNull
+    @Override
+    public String getName() {
+        return null;
+    }
+
+    @NotNull
+    @Override
+    public VirtualFileSystem getFileSystem() {
+        return null;
+    }
+
+    @NotNull
+    @Override
+    public String getPath() {
+        return null;
+    }
+
+    @Override
+    public boolean isWritable() {
+        return false;
+    }
+
+    @Override
+    public boolean isDirectory() {
+        return false;
+    }
+
+    @Override
+    public boolean isValid() {
+        return false;
+    }
+
+    @Override
+    public VirtualFile getParent() {
+        return null;
+    }
+
+    @Override
+    public VirtualFile[] getChildren() {
+        return new VirtualFile[0];
+    }
+
+    @NotNull
+    @Override
+    public OutputStream getOutputStream(Object requestor, long newModificationStamp, long newTimeStamp) throws IOException {
+        return null;
+    }
+
+    @NotNull
+    @Override
+    public byte[] contentsToByteArray() throws IOException {
+        return new byte[0];
+    }
+
+    @Override
+    public long getTimeStamp() {
+        return 0;
+    }
+
+    @Override
+    public long getLength() {
+        return 0;
+    }
+
+    @Override
+    public void refresh(boolean asynchronous, boolean recursive, @Nullable Runnable postRunnable) {
+
+    }
+
+    @Override
+    public InputStream getInputStream() throws IOException {
+        return null;
+    }
+}
