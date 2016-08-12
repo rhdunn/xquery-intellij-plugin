13c315e9e84f300b20d924d1cfd2bbb94245421f
Reece H. Dunn <msclrhd@gmail.com>
2016-08-12 13:01:21 +0100

    XQueryLexer: Implement XQuery 1.0 (A.1) [89] ParenthesizedExpr keywords and operators.

--- src/test/java/uk/co/reecedunn/intellij/plugin/xquery/tests/lexer/XQueryLexerTest.java
+++ src/test/java/uk/co/reecedunn/intellij/plugin/xquery/tests/lexer/XQueryLexerTest.java
@@ -373,6 +373,17 @@ public class XQueryLexerTest extends TestCase {
     }
 
     // endregion
+    // region ParenthesizedExpr
+
+    @Specification(name="XQuery 1.0 2ed", reference="https://www.w3.org/TR/2010/REC-xquery-20101214/#doc-xquery-ParenthesizedExpr")
+    public void testParenthesizedExpr() {
+        Lexer lexer = new XQueryLexer();
+
+        matchSingleToken(lexer, "(", XQueryTokenType.PARENTHESIS_OPEN);
+        matchSingleToken(lexer, ")", XQueryTokenType.PARENTHESIS_CLOSE);
+    }
+
+    // endregion
     // region DirCommentConstructor
 
     @Specification(name="XQuery 1.0 2ed", reference="https://www.w3.org/TR/2010/REC-xquery-20101214/#prod-xquery-DirCommentConstructor")
