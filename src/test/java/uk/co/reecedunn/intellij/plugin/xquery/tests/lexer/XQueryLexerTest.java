e0da00f0fea9c6dfcae4684ddfd26ac11d1c96e1
Reece H. Dunn <msclrhd@gmail.com>
2016-08-13 15:50:53 +0100

    XQueryParser: Implement XQuery 1.0 (A.1) [31] Expr.

--- src/test/java/uk/co/reecedunn/intellij/plugin/xquery/tests/lexer/XQueryLexerTest.java
+++ src/test/java/uk/co/reecedunn/intellij/plugin/xquery/tests/lexer/XQueryLexerTest.java
@@ -363,6 +363,16 @@ public class XQueryLexerTest extends TestCase {
     }
 
     // endregion
+    // region Expr
+
+    @Specification(name="XQuery 1.0 2ed", reference="https://www.w3.org/TR/2010/REC-xquery-20101214/#doc-xquery-Expr")
+    public void testExpr() {
+        Lexer lexer = new XQueryLexer();
+
+        matchSingleToken(lexer, ",", XQueryTokenType.COMMA);
+    }
+
+    // endregion
     // region VarRef
 
     @Specification(name="XQuery 1.0 2ed", reference="https://www.w3.org/TR/2010/REC-xquery-20101214/#doc-xquery-VarRef")
