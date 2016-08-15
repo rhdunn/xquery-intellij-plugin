d3f322cd7ca5518f6f3e8b74152d5f079caf04ad
Reece H. Dunn <msclrhd@gmail.com>
2016-08-15 12:41:52 +0100

    XQueryLexer: Implement XQuery 1.0 (A.1) [116] CompPIConstructor keywords and operators.

--- src/test/java/uk/co/reecedunn/intellij/plugin/xquery/tests/lexer/XQueryLexerTest.java
+++ src/test/java/uk/co/reecedunn/intellij/plugin/xquery/tests/lexer/XQueryLexerTest.java
@@ -1222,6 +1222,18 @@ public class XQueryLexerTest extends TestCase {
     }
 
     // endregion
+    // region CompPIConstructor
+
+    @Specification(name="XQuery 1.0 2ed", reference="https://www.w3.org/TR/2010/REC-xquery-20101214/#doc-xquery-CompPIConstructor")
+    public void testCompPIConstructor() {
+        Lexer lexer = new XQueryLexer();
+
+        matchSingleToken(lexer, "processing-instruction", XQueryTokenType.K_PROCESSING_INSTRUCTION);
+        matchSingleToken(lexer, "{",                      XQueryTokenType.BLOCK_OPEN);
+        matchSingleToken(lexer, "}",                      XQueryTokenType.BLOCK_CLOSE);
+    }
+
+    // endregion
     // region TypeDeclaration
 
     @Specification(name="XQuery 1.0 2ed", reference="https://www.w3.org/TR/2010/REC-xquery-20101214/#doc-xquery-TypeDeclaration")
