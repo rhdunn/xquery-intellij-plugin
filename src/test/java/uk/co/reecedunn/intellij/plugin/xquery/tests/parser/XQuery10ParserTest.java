b1fe43a3be7697c4488fe81a173fdf7b271a58d4
Reece H. Dunn <msclrhd@gmail.com>
2016-09-07 07:26:45 +0100

    XQueryParser: Add tests for keyword-based NCNames in AbbrevForwardStep (documenting the current behaviour).

--- src/test/java/uk/co/reecedunn/intellij/plugin/xquery/tests/parser/XQuery10ParserTest.java
+++ src/test/java/uk/co/reecedunn/intellij/plugin/xquery/tests/parser/XQuery10ParserTest.java
@@ -2530,6 +2530,13 @@ public class XQuery10ParserTest extends ParserTestCase {
         assertThat(prettyPrintASTNode(actual), is(expected));
     }
 
+    @Specification(name="XQuery 1.0 2ed", reference="https://www.w3.org/TR/2010/REC-xquery-20101214/#prod-xquery-AbbrevForwardStep")
+    public void testAbbrevForwardStep_KeywordNCNames_XQuery10() {
+        final String expected = loadResource("tests/parser/xquery-1.0/AbbrevForwardStep_KeywordNCNames_XQuery10.txt");
+        final ASTNode actual = parseResource("tests/parser/xquery-1.0/AbbrevForwardStep_KeywordNCNames_XQuery10.xq");
+        assertThat(prettyPrintASTNode(actual), is(expected));
+    }
+
     // endregion
     // region ForwardStep
 
