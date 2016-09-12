eecb21641f5da59e5f58455bfe56c8bf67ff8e63
Reece H. Dunn <msclrhd@gmail.com>
2016-09-12 21:34:35 +0100

    Only test the Update Facility constructs in the Update Facility parser tests.

--- src/test/java/uk/co/reecedunn/intellij/plugin/xquery/tests/parser/UpdateFacility10ParserTest.java
+++ src/test/java/uk/co/reecedunn/intellij/plugin/xquery/tests/parser/UpdateFacility10ParserTest.java
@@ -23,7 +23,7 @@ import uk.co.reecedunn.intellij.plugin.xquery.tests.Specification;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 
-public class UpdateFacility10ParserTest extends XQueryParserTest {
+public class UpdateFacility10ParserTest extends ParserTestCase {
     // region Initialization
 
     public void initializeSettings(XQueryProjectSettings settings) {
