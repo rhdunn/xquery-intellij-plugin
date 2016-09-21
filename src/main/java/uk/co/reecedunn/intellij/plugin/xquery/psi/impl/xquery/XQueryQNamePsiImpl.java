d2aa392ce15756a100b8b92914123a060557c739
Reece H. Dunn <msclrhd@gmail.com>
2016-09-21 19:38:24 +0100

    Fix XQueryQNamePsiImpl -- implement the XQueryQName API.

--- src/main/java/uk/co/reecedunn/intellij/plugin/xquery/psi/impl/xquery/XQueryQNamePsiImpl.java
+++ src/main/java/uk/co/reecedunn/intellij/plugin/xquery/psi/impl/xquery/XQueryQNamePsiImpl.java
@@ -18,8 +18,9 @@ package uk.co.reecedunn.intellij.plugin.xquery.psi.impl.xquery;
 import com.intellij.extapi.psi.ASTWrapperPsiElement;
 import com.intellij.lang.ASTNode;
 import org.jetbrains.annotations.NotNull;
+import uk.co.reecedunn.intellij.plugin.xquery.ast.xquery.XQueryQName;
 
-public class XQueryQNamePsiImpl extends ASTWrapperPsiElement {
+public class XQueryQNamePsiImpl extends ASTWrapperPsiElement implements XQueryQName {
     public XQueryQNamePsiImpl(@NotNull ASTNode node) {
         super(node);
     }
