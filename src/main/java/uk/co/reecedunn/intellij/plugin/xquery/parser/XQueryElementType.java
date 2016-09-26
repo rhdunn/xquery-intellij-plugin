ca82fe40fe36eec7b9e87508182858a4b43a322a
Reece H. Dunn <msclrhd@gmail.com>
2016-09-26 12:40:52 +0100

    Fix the internal name of the CompBinaryConstructor element type.

--- src/main/java/uk/co/reecedunn/intellij/plugin/xquery/parser/XQueryElementType.java
+++ src/main/java/uk/co/reecedunn/intellij/plugin/xquery/parser/XQueryElementType.java
@@ -212,7 +212,7 @@ public interface XQueryElementType {
     IElementType COMPATIBILITY_ANNOTATION_MARKLOGIC = new ICompositeElementType("XQUERY_COMPATIBILITY_ANNOTATION_MARKLOGIC", MarkLogicCompatibilityAnnotationImpl.class, MarkLogicCompatibilityAnnotationPsiImpl.class);
     IElementType STYLESHEET_IMPORT = new ICompositeElementType("XQUERY_STYLESHEET_IMPORT", MarkLogicStylesheetImportImpl.class, MarkLogicStylesheetImportPsiImpl.class);
 
-    IElementType COMP_BINARY_CONSTRUCTOR = new ICompositeElementType("XQUERY_BINARY_EXPR", MarkLogicCompBinaryConstructorImpl.class, MarkLogicCompBinaryConstructorPsiImpl.class);
+    IElementType COMP_BINARY_CONSTRUCTOR = new ICompositeElementType("XQUERY_COMP_BINARY_CONSTRUCTOR", MarkLogicCompBinaryConstructorImpl.class, MarkLogicCompBinaryConstructorPsiImpl.class);
     IElementType BINARY_KIND_TEST = new ICompositeElementType("XQUERY_BINARY_KIND_TEST", MarkLogicBinaryKindTestImpl.class, MarkLogicBinaryKindTestPsiImpl.class);
 
     // endregion
