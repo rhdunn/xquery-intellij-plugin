486f1aef75ad768266f8c321a1b6b16d138e96c0
Reece H. Dunn <msclrhd@gmail.com>
2016-10-31 07:06:05 +0000

    XQueryLexer: Implement XQuery 3.1 (A.1) [181] UnaryLookup tokens.

--- src/test/java/uk/co/reecedunn/intellij/plugin/xquery/tests/lexer/XQueryLexerTest.java
+++ src/test/java/uk/co/reecedunn/intellij/plugin/xquery/tests/lexer/XQueryLexerTest.java
@@ -3497,6 +3497,16 @@ public class XQueryLexerTest extends TestCase {
     }
 
     // endregion
+    // region XQuery 3.1 :: UnaryLookup
+
+    @Specification(name="XQuery 3.1 CR", reference="https://www.w3.org/TR/2015/CR-xquery-31-20151217/#prod-xquery31-UnaryLookup")
+    public void testUnaryLookup() {
+        Lexer lexer = new XQueryLexer();
+
+        matchSingleToken(lexer, "?", XQueryTokenType.OPTIONAL);
+    }
+
+    // endregion
     // region XQuery 3.1 :: AnyMapTest
 
     @Specification(name="XQuery 3.1 CR", reference="https://www.w3.org/TR/2015/CR-xquery-31-20151217/#prod-xquery31-AnyMapTest")
