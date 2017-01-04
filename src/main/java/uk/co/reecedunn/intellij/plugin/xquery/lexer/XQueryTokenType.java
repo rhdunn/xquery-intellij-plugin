f44ed8b78ebe2699802ad59a108270efc0f8e21e
Reece H. Dunn <msclrhd@gmail.com>
2017-01-04 13:33:15 +0000

    XQueryLexer: Implement XQuery Full Text 1.0 (A.1) [146] FTOr tokens.

--- src/main/java/uk/co/reecedunn/intellij/plugin/xquery/lexer/XQueryTokenType.java
+++ src/main/java/uk/co/reecedunn/intellij/plugin/xquery/lexer/XQueryTokenType.java
@@ -192,6 +192,7 @@ public interface XQueryTokenType extends TokenType {
     IXQueryKeywordOrNCNameType K_FOLLOWING_SIBLING = new IXQueryKeywordOrNCNameType("XQUERY_KEYWORD_OR_NCNAME_FOLLOWING_SIBLING");
     IXQueryKeywordOrNCNameType K_FOR = new IXQueryKeywordOrNCNameType("XQUERY_KEYWORD_OR_NCNAME_FOR");
     IXQueryKeywordOrNCNameType K_FT_OPTION = new IXQueryKeywordOrNCNameType("XQUERY_KEYWORD_OR_NCNAME_FT_OPTION"); // Full Text 1.0
+    IXQueryKeywordOrNCNameType K_FTOR = new IXQueryKeywordOrNCNameType("XQUERY_KEYWORD_OR_NCNAME_FTOR"); // Full Text 1.0
     IXQueryKeywordOrNCNameType K_FUNCTION = new IXQueryKeywordOrNCNameType("XQUERY_KEYWORD_OR_NCNAME_FUNCTION", IXQueryKeywordOrNCNameType.KeywordType.XQUERY30_RESERVED_FUNCTION_NAME);
     IXQueryKeywordOrNCNameType K_GE = new IXQueryKeywordOrNCNameType("XQUERY_KEYWORD_OR_NCNAME_GE");
     IXQueryKeywordOrNCNameType K_GREATEST = new IXQueryKeywordOrNCNameType("XQUERY_KEYWORD_OR_NCNAME_GREATEST");
--- src/test/java/uk/co/reecedunn/intellij/plugin/xquery/tests/lexer/XQueryLexerTest.java
+++ src/test/java/uk/co/reecedunn/intellij/plugin/xquery/tests/lexer/XQueryLexerTest.java
@@ -3995,6 +3995,16 @@ public class XQueryLexerTest extends LexerTestCase {
     }
 
     // endregion
+    // region Full Text 1.0 :: FTOr
+
+    @Specification(name="XQuery Full Text 1.0", reference="https://www.w3.org/TR/2011/REC-xpath-full-text-10-20110317/#prod-xquery10-FTOr")
+    public void testFTOr() {
+        Lexer lexer = new XQueryLexer();
+
+        matchSingleToken(lexer, "ftor", XQueryTokenType.K_FTOR);
+    }
+
+    // endregion
     // region MarkLogic 6.0 :: TransactionSeparator
 
     public void testTransactionSeparator() {
