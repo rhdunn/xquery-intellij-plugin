ab7089ae452f2bbe5c970247a05777d3945be9a7
Reece H. Dunn <msclrhd@gmail.com>
2017-07-16 07:53:50 +0100

    Create a skeleton implementation for a MarkLogic run configuration.

---
+++ src/main/java/uk/co/reecedunn/intellij/plugin/execution/marklogic/configuration/MarkLogicRunConfiguration.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright (C) 2017 Reece H. Dunn
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package uk.co.reecedunn.intellij.plugin.execution.marklogic.configuration;
+
+import com.intellij.execution.ExecutionException;
+import com.intellij.execution.Executor;
+import com.intellij.execution.configurations.*;
+import com.intellij.execution.runners.ExecutionEnvironment;
+import com.intellij.openapi.options.SettingsEditor;
+import com.intellij.openapi.project.Project;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+public class MarkLogicRunConfiguration extends RunConfigurationBase {
+    MarkLogicRunConfiguration(@NotNull Project project, @NotNull ConfigurationFactory factory, String name) {
+        super(project, factory, name);
+    }
+
+    @NotNull
+    @Override
+    public SettingsEditor<? extends RunConfiguration> getConfigurationEditor() {
+        return new MarkLogicSettingsEditor();
+    }
+
+    @Override
+    public void checkConfiguration() throws RuntimeConfigurationException {
+    }
+
+    @Nullable
+    @Override
+    public RunProfileState getState(@NotNull Executor executor, @NotNull ExecutionEnvironment environment) throws ExecutionException {
+        return null;
+    }
+}
---
+++ src/main/java/uk/co/reecedunn/intellij/plugin/execution/marklogic/configuration/MarkLogicSettingsEditor.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright (C) 2017 Reece H. Dunn
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package uk.co.reecedunn.intellij.plugin.execution.marklogic.configuration;
+
+import com.intellij.openapi.options.ConfigurationException;
+import com.intellij.openapi.options.SettingsEditor;
+import org.jetbrains.annotations.NotNull;
+
+import javax.swing.*;
+
+public class MarkLogicSettingsEditor extends SettingsEditor<MarkLogicRunConfiguration> {
+    @Override
+    protected void resetEditorFrom(@NotNull MarkLogicRunConfiguration s) {
+    }
+
+    @Override
+    protected void applyEditorTo(@NotNull MarkLogicRunConfiguration s) throws ConfigurationException {
+    }
+
+    @NotNull
+    @Override
+    protected JComponent createEditor() {
+        return new JPanel();
+    }
+}
