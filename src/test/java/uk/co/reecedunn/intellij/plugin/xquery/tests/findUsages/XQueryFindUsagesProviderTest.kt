a6a3a737fce78bd4f4c6c60ba9d4c39348981a7b
Reece H. Dunn <msclrhd@gmail.com>
2017-08-07 19:00:51 +0100

    Convert XQueryFindUsagesProviderTest to Kotlin.

---
+++ src/test/java/uk/co/reecedunn/intellij/plugin/xquery/tests/findUsages/XQueryFindUsagesProviderTest.kt
@@ -0,0 +1,46 @@
+/*
+ * Copyright (C) 2016-2017 Reece H. Dunn
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package uk.co.reecedunn.intellij.plugin.xquery.tests.findUsages
+
+import com.intellij.lang.HelpID
+import uk.co.reecedunn.intellij.plugin.xquery.ast.xquery.XQueryNCName
+import uk.co.reecedunn.intellij.plugin.xquery.ast.xquery.XQueryVarName
+import uk.co.reecedunn.intellij.plugin.xquery.ast.xquery.XQueryVarRef
+import uk.co.reecedunn.intellij.plugin.xquery.findUsages.XQueryFindUsagesProvider
+import uk.co.reecedunn.intellij.plugin.xquery.tests.parser.ParserTestCase
+
+import org.hamcrest.CoreMatchers.`is`
+import org.hamcrest.MatcherAssert.assertThat
+import uk.co.reecedunn.intellij.plugin.core.extensions.children
+import uk.co.reecedunn.intellij.plugin.core.extensions.descendants
+
+class XQueryFindUsagesProviderTest : ParserTestCase() {
+    fun testNCName() {
+        val provider = XQueryFindUsagesProvider()
+
+        val file = parseResource("tests/parser/xquery-1.0/VarRef.xq")!!
+        val varRef = file.descendants().filterIsInstance<XQueryVarRef>().first()
+        val varName = varRef.children().filterIsInstance<XQueryVarName>().first()
+        val ncName = varName.children().filterIsInstance<XQueryNCName>().first()
+
+        assertThat(provider.canFindUsagesFor(ncName), `is`(true))
+        assertThat(provider.getHelpId(ncName), `is`(HelpID.FIND_OTHER_USAGES))
+        assertThat(provider.getType(ncName), `is`("Identifier"))
+        assertThat(provider.getDescriptiveName(ncName), `is`("one"))
+        assertThat(provider.getNodeText(ncName, true), `is`("one"))
+        assertThat(provider.getNodeText(ncName, false), `is`("one"))
+    }
+}
