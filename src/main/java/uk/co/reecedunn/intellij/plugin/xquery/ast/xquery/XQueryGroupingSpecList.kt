613b3d604bcab13ef62fcb1a37785240550a3ac5
Reece H. Dunn <msclrhd@gmail.com>
2017-08-08 07:57:30 +0100

    Convert XQueryGroupingSpecList to Kotlin.

--- src/main/java/uk/co/reecedunn/intellij/plugin/xquery/ast/xquery/XQueryGroupingSpecList.kt
+++ src/main/java/uk/co/reecedunn/intellij/plugin/xquery/ast/xquery/XQueryGroupingSpecList.kt
@@ -13,12 +13,11 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package uk.co.reecedunn.intellij.plugin.xquery.ast.xquery;
+package uk.co.reecedunn.intellij.plugin.xquery.ast.xquery
 
-import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiElement
 
 /**
- * An XQuery 3.0 <code>GroupingSpecList</code> node in the XQuery AST.
+ * An XQuery 3.0 `GroupingSpecList` node in the XQuery AST.
  */
-public interface XQueryGroupingSpecList extends PsiElement {
-}
+interface XQueryGroupingSpecList : PsiElement
