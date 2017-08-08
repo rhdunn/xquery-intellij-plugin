f7e6ac45c726d84e65b3eabdb6fe321ee26a0542
Reece H. Dunn <msclrhd@gmail.com>
2017-08-08 23:17:21 +0100

    Convert UnboundQNamePrefixInspection to Kotlin.

---
+++ src/main/java/uk/co/reecedunn/intellij/plugin/xquery/inspections/XPST0081/UnboundQNamePrefixInspection.kt
@@ -0,0 +1,71 @@
+/*
+ * Copyright (C) 2016-2017 Reece H. Dunn
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package uk.co.reecedunn.intellij.plugin.xquery.inspections.XPST0081
+
+import com.intellij.codeInspection.*
+import com.intellij.psi.PsiElement
+import com.intellij.psi.PsiFile
+import com.intellij.util.SmartList
+import uk.co.reecedunn.intellij.plugin.xquery.ast.xquery.XQueryEQName
+import uk.co.reecedunn.intellij.plugin.xquery.ast.xquery.XQueryFile
+import uk.co.reecedunn.intellij.plugin.xquery.ast.xquery.XQueryNCName
+import uk.co.reecedunn.intellij.plugin.xquery.resources.XQueryBundle
+
+/** XPST0081 error condition
+
+ * It is a *static error* if a QName used in a query contains a
+ * namespace prefix that is not in the *statically known namespaces*.
+ */
+class UnboundQNamePrefixInspection : LocalInspectionTool() {
+    override fun getDisplayName(): String =
+        XQueryBundle.message("inspection.XPST0081.unbound-qname-prefix.display-name")
+
+    override fun getDescriptionFileName(): String? =
+        id + ".html"
+
+    override fun checkFile(file: PsiFile, manager: InspectionManager, isOnTheFly: Boolean): Array<ProblemDescriptor>? {
+        if (file !is XQueryFile) return null
+
+        val descriptors = SmartList<ProblemDescriptor>()
+        checkElement(file, manager, descriptors, isOnTheFly)
+        return descriptors.toTypedArray()
+    }
+
+    private fun checkElement(element: PsiElement?,
+                             manager: InspectionManager,
+                             descriptors: MutableList<ProblemDescriptor>,
+                             isOnTheFly: Boolean) {
+        var element = element
+        if (element is XQueryEQName) {
+            val qname = element
+            val context = qname.prefix
+            if (context !is XQueryNCName || context.text == "xmlns") {
+                return
+            }
+            if (!qname.resolvePrefixNamespace().iterator().hasNext()) {
+                val description = XQueryBundle.message("inspection.XPST0081.unbound-qname-prefix.message")
+                descriptors.add(manager.createProblemDescriptor(context, description, null as LocalQuickFix?, ProblemHighlightType.GENERIC_ERROR, isOnTheFly))
+            }
+            return
+        }
+
+        element = element?.firstChild
+        while (element != null) {
+            checkElement(element, manager, descriptors, isOnTheFly)
+            element = element.nextSibling
+        }
+    }
+}
