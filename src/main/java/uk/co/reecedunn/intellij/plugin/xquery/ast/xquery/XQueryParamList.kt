645ac75f0c16ba53322a3fdbffae38c89f235a1e
Reece H. Dunn <msclrhd@gmail.com>
2017-08-08 21:17:04 +0100

    Convert XQueryParamList to Kotlin.

--- src/main/java/uk/co/reecedunn/intellij/plugin/xquery/ast/xquery/XQueryParamList.kt
+++ src/main/java/uk/co/reecedunn/intellij/plugin/xquery/ast/xquery/XQueryParamList.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2016 Reece H. Dunn
+ * Copyright (C) 2016-2017 Reece H. Dunn
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,13 +13,13 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package uk.co.reecedunn.intellij.plugin.xquery.ast.xquery;
+package uk.co.reecedunn.intellij.plugin.xquery.ast.xquery
 
-import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiElement
 
 /**
- * An XQuery 1.0 <code>ParamList</code> node in the XQuery AST.
+ * An XQuery 1.0 `ParamList` node in the XQuery AST.
  */
-public interface XQueryParamList extends PsiElement {
-    int getArity();
+interface XQueryParamList : PsiElement {
+    val arity: Int
 }
