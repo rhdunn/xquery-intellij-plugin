7acd1cb45a35ee10695fe132b14548460c847fe5
Reece H. Dunn <msclrhd@gmail.com>
2017-08-18 07:37:23 +0100

    Model: Move the list items to named constants in the implementation objects.

--- src/main/java/uk/co/reecedunn/intellij/plugin/xquery/lang/Model.kt
+++ src/main/java/uk/co/reecedunn/intellij/plugin/xquery/lang/Model.kt
@@ -53,13 +53,18 @@ private class BaseXProduct(id: String, name: String, implementation: Implementat
 }
 
 object BaseX : Implementation("basex", "BaseX", "http://www.basex.org/") {
-    override val versions: List<Version> = listOf(
-        ProductVersion("8.4"),
-        ProductVersion("8.5"),
-        ProductVersion("8.6"))
+    val VERSION_8_4: Version = ProductVersion("8.4")
+    val VERSION_8_5: Version = ProductVersion("8.5")
+    val VERSION_8_6: Version = ProductVersion("8.6")
 
-    override val products: List<Product> = listOf(
-        BaseXProduct("basex", "BaseX", this))
+    override val versions: List<Version> = listOf(
+        VERSION_8_4,
+        VERSION_8_5,
+        VERSION_8_6)
+
+    val BASEX: Product = BaseXProduct("basex", "BaseX", this)
+
+    override val products: List<Product> = listOf(BASEX)
 }
 
 // endregion
@@ -70,14 +75,20 @@ private class MarkLogicProduct(id: String, name: String, implementation: Impleme
 }
 
 object MarkLogic : Implementation("marklogic", "MarkLogic", "http://www.marklogic.com/") {
-    override val versions: List<Version> = listOf(
-        ProductVersion("6.0"),
-        ProductVersion("7.0"),
-        ProductVersion("8.0"),
-        ProductVersion("9.0"))
+    val VERSION_6_0: Version = ProductVersion("6.0")
+    val VERSION_7_0: Version = ProductVersion("7.0")
+    val VERSION_8_0: Version = ProductVersion("8.0")
+    val VERSION_9_0: Version = ProductVersion("9.0")
 
-    override val products: List<Product> = listOf(
-        MarkLogicProduct("marklogic", "MarkLogic", this))
+    override val versions: List<Version> = listOf(
+        VERSION_6_0,
+        VERSION_7_0,
+        VERSION_8_0,
+        VERSION_9_0)
+
+    val MARKLOGIC: Product = MarkLogicProduct("marklogic", "MarkLogic", this)
+
+    override val products: List<Product> = listOf(MARKLOGIC)
 }
 
 // endregion
@@ -98,19 +109,25 @@ private class SaxonProduct(id: String, name: String, implementation: Implementat
 }
 
 object Saxon : Implementation("saxon", "Saxon", "http://www.saxonica.com") {
-    override val versions: List<Version> = listOf(
-        ProductVersion("9.5"),
-        ProductVersion("9.6"),
-        ProductVersion("9.7"),
-        ProductVersion("9.8"))
+    val VERSION_9_5: Version = ProductVersion("9.5")
+    val VERSION_9_6: Version = ProductVersion("9.6")
+    val VERSION_9_7: Version = ProductVersion("9.7")
+    val VERSION_9_8: Version = ProductVersion("9.8")
 
-    override val products: List<Product> = listOf(
-        SaxonProduct("HE", "Home Edition", this),
-        SaxonProduct("PE", "Professional Edition", this),
-        SaxonProduct("EE", "Enterprise Edition", this),
-        SaxonProduct("EE-T", "Enterprise Edition (Transformation package)", this),
-        SaxonProduct("EE-Q", "Enterprise Edition (Query package)", this),
-        SaxonProduct("EE-V", "Enterprise Edition (Validation package)", this))
+    override val versions: List<Version> = listOf(
+        VERSION_9_5,
+        VERSION_9_6,
+        VERSION_9_7,
+        VERSION_9_8)
+
+    val HE: Product = SaxonProduct("HE", "Home Edition", this)
+    val PE: Product = SaxonProduct("PE", "Professional Edition", this)
+    val EE: Product = SaxonProduct("EE", "Enterprise Edition", this)
+    val EE_T: Product = SaxonProduct("EE-T", "Enterprise Edition (Transformation package)", this)
+    val EE_Q: Product = SaxonProduct("EE-Q", "Enterprise Edition (Query package)", this)
+    val EE_V: Product = SaxonProduct("EE-V", "Enterprise Edition (Validation package)", this)
+
+    override val products: List<Product> = listOf(HE, PE, EE, EE_T, EE_Q, EE_V)
 }
 
 // endregion
@@ -121,12 +138,14 @@ private class W3CProduct(id: String, name: String, implementation: Implementatio
 }
 
 object W3C : Implementation("w3c", "W3C", "https://www.w3.org/XML/Query/") {
-    override val versions: List<Version> = listOf(
-        NamedVersion("1ed", 1.0, "First Edition"),
-        NamedVersion("2ed", 2.0, "Second Edition"))
+    val FIRST_EDITION: NamedVersion = NamedVersion("1ed", 1.0, "First Edition")
+    val SECOND_EDITION: NamedVersion = NamedVersion("2ed", 2.0, "Second Edition")
 
-    override val products: List<Product> = listOf(
-        W3CProduct("rec", "Recommendation", this))
+    override val versions: List<Version> = listOf(FIRST_EDITION, SECOND_EDITION)
+
+    val REC: Product = W3CProduct("rec", "Recommendation", this)
+
+    override val products: List<Product> = listOf(REC)
 }
 
 // endregion
