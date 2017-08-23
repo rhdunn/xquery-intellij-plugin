5681ca29450fcff141c3047af92136f25e5dbbff
Reece H. Dunn <msclrhd@gmail.com>
2017-08-23 07:54:42 +0100

    Convert ScriptingAssignmentExpr to Kotlin.

--- src/main/java/uk/co/reecedunn/intellij/plugin/xquery/ast/scripting/ScriptingAssignmentExpr.kt
+++ src/main/java/uk/co/reecedunn/intellij/plugin/xquery/ast/scripting/ScriptingAssignmentExpr.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2016 Reece H. Dunn
+ * Copyright (C) 2016-2017 Reece H. Dunn
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,12 +13,11 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package uk.co.reecedunn.intellij.plugin.xquery.ast.scripting;
+package uk.co.reecedunn.intellij.plugin.xquery.ast.scripting
 
-import uk.co.reecedunn.intellij.plugin.xquery.ast.xquery.XQueryExprSingle;
+import uk.co.reecedunn.intellij.plugin.xquery.ast.xquery.XQueryExprSingle
 
 /**
- * An XQuery Scripting Extension 1.0 <code>AssignmentExpr</code> node in the XQuery AST.
+ * An XQuery Scripting Extension 1.0 `AssignmentExpr` node in the XQuery AST.
  */
-public interface ScriptingAssignmentExpr extends XQueryExprSingle {
-}
+interface ScriptingAssignmentExpr : XQueryExprSingle
