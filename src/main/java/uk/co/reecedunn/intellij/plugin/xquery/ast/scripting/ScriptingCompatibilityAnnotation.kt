8ea7a20fc760da718fd59f30ff635f7b37d74420
Reece H. Dunn <msclrhd@gmail.com>
2017-08-23 08:00:33 +0100

    Convert ScriptingCompatibilityAnnotation to Kotlin.

--- src/main/java/uk/co/reecedunn/intellij/plugin/xquery/ast/scripting/ScriptingCompatibilityAnnotation.kt
+++ src/main/java/uk/co/reecedunn/intellij/plugin/xquery/ast/scripting/ScriptingCompatibilityAnnotation.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2016 Reece H. Dunn
+ * Copyright (C) 2016-2017 Reece H. Dunn
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,12 +13,11 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package uk.co.reecedunn.intellij.plugin.xquery.ast.scripting;
+package uk.co.reecedunn.intellij.plugin.xquery.ast.scripting
 
-import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiElement
 
 /**
- * An XQuery Scripting Extension 1.0 <code>VarDecl</code> annotation in the XQuery AST.
+ * An XQuery Scripting Extension 1.0 `VarDecl` annotation in the XQuery AST.
  */
-public interface ScriptingCompatibilityAnnotation extends PsiElement {
-}
+interface ScriptingCompatibilityAnnotation : PsiElement
