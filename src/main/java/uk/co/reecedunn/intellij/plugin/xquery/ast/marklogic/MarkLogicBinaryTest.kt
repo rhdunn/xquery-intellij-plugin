f5ada6a8c6dff957aab852d19f45b66c7523ee9e
Reece H. Dunn <msclrhd@gmail.com>
2017-08-23 12:51:37 +0100

    Convert MarkLogicBinaryTest to Kotlin.

--- src/main/java/uk/co/reecedunn/intellij/plugin/xquery/ast/marklogic/MarkLogicBinaryTest.kt
+++ src/main/java/uk/co/reecedunn/intellij/plugin/xquery/ast/marklogic/MarkLogicBinaryTest.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2016 Reece H. Dunn
+ * Copyright (C) 2016-2017 Reece H. Dunn
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,12 +13,11 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package uk.co.reecedunn.intellij.plugin.xquery.ast.marklogic;
+package uk.co.reecedunn.intellij.plugin.xquery.ast.marklogic
 
-import uk.co.reecedunn.intellij.plugin.xquery.ast.xquery.XQueryKindTest;
+import uk.co.reecedunn.intellij.plugin.xquery.ast.xquery.XQueryKindTest
 
 /**
- * A MarkLogic <code>BinaryTest</code> node in the XQuery AST.
+ * A MarkLogic `BinaryTest` node in the XQuery AST.
  */
-public interface MarkLogicBinaryTest extends XQueryKindTest {
-}
+interface MarkLogicBinaryTest : XQueryKindTest
