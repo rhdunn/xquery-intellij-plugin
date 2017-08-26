1fc4bea83d7f34bcfa0dd7230b5a7b223a522af9
Reece H. Dunn <msclrhd@gmail.com>
2017-08-26 17:40:11 +0100

    Convert XQueryStringConcatExpr to Kotlin.

--- src/main/java/uk/co/reecedunn/intellij/plugin/xquery/ast/xquery/XQueryStringConcatExpr.kt
+++ src/main/java/uk/co/reecedunn/intellij/plugin/xquery/ast/xquery/XQueryStringConcatExpr.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2016 Reece H. Dunn
+ * Copyright (C) 2016-2017 Reece H. Dunn
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,12 +13,11 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package uk.co.reecedunn.intellij.plugin.xquery.ast.xquery;
+package uk.co.reecedunn.intellij.plugin.xquery.ast.xquery
 
-import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiElement
 
 /**
- * An XQuery 3.0 <code>StringConcatExpr</code> node in the XQuery AST.
+ * An XQuery 3.0 `StringConcatExpr` node in the XQuery AST.
  */
-public interface XQueryStringConcatExpr extends PsiElement {
-}
+interface XQueryStringConcatExpr : PsiElement
