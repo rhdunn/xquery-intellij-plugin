05b3d8299a0716a28ba6c3d8c2bd68b99934f633
Reece H. Dunn <msclrhd@gmail.com>
2017-08-26 15:56:03 +0100

    Convert XQueryEnclosedExpr to Kotlin.

---
+++ src/main/java/uk/co/reecedunn/intellij/plugin/xquery/psi/impl/xquery/XQueryEnclosedExprPsiImpl.kt
@@ -0,0 +1,56 @@
+/*
+ * Copyright (C) 2016-2017 Reece H. Dunn
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package uk.co.reecedunn.intellij.plugin.xquery.psi.impl.xquery
+
+import com.intellij.extapi.psi.ASTWrapperPsiElement
+import com.intellij.lang.ASTNode
+import com.intellij.psi.PsiElement
+import uk.co.reecedunn.intellij.plugin.xquery.ast.xquery.*
+import uk.co.reecedunn.intellij.plugin.xquery.lang.*
+import uk.co.reecedunn.intellij.plugin.xquery.parser.XQueryElementType
+import uk.co.reecedunn.intellij.plugin.xquery.psi.XQueryConformanceCheck
+import uk.co.reecedunn.intellij.plugin.xquery.resources.XQueryBundle
+
+open class XQueryEnclosedExprPsiImpl(node: ASTNode) : ASTWrapperPsiElement(node), XQueryEnclosedExpr, XQueryConformanceCheck {
+    private fun previousVersionSupportsOptionalExpr(parent: PsiElement): Boolean =
+        parent is XQueryCompPIConstructor ||
+        parent is XQueryCompAttrConstructor ||
+        parent is XQueryExtensionExpr ||
+        parent is XQueryCurlyArrayConstructor
+
+    private fun marklogicSupportsOptionalExpr(parent: PsiElement): Boolean =
+        parent is XQueryCompTextConstructor ||
+        parent is XQueryDirAttributeValue ||
+        parent is XQueryDirElemContent ||
+        parent is XQueryCatchClause
+
+    override fun conformsTo(implementation: ImplementationItem): Boolean {
+        val parent = parent
+        if (previousVersionSupportsOptionalExpr(parent) || conformanceElement !== firstChild) {
+            return true
+        }
+        if (marklogicSupportsOptionalExpr(parent) && implementation.getVersion(MarkLogic).supportsVersion(XQueryVersion.VERSION_6_0)) {
+            return true
+        }
+        return implementation.getVersion(XQuery).supportsVersion(XQueryVersion.VERSION_3_1)
+    }
+
+    override val conformanceElement get(): PsiElement =
+        findChildByType(XQueryElementType.EXPR) ?: firstChild
+
+    override val conformanceErrorMessage get(): String =
+        XQueryBundle.message("requires.empty-expression.version", XQueryVersion.VERSION_3_1)
+}
