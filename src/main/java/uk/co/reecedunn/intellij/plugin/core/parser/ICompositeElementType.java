554d2aca88a08747ceca2396fccbaee5507782a8
Reece H. Dunn <msclrhd@gmail.com>
2017-09-29 07:08:34 +0100

    Rethrow ProcessCanceledException from createPsiElement.

--- src/main/java/uk/co/reecedunn/intellij/plugin/core/parser/ICompositeElementType.java
+++ src/main/java/uk/co/reecedunn/intellij/plugin/core/parser/ICompositeElementType.java
@@ -17,12 +17,14 @@ package uk.co.reecedunn.intellij.plugin.core.parser;
 
 import com.intellij.lang.ASTNode;
 import com.intellij.lang.Language;
+import com.intellij.openapi.progress.ProcessCanceledException;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.tree.IElementType;
 import org.jetbrains.annotations.NonNls;
 import org.jetbrains.annotations.NotNull;
 
 import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
 
 public class ICompositeElementType extends IElementType {
     private Constructor<?> mPsiConstructor;
@@ -40,6 +42,12 @@ public class ICompositeElementType extends IElementType {
         try {
             final Object[] arguments = new Object[]{ node };
             return (PsiElement)mPsiConstructor.newInstance(arguments);
+        } catch (InvocationTargetException e) {
+            Throwable cause = e.getCause();
+            if (cause instanceof ProcessCanceledException) {
+                throw (ProcessCanceledException)cause;
+            }
+            throw new AssertionError("Cannot create XQuery PsiElement for " + this, cause);
         } catch (Exception e) {
             throw new AssertionError("Cannot create XQuery PsiElement for " + this, e);
         }
