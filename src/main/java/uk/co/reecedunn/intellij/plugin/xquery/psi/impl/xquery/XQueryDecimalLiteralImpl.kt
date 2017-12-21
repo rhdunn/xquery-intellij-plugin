343ba4a6a7415a707cd370576555a76b41bab34d
Reece H. Dunn <msclrhd@gmail.com>
2017-12-21 09:53:11 +0000

    Convert XQueryDecimalLiteral to Kotlin.

--- src/main/java/uk/co/reecedunn/intellij/plugin/xquery/psi/impl/xquery/XQueryDecimalLiteralImpl.kt
+++ src/main/java/uk/co/reecedunn/intellij/plugin/xquery/psi/impl/xquery/XQueryDecimalLiteralImpl.kt
@@ -13,10 +13,10 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package uk.co.reecedunn.intellij.plugin.xquery.ast.xquery;
+package uk.co.reecedunn.intellij.plugin.xquery.psi.impl.xquery
 
-/**
- * An XQuery 1.0 <code>DecimalLiteral</code> node in the XQuery AST.
- */
-public interface XQueryDecimalLiteral extends XQueryNumericLiteral {
-}
+import com.intellij.psi.impl.source.tree.LeafPsiElement
+import com.intellij.psi.tree.IElementType
+import uk.co.reecedunn.intellij.plugin.xquery.ast.xquery.XQueryDecimalLiteral
+
+class XQueryDecimalLiteralImpl(type: IElementType, text: CharSequence) : LeafPsiElement(type, text), XQueryDecimalLiteral
