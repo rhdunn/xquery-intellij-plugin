7ca66fe495c5f86ff3e272f6fc688172c22ec9f9
Reece H. Dunn <msclrhd@gmail.com>
2017-12-21 10:16:03 +0000

    Convert XQueryDoubleLiteral to Kotlin.

--- src/main/java/uk/co/reecedunn/intellij/plugin/xquery/ast/xquery/XQueryDoubleLiteral.kt
+++ src/main/java/uk/co/reecedunn/intellij/plugin/xquery/ast/xquery/XQueryDoubleLiteral.kt
@@ -13,15 +13,9 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package uk.co.reecedunn.intellij.plugin.xquery.psi.impl.xquery;
+package uk.co.reecedunn.intellij.plugin.xquery.ast.xquery
 
-import com.intellij.psi.impl.source.tree.LeafPsiElement;
-import com.intellij.psi.tree.IElementType;
-import org.jetbrains.annotations.NotNull;
-import uk.co.reecedunn.intellij.plugin.xquery.ast.xquery.XQueryDoubleLiteral;
-
-public class XQueryDoubleLiteralImpl extends LeafPsiElement implements XQueryDoubleLiteral {
-    public XQueryDoubleLiteralImpl(@NotNull IElementType type, @NotNull CharSequence text) {
-        super(type, text);
-    }
-}
+/**
+ * An XQuery 1.0 `DoubleLiteral` node in the XQuery AST.
+ */
+interface XQueryDoubleLiteral : XQueryNumericLiteral
