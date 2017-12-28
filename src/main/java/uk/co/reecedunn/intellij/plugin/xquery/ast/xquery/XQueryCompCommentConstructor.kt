f78446e0ea9647f429293ed1462039fbe8ae20b7
Reece H. Dunn <msclrhd@gmail.com>
2017-12-28 23:37:52 +0000

    Convert XQueryCompCommentConstructor to Kotlin.

--- src/main/java/uk/co/reecedunn/intellij/plugin/xquery/ast/xquery/XQueryCompCommentConstructor.kt
+++ src/main/java/uk/co/reecedunn/intellij/plugin/xquery/ast/xquery/XQueryCompCommentConstructor.kt
@@ -13,15 +13,9 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package uk.co.reecedunn.intellij.plugin.xquery.psi.impl.xquery;
+package uk.co.reecedunn.intellij.plugin.xquery.ast.xquery
 
-import com.intellij.extapi.psi.ASTWrapperPsiElement;
-import com.intellij.lang.ASTNode;
-import org.jetbrains.annotations.NotNull;
-import uk.co.reecedunn.intellij.plugin.xquery.ast.xquery.XQueryCompCommentConstructor;
-
-public class XQueryCompCommentConstructorPsiImpl extends ASTWrapperPsiElement implements XQueryCompCommentConstructor {
-    public XQueryCompCommentConstructorPsiImpl(@NotNull ASTNode node) {
-        super(node);
-    }
-}
+/**
+ * An XQuery 1.0 `CompCommentConstructor` node in the XQuery AST.
+ */
+interface XQueryCompCommentConstructor : XQueryComputedConstructor
--- src/main/java/uk/co/reecedunn/intellij/plugin/xquery/psi/impl/xquery/XQueryCompCommentConstructorPsiImpl.kt
+++ src/main/java/uk/co/reecedunn/intellij/plugin/xquery/psi/impl/xquery/XQueryCompCommentConstructorPsiImpl.kt
@@ -13,10 +13,10 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package uk.co.reecedunn.intellij.plugin.xquery.ast.xquery;
+package uk.co.reecedunn.intellij.plugin.xquery.psi.impl.xquery
 
-/**
- * An XQuery 1.0 <code>CompCommentConstructor</code> node in the XQuery AST.
- */
-public interface XQueryCompCommentConstructor extends XQueryComputedConstructor {
-}
+import com.intellij.extapi.psi.ASTWrapperPsiElement
+import com.intellij.lang.ASTNode
+import uk.co.reecedunn.intellij.plugin.xquery.ast.xquery.XQueryCompCommentConstructor
+
+class XQueryCompCommentConstructorPsiImpl(node: ASTNode) : ASTWrapperPsiElement(node), XQueryCompCommentConstructor
