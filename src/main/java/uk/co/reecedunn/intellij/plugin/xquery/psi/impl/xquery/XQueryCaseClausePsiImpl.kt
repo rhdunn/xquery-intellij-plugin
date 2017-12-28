18d3b4f8752ccfcb4db0d04089e11f49f426ebe4
Reece H. Dunn <msclrhd@gmail.com>
2017-12-28 21:46:08 +0000

    Convert XQueryCaseClause to Kotlin.

---
+++ src/main/java/uk/co/reecedunn/intellij/plugin/xquery/psi/impl/xquery/XQueryCaseClausePsiImpl.kt
@@ -0,0 +1,34 @@
+/*
+ * Copyright (C) 2016-2017 Reece H. Dunn
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package uk.co.reecedunn.intellij.plugin.xquery.psi.impl.xquery
+
+import com.intellij.extapi.psi.ASTWrapperPsiElement
+import com.intellij.lang.ASTNode
+import com.intellij.psi.PsiElement
+import uk.co.reecedunn.intellij.plugin.xquery.ast.xquery.XQueryCaseClause
+import uk.co.reecedunn.intellij.plugin.xpath.ast.xpath.XPathEQName
+import uk.co.reecedunn.intellij.plugin.xquery.parser.XQueryElementType
+import uk.co.reecedunn.intellij.plugin.xquery.psi.XQueryVariable
+import uk.co.reecedunn.intellij.plugin.xquery.psi.XQueryVariableResolver
+
+class XQueryCaseClausePsiImpl(node: ASTNode) : ASTWrapperPsiElement(node), XQueryCaseClause, XQueryVariableResolver {
+    override fun resolveVariable(name: XPathEQName?): XQueryVariable? {
+        val element = findChildByType<PsiElement>(XQueryElementType.VAR_NAME)
+        return if (element != null && element == name) {
+            XQueryVariable(element, this)
+        } else null
+    }
+}
