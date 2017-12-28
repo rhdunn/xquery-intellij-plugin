e83ca8d29254429d85ff0c8568fc16f6cdcdfeb2
Reece H. Dunn <msclrhd@gmail.com>
2017-12-28 22:38:01 +0000

    Convert XQueryCompAttrConstructor to Kotlin.

--- src/main/java/uk/co/reecedunn/intellij/plugin/xquery/ast/xquery/XQueryCompAttrConstructor.kt
+++ src/main/java/uk/co/reecedunn/intellij/plugin/xquery/ast/xquery/XQueryCompAttrConstructor.kt
@@ -13,10 +13,9 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package uk.co.reecedunn.intellij.plugin.xquery.ast.xquery;
+package uk.co.reecedunn.intellij.plugin.xquery.ast.xquery
 
 /**
- * An XQuery 1.0 <code>CompAttrConstructor</code> node in the XQuery AST.
+ * An XQuery 1.0 `CompAttrConstructor` node in the XQuery AST.
  */
-public interface XQueryCompAttrConstructor extends XQueryComputedConstructor {
-}
+interface XQueryCompAttrConstructor : XQueryComputedConstructor
