16515d7518a4b0145862eef4f06159d728952727
Reece H. Dunn <msclrhd@gmail.com>
2017-12-28 21:44:38 +0000

    Convert XQueryBoundarySpaceDecl to Kotlin.

--- src/main/java/uk/co/reecedunn/intellij/plugin/xquery/ast/xquery/XQueryBoundarySpaceDecl.kt
+++ src/main/java/uk/co/reecedunn/intellij/plugin/xquery/ast/xquery/XQueryBoundarySpaceDecl.kt
@@ -13,10 +13,9 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package uk.co.reecedunn.intellij.plugin.xquery.ast.xquery;
+package uk.co.reecedunn.intellij.plugin.xquery.ast.xquery
 
 /**
- * An XQuery 1.0 <code>BoundarySpaceDecl</code> node in the XQuery AST.
+ * An XQuery 1.0 `BoundarySpaceDecl` node in the XQuery AST.
  */
-public interface XQueryBoundarySpaceDecl extends XQuerySetter {
-}
+interface XQueryBoundarySpaceDecl : XQuerySetter
