f9819892055278580e1bcdaf9392b776ffdbaa57
Reece H. Dunn <msclrhd@gmail.com>
2017-12-28 22:21:26 +0000

    Rename the X{Query => Path}CommentTest AST and PSI as Kotlin.

--- src/main/java/uk/co/reecedunn/intellij/plugin/xpath/ast/xpath/XPathCommentTest.kt
+++ src/main/java/uk/co/reecedunn/intellij/plugin/xpath/ast/xpath/XPathCommentTest.kt
@@ -13,10 +13,11 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package uk.co.reecedunn.intellij.plugin.xquery.ast.xquery;
+package uk.co.reecedunn.intellij.plugin.xpath.ast.xpath
+
+import uk.co.reecedunn.intellij.plugin.xquery.ast.xquery.XQueryKindTest
 
 /**
- * An XQuery 1.0 <code>CommentTest</code> node in the XQuery AST.
+ * An XPath 2.0 and XQuery 1.0 `CommentTest` node in the XQuery AST.
  */
-public interface XQueryCommentTest extends XQueryKindTest {
-}
+interface XPathCommentTest : XQueryKindTest
--- src/main/java/uk/co/reecedunn/intellij/plugin/xquery/parser/XQueryElementType.java
+++ src/main/java/uk/co/reecedunn/intellij/plugin/xquery/parser/XQueryElementType.java
@@ -129,7 +129,7 @@ public interface XQueryElementType {
     IElementType ATTRIBUTE_TEST = new ICompositeElementType("XQUERY_ATTRIBUTE_TEST", XPathAttributeTestPsiImpl.class, XQuery.INSTANCE);
     IElementType SCHEMA_ATTRIBUTE_TEST = new ICompositeElementType("XQUERY_SCHEMA_ATTRIBUTE_TEST", XQuerySchemaAttributeTestPsiImpl.class, XQuery.INSTANCE);
     IElementType PI_TEST = new ICompositeElementType("XQUERY_PI_TEST", XQueryPITestPsiImpl.class, XQuery.INSTANCE);
-    IElementType COMMENT_TEST = new ICompositeElementType("XQUERY_COMMENT_TEST", XQueryCommentTestPsiImpl.class, XQuery.INSTANCE);
+    IElementType COMMENT_TEST = new ICompositeElementType("XQUERY_COMMENT_TEST", XPathCommentTestPsiImpl.class, XQuery.INSTANCE);
     IElementType TEXT_TEST = new ICompositeElementType("XQUERY_TEXT_TEST", XQueryTextTestPsiImpl.class, XQuery.INSTANCE);
     IElementType ANY_KIND_TEST = new ICompositeElementType("XQUERY_ANY_KIND_TEST", XPathAnyKindTestPsiImpl.class, XQuery.INSTANCE);
     IElementType ITEM_TYPE = new ICompositeElementType("XQUERY_ITEM_TYPE", XQueryItemTypePsiImpl.class, XQuery.INSTANCE);
