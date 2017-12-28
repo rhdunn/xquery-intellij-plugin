deb3c7dc427191329e33864e4e370c07d5ddc22a
Reece H. Dunn <msclrhd@gmail.com>
2017-12-28 23:42:58 +0000

    Convert XQueryConstructionDecl to Kotlin.

--- src/main/java/uk/co/reecedunn/intellij/plugin/xquery/ast/xquery/XQueryConstructionDecl.kt
+++ src/main/java/uk/co/reecedunn/intellij/plugin/xquery/ast/xquery/XQueryConstructionDecl.kt
@@ -13,10 +13,9 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package uk.co.reecedunn.intellij.plugin.xquery.ast.xquery;
+package uk.co.reecedunn.intellij.plugin.xquery.ast.xquery
 
 /**
- * An XQuery 1.0 <code>ConstructionDecl</code> node in the XQuery AST.
+ * An XQuery 1.0 `ConstructionDecl` node in the XQuery AST.
  */
-public interface XQueryConstructionDecl extends XQuerySetter {
-}
+interface XQueryConstructionDecl : XQuerySetter
