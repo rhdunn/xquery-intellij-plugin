ee84e4f7e2c4e5d109a07493400c01164ab9a556
Reece H. Dunn <msclrhd@gmail.com>
2017-12-29 19:52:35 +0000

    Convert XQueryLetBinding to Kotlin.

--- src/main/java/uk/co/reecedunn/intellij/plugin/xquery/ast/xquery/XQueryLetBinding.kt
+++ src/main/java/uk/co/reecedunn/intellij/plugin/xquery/ast/xquery/XQueryLetBinding.kt
@@ -13,12 +13,11 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package uk.co.reecedunn.intellij.plugin.xquery.ast.xquery;
+package uk.co.reecedunn.intellij.plugin.xquery.ast.xquery
 
-import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiElement
 
 /**
- * An XQuery 3.0 <code>LetBinding</code> node in the XQuery AST.
+ * An XQuery 3.0 `LetBinding` node in the XQuery AST.
  */
-public interface XQueryLetBinding extends PsiElement {
-}
+interface XQueryLetBinding : PsiElement
