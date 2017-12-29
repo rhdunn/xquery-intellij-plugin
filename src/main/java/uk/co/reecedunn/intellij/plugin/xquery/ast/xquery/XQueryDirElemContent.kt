a26eb51cb740148acc8dac551cbb7b678de2e434
Reece H. Dunn <msclrhd@gmail.com>
2017-12-29 08:39:34 +0000

    Convert XQueryDirElemContent to Kotlin.

--- src/main/java/uk/co/reecedunn/intellij/plugin/xquery/ast/xquery/XQueryDirElemContent.kt
+++ src/main/java/uk/co/reecedunn/intellij/plugin/xquery/ast/xquery/XQueryDirElemContent.kt
@@ -13,15 +13,11 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package uk.co.reecedunn.intellij.plugin.xquery.psi.impl.xquery;
+package uk.co.reecedunn.intellij.plugin.xquery.ast.xquery
 
-import com.intellij.extapi.psi.ASTWrapperPsiElement;
-import com.intellij.lang.ASTNode;
-import org.jetbrains.annotations.NotNull;
-import uk.co.reecedunn.intellij.plugin.xquery.ast.xquery.XQueryDirElemContent;
+import com.intellij.psi.PsiElement
 
-public class XQueryDirElemContentPsiImpl extends ASTWrapperPsiElement implements XQueryDirElemContent {
-    public XQueryDirElemContentPsiImpl(@NotNull ASTNode node) {
-        super(node);
-    }
-}
+/**
+ * An XQuery 1.0 `DirElemContent` node in the XQuery AST.
+ */
+interface XQueryDirElemContent : PsiElement
--- src/main/java/uk/co/reecedunn/intellij/plugin/xquery/psi/impl/xquery/XQueryDirElemContentPsiImpl.kt
+++ src/main/java/uk/co/reecedunn/intellij/plugin/xquery/psi/impl/xquery/XQueryDirElemContentPsiImpl.kt
@@ -13,12 +13,10 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package uk.co.reecedunn.intellij.plugin.xquery.ast.xquery;
+package uk.co.reecedunn.intellij.plugin.xquery.psi.impl.xquery
 
-import com.intellij.psi.PsiElement;
+import com.intellij.extapi.psi.ASTWrapperPsiElement
+import com.intellij.lang.ASTNode
+import uk.co.reecedunn.intellij.plugin.xquery.ast.xquery.XQueryDirElemContent
 
-/**
- * An XQuery 1.0 <code>DirElemContent</code> node in the XQuery AST.
- */
-public interface XQueryDirElemContent extends PsiElement {
-}
+class XQueryDirElemContentPsiImpl(node: ASTNode) : ASTWrapperPsiElement(node), XQueryDirElemContent
