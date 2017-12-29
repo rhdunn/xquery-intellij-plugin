049dd3aa34a5da2c76639ba18885be679e66710a
Reece H. Dunn <msclrhd@gmail.com>
2017-12-29 12:42:09 +0000

    Rename the X{Query => Path}ForwardStep AST and PSI.

--- src/main/java/uk/co/reecedunn/intellij/plugin/xpath/psi/impl/xpath/XPathForwardStepPsiImpl.kt
+++ src/main/java/uk/co/reecedunn/intellij/plugin/xpath/psi/impl/xpath/XPathForwardStepPsiImpl.kt
@@ -13,12 +13,10 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package uk.co.reecedunn.intellij.plugin.xquery.ast.xquery;
+package uk.co.reecedunn.intellij.plugin.xpath.psi.impl.xpath
 
-import com.intellij.psi.PsiElement;
+import com.intellij.extapi.psi.ASTWrapperPsiElement
+import com.intellij.lang.ASTNode
+import uk.co.reecedunn.intellij.plugin.xpath.ast.xpath.XPathForwardStep
 
-/**
- * An XQuery 1.0 <code>ForwardStep</code> node in the XQuery AST.
- */
-public interface XQueryForwardStep extends PsiElement {
-}
+class XPathForwardStepPsiImpl(node: ASTNode) : ASTWrapperPsiElement(node), XPathForwardStep
