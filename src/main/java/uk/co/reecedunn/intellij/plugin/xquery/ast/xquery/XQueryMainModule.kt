a13c3a1afe66e83c53be6b89b57c76d948d8be11
Reece H. Dunn <msclrhd@gmail.com>
2017-12-29 17:56:57 +0000

    Convert XQueryMainModule to Kotlin.

--- src/main/java/uk/co/reecedunn/intellij/plugin/xquery/ast/xquery/XQueryMainModule.kt
+++ src/main/java/uk/co/reecedunn/intellij/plugin/xquery/ast/xquery/XQueryMainModule.kt
@@ -13,12 +13,11 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package uk.co.reecedunn.intellij.plugin.xquery.ast.xquery;
+package uk.co.reecedunn.intellij.plugin.xquery.ast.xquery
 
-import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiElement
 
 /**
- * An XQuery 1.0 <code>MainModule</code> node in the XQuery AST.
+ * An XQuery 1.0 `MainModule` node in the XQuery AST.
  */
-public interface XQueryMainModule extends PsiElement {
-}
+interface XQueryMainModule : PsiElement
