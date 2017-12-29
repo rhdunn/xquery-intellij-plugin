7cba5bfd920ade9c537dfcfad8fd1f0da249acd8
Reece H. Dunn <msclrhd@gmail.com>
2017-12-29 13:37:00 +0000

    Convert XQueryGroupByClause to Kotlin.

--- src/main/java/uk/co/reecedunn/intellij/plugin/xquery/ast/xquery/XQueryGroupByClause.kt
+++ src/main/java/uk/co/reecedunn/intellij/plugin/xquery/ast/xquery/XQueryGroupByClause.kt
@@ -13,12 +13,11 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package uk.co.reecedunn.intellij.plugin.xquery.ast.xquery;
+package uk.co.reecedunn.intellij.plugin.xquery.ast.xquery
 
-import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiElement
 
 /**
- * An XQuery 3.0 <code>GroupByClause</code> node in the XQuery AST.
+ * An XQuery 3.0 `GroupByClause` node in the XQuery AST.
  */
-public interface XQueryGroupByClause extends PsiElement {
-}
+interface XQueryGroupByClause : PsiElement
