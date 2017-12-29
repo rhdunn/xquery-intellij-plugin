1400ce942c7b97790f1800acdb40593b694fe123
Reece H. Dunn <msclrhd@gmail.com>
2017-12-29 13:48:24 +0000

    Convert XQueryInitialClause to Kotlin.

--- src/main/java/uk/co/reecedunn/intellij/plugin/xquery/ast/xquery/XQueryInitialClause.kt
+++ src/main/java/uk/co/reecedunn/intellij/plugin/xquery/ast/xquery/XQueryInitialClause.kt
@@ -13,12 +13,11 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package uk.co.reecedunn.intellij.plugin.xquery.ast.xquery;
+package uk.co.reecedunn.intellij.plugin.xquery.ast.xquery
 
-import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiElement
 
 /**
- * An XQuery 3.0 <code>InitialClause</code> node in the XQuery AST.
+ * An XQuery 3.0 `InitialClause` node in the XQuery AST.
  */
-public interface XQueryInitialClause extends PsiElement {
-}
+interface XQueryInitialClause : PsiElement
