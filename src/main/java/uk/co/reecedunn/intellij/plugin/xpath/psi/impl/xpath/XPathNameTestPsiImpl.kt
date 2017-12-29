196fa7974b0d0c6d2b7ac7f4d57f07c4a8648390
Reece H. Dunn <msclrhd@gmail.com>
2017-12-29 21:19:46 +0000

    Rename the X{Query => Path}NameTest AST and PSI as Kotlin.

--- src/main/java/uk/co/reecedunn/intellij/plugin/xpath/psi/impl/xpath/XPathNameTestPsiImpl.kt
+++ src/main/java/uk/co/reecedunn/intellij/plugin/xpath/psi/impl/xpath/XPathNameTestPsiImpl.kt
@@ -13,15 +13,10 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package uk.co.reecedunn.intellij.plugin.xquery.psi.impl.xquery;
+package uk.co.reecedunn.intellij.plugin.xpath.psi.impl.xpath
 
-import com.intellij.extapi.psi.ASTWrapperPsiElement;
-import com.intellij.lang.ASTNode;
-import org.jetbrains.annotations.NotNull;
-import uk.co.reecedunn.intellij.plugin.xquery.ast.xquery.XQueryNameTest;
+import com.intellij.extapi.psi.ASTWrapperPsiElement
+import com.intellij.lang.ASTNode
+import uk.co.reecedunn.intellij.plugin.xpath.ast.xpath.XPathNameTest
 
-public class XQueryNameTestPsiImpl extends ASTWrapperPsiElement implements XQueryNameTest {
-    public XQueryNameTestPsiImpl(@NotNull ASTNode node) {
-        super(node);
-    }
-}
+class XPathNameTestPsiImpl(node: ASTNode) : ASTWrapperPsiElement(node), XPathNameTest
