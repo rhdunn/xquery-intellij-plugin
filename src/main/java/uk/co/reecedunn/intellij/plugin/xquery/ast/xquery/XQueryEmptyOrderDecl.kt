dbd19aa4f7c34b081c3fa89586cadbc015331ef4
Reece H. Dunn <msclrhd@gmail.com>
2017-12-29 10:29:38 +0000

    Convert XQueryEmptyOrderDecl to Kotlin.

--- src/main/java/uk/co/reecedunn/intellij/plugin/xquery/ast/xquery/XQueryEmptyOrderDecl.kt
+++ src/main/java/uk/co/reecedunn/intellij/plugin/xquery/ast/xquery/XQueryEmptyOrderDecl.kt
@@ -13,10 +13,9 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package uk.co.reecedunn.intellij.plugin.xquery.ast.xquery;
+package uk.co.reecedunn.intellij.plugin.xquery.ast.xquery
 
 /**
- * An XQuery 1.0 <code>EmptyOrderDecl</code> node in the XQuery AST.
+ * An XQuery 1.0 `EmptyOrderDecl` node in the XQuery AST.
  */
-public interface XQueryEmptyOrderDecl extends XQuerySetter {
-}
+interface XQueryEmptyOrderDecl : XQuerySetter
--- src/main/java/uk/co/reecedunn/intellij/plugin/xquery/psi/impl/xquery/XQueryEmptyOrderDeclPsiImpl.kt
+++ src/main/java/uk/co/reecedunn/intellij/plugin/xquery/psi/impl/xquery/XQueryEmptyOrderDeclPsiImpl.kt
@@ -13,15 +13,10 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package uk.co.reecedunn.intellij.plugin.xquery.psi.impl.xquery;
+package uk.co.reecedunn.intellij.plugin.xquery.psi.impl.xquery
 
-import com.intellij.extapi.psi.ASTWrapperPsiElement;
-import com.intellij.lang.ASTNode;
-import org.jetbrains.annotations.NotNull;
-import uk.co.reecedunn.intellij.plugin.xquery.ast.xquery.XQueryEmptyOrderDecl;
+import com.intellij.extapi.psi.ASTWrapperPsiElement
+import com.intellij.lang.ASTNode
+import uk.co.reecedunn.intellij.plugin.xquery.ast.xquery.XQueryEmptyOrderDecl
 
-public class XQueryEmptyOrderDeclPsiImpl extends ASTWrapperPsiElement implements XQueryEmptyOrderDecl {
-    public XQueryEmptyOrderDeclPsiImpl(@NotNull ASTNode node) {
-        super(node);
-    }
-}
+class XQueryEmptyOrderDeclPsiImpl(node: ASTNode) : ASTWrapperPsiElement(node), XQueryEmptyOrderDecl
