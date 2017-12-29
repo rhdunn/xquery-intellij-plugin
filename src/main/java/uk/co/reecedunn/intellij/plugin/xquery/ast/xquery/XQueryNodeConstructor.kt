744746c4a4ff709fd5f1cb77f5e4cc7084f1f36a
Reece H. Dunn <msclrhd@gmail.com>
2017-12-29 21:22:29 +0000

    Convert XQueryNodeConstructor to Kotlin.

--- src/main/java/uk/co/reecedunn/intellij/plugin/xquery/ast/xquery/XQueryNodeConstructor.kt
+++ src/main/java/uk/co/reecedunn/intellij/plugin/xquery/ast/xquery/XQueryNodeConstructor.kt
@@ -13,15 +13,13 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package uk.co.reecedunn.intellij.plugin.xquery.psi.impl.xquery;
+package uk.co.reecedunn.intellij.plugin.xquery.ast.xquery
 
-import com.intellij.extapi.psi.ASTWrapperPsiElement;
-import com.intellij.lang.ASTNode;
-import org.jetbrains.annotations.NotNull;
-import uk.co.reecedunn.intellij.plugin.xquery.ast.xquery.XQueryNodeConstructor;
+import uk.co.reecedunn.intellij.plugin.xpath.ast.xpath.XPathPrimaryExpr
 
-public class XQueryNodeConstructorPsiImpl extends ASTWrapperPsiElement implements XQueryNodeConstructor {
-    public XQueryNodeConstructorPsiImpl(@NotNull ASTNode node) {
-        super(node);
-    }
-}
+/**
+ * An XQuery 1.0 `NodeConstructor` node in the XQuery AST.
+ *
+ * This is a replacement for the XQuery 1.0 `Constructor`.
+ */
+interface XQueryNodeConstructor : XPathPrimaryExpr
--- src/main/java/uk/co/reecedunn/intellij/plugin/xquery/psi/impl/xquery/XQueryNodeConstructorPsiImpl.kt
+++ src/main/java/uk/co/reecedunn/intellij/plugin/xquery/psi/impl/xquery/XQueryNodeConstructorPsiImpl.kt
@@ -13,14 +13,10 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package uk.co.reecedunn.intellij.plugin.xquery.ast.xquery;
+package uk.co.reecedunn.intellij.plugin.xquery.psi.impl.xquery
 
-import uk.co.reecedunn.intellij.plugin.xpath.ast.xpath.XPathPrimaryExpr;
+import com.intellij.extapi.psi.ASTWrapperPsiElement
+import com.intellij.lang.ASTNode
+import uk.co.reecedunn.intellij.plugin.xquery.ast.xquery.XQueryNodeConstructor
 
-/**
- * An XQuery 1.0 <code>NodeConstructor</code> node in the XQuery AST.
- *
- * This is a replacement for the XQuery 1.0 <code>Constructor</code>.
- */
-public interface XQueryNodeConstructor extends XPathPrimaryExpr {
-}
+class XQueryNodeConstructorPsiImpl(node: ASTNode) : ASTWrapperPsiElement(node), XQueryNodeConstructor
