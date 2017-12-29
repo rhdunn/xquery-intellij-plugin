f06a651e94980d9722a55382d5b1e3cfa0cd666f
Reece H. Dunn <msclrhd@gmail.com>
2017-12-29 22:04:32 +0000

    Convert XQueryOrderedExpr to Kotlin.

--- src/main/java/uk/co/reecedunn/intellij/plugin/xquery/ast/xquery/XQueryOrderedExpr.kt
+++ src/main/java/uk/co/reecedunn/intellij/plugin/xquery/ast/xquery/XQueryOrderedExpr.kt
@@ -13,15 +13,11 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package uk.co.reecedunn.intellij.plugin.xquery.psi.impl.xquery;
+package uk.co.reecedunn.intellij.plugin.xquery.ast.xquery
 
-import com.intellij.extapi.psi.ASTWrapperPsiElement;
-import com.intellij.lang.ASTNode;
-import org.jetbrains.annotations.NotNull;
-import uk.co.reecedunn.intellij.plugin.xquery.ast.xquery.XQueryOrderedExpr;
+import uk.co.reecedunn.intellij.plugin.xpath.ast.xpath.XPathPrimaryExpr
 
-public class XQueryOrderedExprPsiImpl extends ASTWrapperPsiElement implements XQueryOrderedExpr {
-    public XQueryOrderedExprPsiImpl(@NotNull ASTNode node) {
-        super(node);
-    }
-}
+/**
+ * An XQuery 1.0 `OrderedExpr` node in the XQuery AST.
+ */
+interface XQueryOrderedExpr : XPathPrimaryExpr
