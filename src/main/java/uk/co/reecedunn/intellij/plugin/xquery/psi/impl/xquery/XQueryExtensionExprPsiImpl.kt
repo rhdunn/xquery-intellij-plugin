fe8bcbfce18f1b1e06a872c7e6dc92b2efa01ad0
Reece H. Dunn <msclrhd@gmail.com>
2017-12-29 11:24:15 +0000

    Convert XQueryExtensionExpr to Kotlin.

--- src/main/java/uk/co/reecedunn/intellij/plugin/xquery/psi/impl/xquery/XQueryExtensionExprPsiImpl.kt
+++ src/main/java/uk/co/reecedunn/intellij/plugin/xquery/psi/impl/xquery/XQueryExtensionExprPsiImpl.kt
@@ -13,10 +13,10 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package uk.co.reecedunn.intellij.plugin.xquery.ast.xquery;
+package uk.co.reecedunn.intellij.plugin.xquery.psi.impl.xquery
 
-/**
- * An XQuery 1.0 <code>ExtensionExpr</code> node in the XQuery AST.
- */
-public interface XQueryExtensionExpr extends XQueryValueExpr {
-}
+import com.intellij.extapi.psi.ASTWrapperPsiElement
+import com.intellij.lang.ASTNode
+import uk.co.reecedunn.intellij.plugin.xquery.ast.xquery.XQueryExtensionExpr
+
+class XQueryExtensionExprPsiImpl(node: ASTNode) : ASTWrapperPsiElement(node), XQueryExtensionExpr
