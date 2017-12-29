599b87da0ee0cb2319e797fbadf8c4c0af069421
Reece H. Dunn <msclrhd@gmail.com>
2017-12-29 08:21:37 +0000

    Convert XQueryCountClause to Kotlin.

--- src/main/java/uk/co/reecedunn/intellij/plugin/xquery/ast/xquery/XQueryCountClause.kt
+++ src/main/java/uk/co/reecedunn/intellij/plugin/xquery/ast/xquery/XQueryCountClause.kt
@@ -13,12 +13,11 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package uk.co.reecedunn.intellij.plugin.xquery.ast.xquery;
+package uk.co.reecedunn.intellij.plugin.xquery.ast.xquery
 
-import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiElement
 
 /**
- * An XQuery 3.0 <code>CountClause</code> node in the XQuery AST.
+ * An XQuery 3.0 `CountClause` node in the XQuery AST.
  */
-public interface XQueryCountClause extends PsiElement {
-}
+interface XQueryCountClause : PsiElement
