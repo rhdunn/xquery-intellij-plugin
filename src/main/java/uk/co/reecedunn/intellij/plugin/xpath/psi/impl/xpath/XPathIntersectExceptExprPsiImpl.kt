aed63db289b37e9dc1ae50fd3abc8123e564deb3
Reece H. Dunn <msclrhd@gmail.com>
2017-12-29 14:30:59 +0000

    Rename the X{Query => Path}IntersectExceptExpr AST and PSI as Kotlin.

--- src/main/java/uk/co/reecedunn/intellij/plugin/xpath/psi/impl/xpath/XPathIntersectExceptExprPsiImpl.kt
+++ src/main/java/uk/co/reecedunn/intellij/plugin/xpath/psi/impl/xpath/XPathIntersectExceptExprPsiImpl.kt
@@ -13,12 +13,10 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package uk.co.reecedunn.intellij.plugin.xquery.ast.xquery;
+package uk.co.reecedunn.intellij.plugin.xpath.psi.impl.xpath
 
-import com.intellij.psi.PsiElement;
+import com.intellij.extapi.psi.ASTWrapperPsiElement
+import com.intellij.lang.ASTNode
+import uk.co.reecedunn.intellij.plugin.xpath.ast.xpath.XPathIntersectExceptExpr
 
-/**
- * An XQuery 1.0 <code>IntersectExcept</code> node in the XQuery AST.
- */
-public interface XQueryIntersectExceptExpr extends PsiElement {
-}
+class XPathIntersectExceptExprPsiImpl(node: ASTNode) : ASTWrapperPsiElement(node), XPathIntersectExceptExpr
