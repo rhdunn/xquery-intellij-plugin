85905449247a5de44103286ad92c420ff05b038d
Reece H. Dunn <msclrhd@gmail.com>
2017-12-29 08:40:43 +0000

    Convert XQueryDirPIConstructor to Kotlin.

--- src/main/java/uk/co/reecedunn/intellij/plugin/xquery/ast/xquery/XQueryDirPIConstructor.kt
+++ src/main/java/uk/co/reecedunn/intellij/plugin/xquery/ast/xquery/XQueryDirPIConstructor.kt
@@ -13,10 +13,9 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package uk.co.reecedunn.intellij.plugin.xquery.ast.xquery;
+package uk.co.reecedunn.intellij.plugin.xquery.ast.xquery
 
 /**
- * An XQuery 1.0 <code>DirPIConstructor</code> node in the XQuery AST.
+ * An XQuery 1.0 `DirPIConstructor` node in the XQuery AST.
  */
-public interface XQueryDirPIConstructor extends XQueryDirectConstructor {
-}
+interface XQueryDirPIConstructor : XQueryDirectConstructor
--- src/main/java/uk/co/reecedunn/intellij/plugin/xquery/psi/impl/xquery/XQueryDirPIConstructorPsiImpl.kt
+++ src/main/java/uk/co/reecedunn/intellij/plugin/xquery/psi/impl/xquery/XQueryDirPIConstructorPsiImpl.kt
@@ -13,15 +13,10 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package uk.co.reecedunn.intellij.plugin.xquery.psi.impl.xquery;
+package uk.co.reecedunn.intellij.plugin.xquery.psi.impl.xquery
 
-import com.intellij.extapi.psi.ASTWrapperPsiElement;
-import com.intellij.lang.ASTNode;
-import org.jetbrains.annotations.NotNull;
-import uk.co.reecedunn.intellij.plugin.xquery.ast.xquery.XQueryDirPIConstructor;
+import com.intellij.extapi.psi.ASTWrapperPsiElement
+import com.intellij.lang.ASTNode
+import uk.co.reecedunn.intellij.plugin.xquery.ast.xquery.XQueryDirPIConstructor
 
-public class XQueryDirPIConstructorPsiImpl extends ASTWrapperPsiElement implements XQueryDirPIConstructor {
-    public XQueryDirPIConstructorPsiImpl(@NotNull ASTNode node) {
-        super(node);
-    }
-}
+class XQueryDirPIConstructorPsiImpl(node: ASTNode) : ASTWrapperPsiElement(node), XQueryDirPIConstructor
