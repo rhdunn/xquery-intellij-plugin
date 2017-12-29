d1535a167ced666fb42844ea6aac4798ef0d5d11
Reece H. Dunn <msclrhd@gmail.com>
2017-12-29 08:37:17 +0000

    Convert XQueryDirCommentConstructor to Kotlin.

--- src/main/java/uk/co/reecedunn/intellij/plugin/xquery/ast/xquery/XQueryDirCommentConstructor.kt
+++ src/main/java/uk/co/reecedunn/intellij/plugin/xquery/ast/xquery/XQueryDirCommentConstructor.kt
@@ -13,10 +13,9 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package uk.co.reecedunn.intellij.plugin.xquery.ast.xquery;
+package uk.co.reecedunn.intellij.plugin.xquery.ast.xquery
 
 /**
- * An XQuery 1.0 <code>DirCommentConstructor</code> node in the XQuery AST.
+ * An XQuery 1.0 `DirCommentConstructor` node in the XQuery AST.
  */
-public interface XQueryDirCommentConstructor extends XQueryDirectConstructor {
-}
+interface XQueryDirCommentConstructor : XQueryDirectConstructor
