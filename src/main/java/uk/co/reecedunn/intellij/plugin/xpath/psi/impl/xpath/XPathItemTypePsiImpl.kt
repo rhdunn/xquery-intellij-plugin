a02ae1193fd0d4c6441ce2c6681345ce62476746
Reece H. Dunn <msclrhd@gmail.com>
2017-12-29 19:29:21 +0000

    Rename the X{Query => Path}ItemType AST and PSI as Kotlin.

--- src/main/java/uk/co/reecedunn/intellij/plugin/xpath/psi/impl/xpath/XPathItemTypePsiImpl.kt
+++ src/main/java/uk/co/reecedunn/intellij/plugin/xpath/psi/impl/xpath/XPathItemTypePsiImpl.kt
@@ -13,12 +13,10 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package uk.co.reecedunn.intellij.plugin.xquery.ast.xquery;
+package uk.co.reecedunn.intellij.plugin.xpath.psi.impl.xpath
 
-import com.intellij.psi.PsiElement;
+import com.intellij.extapi.psi.ASTWrapperPsiElement
+import com.intellij.lang.ASTNode
+import uk.co.reecedunn.intellij.plugin.xpath.ast.xpath.XPathItemType
 
-/**
- * An XQuery 1.0 <code>ItemType</code> node in the XQuery AST.
- */
-public interface XQueryItemType extends PsiElement {
-}
+class XPathItemTypePsiImpl(node: ASTNode) : ASTWrapperPsiElement(node), XPathItemType
--- src/main/java/uk/co/reecedunn/intellij/plugin/xquery/parser/XQueryElementType.java
+++ src/main/java/uk/co/reecedunn/intellij/plugin/xquery/parser/XQueryElementType.java
@@ -132,7 +132,7 @@ public interface XQueryElementType {
     IElementType COMMENT_TEST = new ICompositeElementType("XQUERY_COMMENT_TEST", XPathCommentTestPsiImpl.class, XQuery.INSTANCE);
     IElementType TEXT_TEST = new ICompositeElementType("XQUERY_TEXT_TEST", XQueryTextTestPsiImpl.class, XQuery.INSTANCE);
     IElementType ANY_KIND_TEST = new ICompositeElementType("XQUERY_ANY_KIND_TEST", XPathAnyKindTestPsiImpl.class, XQuery.INSTANCE);
-    IElementType ITEM_TYPE = new ICompositeElementType("XQUERY_ITEM_TYPE", XQueryItemTypePsiImpl.class, XQuery.INSTANCE);
+    IElementType ITEM_TYPE = new ICompositeElementType("XQUERY_ITEM_TYPE", XPathItemTypePsiImpl.class, XQuery.INSTANCE);
     IElementType OCCURRENCE_INDICATOR = new ICompositeElementType("XQUERY_OCCURRENCE_INDICATOR", XQueryOccurrenceIndicatorPsiImpl.class, XQuery.INSTANCE);
     IElementType TYPE_DECLARATION = new ICompositeElementType("XQUERY_TYPE_DECLARATION", XQueryTypeDeclarationPsiImpl.class, XQuery.INSTANCE);
     IElementType SEQUENCE_TYPE = new ICompositeElementType("XQUERY_SEQUENCE_TYPE", XQuerySequenceTypePsiImpl.class, XQuery.INSTANCE);
