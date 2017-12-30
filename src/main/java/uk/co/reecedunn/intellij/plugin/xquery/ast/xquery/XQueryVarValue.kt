953d7dde8a601ec92f45168ae1e361b283449dae
Reece H. Dunn <msclrhd@gmail.com>
2017-12-30 14:14:30 +0000

    Convert XQueryVarValue to Kotlin.

--- src/main/java/uk/co/reecedunn/intellij/plugin/xquery/ast/xquery/XQueryVarValue.kt
+++ src/main/java/uk/co/reecedunn/intellij/plugin/xquery/ast/xquery/XQueryVarValue.kt
@@ -13,15 +13,11 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package uk.co.reecedunn.intellij.plugin.xquery.psi.impl.xquery;
+package uk.co.reecedunn.intellij.plugin.xquery.ast.xquery
 
-import com.intellij.extapi.psi.ASTWrapperPsiElement;
-import com.intellij.lang.ASTNode;
-import org.jetbrains.annotations.NotNull;
-import uk.co.reecedunn.intellij.plugin.xquery.ast.xquery.XQueryVarValue;
+import com.intellij.psi.PsiElement
 
-public class XQueryVarValuePsiImpl extends ASTWrapperPsiElement implements XQueryVarValue {
-    public XQueryVarValuePsiImpl(@NotNull ASTNode node) {
-        super(node);
-    }
-}
+/**
+ * An XQuery 3.0 `VarValue` node in the XQuery AST.
+ */
+interface XQueryVarValue : PsiElement
