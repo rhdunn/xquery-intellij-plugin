8b4df80064b0066a80535804d1a0bf80741228ab
Reece H. Dunn <msclrhd@gmail.com>
2017-12-30 10:00:50 +0000

    Convert XQuerySchemaPrefix to Kotlin.

--- src/main/java/uk/co/reecedunn/intellij/plugin/xquery/psi/impl/xquery/XQuerySchemaPrefixPsiImpl.kt
+++ src/main/java/uk/co/reecedunn/intellij/plugin/xquery/psi/impl/xquery/XQuerySchemaPrefixPsiImpl.kt
@@ -13,12 +13,10 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package uk.co.reecedunn.intellij.plugin.xquery.ast.xquery;
+package uk.co.reecedunn.intellij.plugin.xquery.psi.impl.xquery
 
-import com.intellij.psi.PsiElement;
+import com.intellij.extapi.psi.ASTWrapperPsiElement
+import com.intellij.lang.ASTNode
+import uk.co.reecedunn.intellij.plugin.xquery.ast.xquery.XQuerySchemaPrefix
 
-/**
- * An XQuery 1.0 <code>SchemaPrefix</code> node in the XQuery AST.
- */
-public interface XQuerySchemaPrefix extends PsiElement {
-}
+class XQuerySchemaPrefixPsiImpl(node: ASTNode) : ASTWrapperPsiElement(node), XQuerySchemaPrefix
