bbf24dfbe6cbda980417a2aadca985d6c267ed80
Reece H. Dunn <msclrhd@gmail.com>
2017-12-30 11:10:33 +0000

    Convert XQueryStringConstructorContent to Kotlin.

--- src/main/java/uk/co/reecedunn/intellij/plugin/xquery/ast/xquery/XQueryStringConstructorContent.kt
+++ src/main/java/uk/co/reecedunn/intellij/plugin/xquery/ast/xquery/XQueryStringConstructorContent.kt
@@ -13,12 +13,11 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package uk.co.reecedunn.intellij.plugin.xquery.ast.xquery;
+package uk.co.reecedunn.intellij.plugin.xquery.ast.xquery
 
-import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiElement
 
 /**
- * An XQuery 3.1 <code>StringConstructorContent</code> node in the XQuery AST.
+ * An XQuery 3.1 `StringConstructorContent` node in the XQuery AST.
  */
-public interface XQueryStringConstructorContent extends PsiElement {
-}
+interface XQueryStringConstructorContent : PsiElement
