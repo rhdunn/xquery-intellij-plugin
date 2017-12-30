d0f4be73f3d1b2602ab612993fffb79ae6fc2953
Reece H. Dunn <msclrhd@gmail.com>
2017-12-30 08:49:21 +0000

    Convert XQueryPositionalVar to Kotlin.

--- src/main/java/uk/co/reecedunn/intellij/plugin/xquery/ast/xquery/XQueryPositionalVar.kt
+++ src/main/java/uk/co/reecedunn/intellij/plugin/xquery/ast/xquery/XQueryPositionalVar.kt
@@ -13,12 +13,11 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package uk.co.reecedunn.intellij.plugin.xquery.ast.xquery;
+package uk.co.reecedunn.intellij.plugin.xquery.ast.xquery
 
-import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiElement
 
 /**
- * An XQuery 1.0 <code>PositionalVar</code> node in the XQuery AST.
+ * An XQuery 1.0 `PositionalVar` node in the XQuery AST.
  */
-public interface XQueryPositionalVar extends PsiElement {
-}
+interface XQueryPositionalVar : PsiElement
---
+++ src/main/java/uk/co/reecedunn/intellij/plugin/xquery/psi/impl/xquery/XQueryPositionalVarPsiImpl.kt
@@ -0,0 +1,33 @@
+/*
+ * Copyright (C) 2016-2017 Reece H. Dunn
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package uk.co.reecedunn.intellij.plugin.xquery.psi.impl.xquery
+
+import com.intellij.extapi.psi.ASTWrapperPsiElement
+import com.intellij.lang.ASTNode
+import com.intellij.psi.PsiElement
+import uk.co.reecedunn.intellij.plugin.xpath.ast.xpath.XPathEQName
+import uk.co.reecedunn.intellij.plugin.xquery.ast.xquery.XQueryPositionalVar
+import uk.co.reecedunn.intellij.plugin.xquery.psi.XQueryVariable
+import uk.co.reecedunn.intellij.plugin.xquery.psi.XQueryVariableResolver
+
+class XQueryPositionalVarPsiImpl(node: ASTNode) : ASTWrapperPsiElement(node), XQueryPositionalVar, XQueryVariableResolver {
+    override fun resolveVariable(name: XPathEQName?): XQueryVariable? {
+        val eqname = findChildByClass(XPathEQName::class.java)
+        return if (eqname != null && eqname == name) {
+            XQueryVariable(eqname, this)
+        } else null
+    }
+}
