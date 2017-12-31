bef8fcad00b39b2d6c7bd9f0ba93f9f61c5d06b5
Reece H. Dunn <msclrhd@gmail.com>
2017-12-31 16:11:57 +0000

    Use XdmConstantExpression to implement the UndefinedFunctionInspection.

--- src/main/java/uk/co/reecedunn/intellij/plugin/xquery/inspections/xpath/XPST0017/UndefinedFunctionInspection.kt
+++ src/main/java/uk/co/reecedunn/intellij/plugin/xquery/inspections/xpath/XPST0017/UndefinedFunctionInspection.kt
@@ -16,9 +16,12 @@
 package uk.co.reecedunn.intellij.plugin.xquery.inspections.xpath.XPST0017
 
 import com.intellij.codeInspection.*
+import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiFile
 import com.intellij.util.SmartList
 import uk.co.reecedunn.intellij.plugin.core.sequences.walkTree
+import uk.co.reecedunn.intellij.plugin.xdm.datatype.QName
+import uk.co.reecedunn.intellij.plugin.xdm.model.XdmConstantExpression
 import uk.co.reecedunn.intellij.plugin.xpath.ast.xpath.XPathArrowFunctionSpecifier
 import uk.co.reecedunn.intellij.plugin.xpath.ast.xpath.XPathEQName
 import uk.co.reecedunn.intellij.plugin.xpath.ast.xpath.XPathFunctionCall
@@ -46,13 +49,14 @@ class UndefinedFunctionInspection : LocalInspectionTool() {
                        .filter { qname -> qname.type == XPathEQName.Type.Function }
                        .forEach { qname ->
             val declarations = qname.resolveFunctionDecls().toList()
-            val context = qname.localName
+            val context = (qname as? XdmConstantExpression)?.constantValue as? QName
             if (context == null) {
                 // Missing local name -- do nothing.
             } else if (declarations.isEmpty()) {
                 // 1. The expanded QName does not match the name of a function signature in the static context.
                 val description = XQueryBundle.message("inspection.XPST0017.undefined-function.unresolved-qname")
-                descriptors.add(manager.createProblemDescriptor(context, description, null as LocalQuickFix?, ProblemHighlightType.GENERIC_ERROR, isOnTheFly))
+                val decl = context.declaration?.get()!! as PsiElement
+                descriptors.add(manager.createProblemDescriptor(decl, description, null as LocalQuickFix?, ProblemHighlightType.GENERIC_ERROR, isOnTheFly))
             } else {
                 // 2. The number of arguments does not match the arity of a function signature in the static context.
                 val parent = qname.parent
@@ -64,7 +68,8 @@ class UndefinedFunctionInspection : LocalInspectionTool() {
                 }
                 if (declarations.firstOrNull { f -> f.arity == arity } == null) {
                     val description = XQueryBundle.message("inspection.XPST0017.undefined-function.unresolved-arity")
-                    descriptors.add(manager.createProblemDescriptor(context, description, null as LocalQuickFix?, ProblemHighlightType.GENERIC_ERROR, isOnTheFly))
+                    val decl = context.declaration?.get()!! as PsiElement
+                    descriptors.add(manager.createProblemDescriptor(decl, description, null as LocalQuickFix?, ProblemHighlightType.GENERIC_ERROR, isOnTheFly))
                 }
             }
         }
