b8148e1e30a1c23baf42b58a17027b1f54917d7d
Reece H. Dunn <msclrhd@gmail.com>
2017-12-31 14:44:48 +0000

    Convert QNameAnnotator to Kotlin.

---
+++ src/main/java/uk/co/reecedunn/intellij/plugin/xquery/annotation/QNameAnnotator.kt
@@ -0,0 +1,76 @@
+/*
+ * Copyright (C) 2016 Reece H. Dunn
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package uk.co.reecedunn.intellij.plugin.xquery.annotation
+
+import com.intellij.lang.annotation.AnnotationHolder
+import com.intellij.lang.annotation.Annotator
+import com.intellij.openapi.editor.markup.TextAttributes
+import com.intellij.psi.PsiElement
+import uk.co.reecedunn.intellij.plugin.xpath.ast.xpath.XPathBracedURILiteral
+import uk.co.reecedunn.intellij.plugin.xpath.ast.xpath.XPathEQName
+import uk.co.reecedunn.intellij.plugin.xpath.ast.xpath.XPathNCName
+import uk.co.reecedunn.intellij.plugin.xquery.ast.xquery.XQueryAnnotation
+import uk.co.reecedunn.intellij.plugin.xquery.ast.xquery.XQueryDirAttributeList
+import uk.co.reecedunn.intellij.plugin.xquery.ast.xquery.XQueryDirElemConstructor
+import uk.co.reecedunn.intellij.plugin.xquery.lexer.IXQueryKeywordOrNCNameType
+import uk.co.reecedunn.intellij.plugin.xquery.lexer.SyntaxHighlighter
+
+class QNameAnnotator : Annotator {
+    override fun annotate(element: PsiElement, holder: AnnotationHolder) {
+        if (element !is XPathEQName) return
+        if (element.getParent() is XPathEQName) return
+
+        val xmlns: Boolean
+        val prefix = element.prefix
+        if (prefix != null && prefix !is XPathBracedURILiteral) {
+            if (prefix.text == "xmlns") {
+                xmlns = true
+            } else {
+                xmlns = false
+                holder.createInfoAnnotation(prefix, null).enforcedTextAttributes = TextAttributes.ERASE_MARKER
+                if (element.getParent() is XQueryDirAttributeList || element.getParent() is XQueryDirElemConstructor) {
+                    holder.createInfoAnnotation(prefix, null).textAttributes = SyntaxHighlighter.XML_TAG
+                }
+                holder.createInfoAnnotation(prefix, null).textAttributes = SyntaxHighlighter.NS_PREFIX
+            }
+        } else {
+            xmlns = false
+        }
+
+        val localName = element.localName
+        if (localName != null) {
+            if (xmlns) {
+                holder.createInfoAnnotation(localName, null).enforcedTextAttributes = TextAttributes.ERASE_MARKER
+                if (element.getParent() is XQueryDirAttributeList) {
+                    holder.createInfoAnnotation(localName, null).textAttributes = SyntaxHighlighter.XML_TAG
+                }
+                holder.createInfoAnnotation(localName, null).textAttributes = SyntaxHighlighter.NS_PREFIX
+            } else if (element.parent is XQueryAnnotation) {
+                holder.createInfoAnnotation(localName, null).enforcedTextAttributes = TextAttributes.ERASE_MARKER
+                holder.createInfoAnnotation(localName, null).textAttributes = SyntaxHighlighter.ANNOTATION
+            } else if (localName.node.elementType is IXQueryKeywordOrNCNameType) {
+                holder.createInfoAnnotation(localName, null).enforcedTextAttributes = TextAttributes.ERASE_MARKER
+                holder.createInfoAnnotation(localName, null).textAttributes = SyntaxHighlighter.IDENTIFIER
+            } else if (localName is XPathNCName) {
+                val ncname = localName.localName
+                if (ncname != null && ncname.node.elementType is IXQueryKeywordOrNCNameType) {
+                    holder.createInfoAnnotation(localName, null).enforcedTextAttributes = TextAttributes.ERASE_MARKER
+                    holder.createInfoAnnotation(localName, null).textAttributes = SyntaxHighlighter.IDENTIFIER
+                }
+            }
+        }
+    }
+}
