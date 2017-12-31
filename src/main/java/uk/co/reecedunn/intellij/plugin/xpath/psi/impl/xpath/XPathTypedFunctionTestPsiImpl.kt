f1cbba16461af8444b5e2835494df0cf8c45c928
Reece H. Dunn <msclrhd@gmail.com>
2017-12-31 10:07:02 +0000

    Move {xquery => xpath}.psi.impl.{xquery => xpath}.XPathTypedFunctionTestPsiImpl.kt

--- src/main/java/uk/co/reecedunn/intellij/plugin/xpath/psi/impl/xpath/XPathTypedFunctionTestPsiImpl.kt
+++ src/main/java/uk/co/reecedunn/intellij/plugin/xpath/psi/impl/xpath/XPathTypedFunctionTestPsiImpl.kt
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package uk.co.reecedunn.intellij.plugin.xquery.psi.impl.xquery
+package uk.co.reecedunn.intellij.plugin.xpath.psi.impl.xpath
 
 import com.intellij.extapi.psi.ASTWrapperPsiElement
 import com.intellij.lang.ASTNode
