9993f404f5aee06b6f7127d2886b2ffbe3588bb3
Reece H. Dunn <msclrhd@gmail.com>
2018-01-03 22:23:26 +0000

    Implement XdmNamespaceDeclaration for SchemaImport elements.

--- src/main/java/uk/co/reecedunn/intellij/plugin/xquery/psi/impl/xquery/XQuerySchemaImportPsiImpl.kt
+++ src/main/java/uk/co/reecedunn/intellij/plugin/xquery/psi/impl/xquery/XQuerySchemaImportPsiImpl.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2016-2017 Reece H. Dunn
+ * Copyright (C) 2016-2018 Reece H. Dunn
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -19,14 +19,34 @@ import com.intellij.extapi.psi.ASTWrapperPsiElement
 import com.intellij.lang.ASTNode
 import com.intellij.psi.PsiElement
 import uk.co.reecedunn.intellij.plugin.core.sequences.children
+import uk.co.reecedunn.intellij.plugin.xdm.model.XdmLexicalValue
+import uk.co.reecedunn.intellij.plugin.xdm.model.XdmNamespaceDeclaration
 import uk.co.reecedunn.intellij.plugin.xpath.ast.xpath.XPathNCName
 import uk.co.reecedunn.intellij.plugin.xquery.ast.xquery.XQuerySchemaImport
 import uk.co.reecedunn.intellij.plugin.xquery.ast.xquery.XQuerySchemaPrefix
+import uk.co.reecedunn.intellij.plugin.xquery.ast.xquery.XQueryUriLiteral
 import uk.co.reecedunn.intellij.plugin.xquery.parser.XQueryElementType
 import uk.co.reecedunn.intellij.plugin.xquery.psi.XQueryNamespace
 import uk.co.reecedunn.intellij.plugin.xquery.psi.XQueryNamespaceResolver
 
-class XQuerySchemaImportPsiImpl(node: ASTNode) : ASTWrapperPsiElement(node), XQuerySchemaImport, XQueryNamespaceResolver {
+class XQuerySchemaImportPsiImpl(node: ASTNode):
+        ASTWrapperPsiElement(node),
+        XQuerySchemaImport,
+        XQueryNamespaceResolver,
+        XdmNamespaceDeclaration {
+    // region XdmNamespaceDeclaration
+
+    override val namespacePrefix get(): XdmLexicalValue? {
+        val schema = children().filterIsInstance<XQuerySchemaPrefix>().firstOrNull() ?: return null
+        return schema.children().filterIsInstance<XPathNCName>().firstOrNull()?.localName as? XdmLexicalValue
+    }
+
+    override val namespaceUri get(): XdmLexicalValue? =
+        children().filterIsInstance<XQueryUriLiteral>().firstOrNull() as? XdmLexicalValue
+
+    // endregion
+    // region XQuerySchemaImport
+
     override val namespace get(): XQueryNamespace? {
         val schema = findChildByType<XQuerySchemaPrefix>(XQueryElementType.SCHEMA_PREFIX) ?: return null
         return schema.children().filterIsInstance<XPathNCName>().map { name -> name.localName }.map { localName ->
@@ -35,8 +55,13 @@ class XQuerySchemaImportPsiImpl(node: ASTNode) : ASTWrapperPsiElement(node), XQu
         }.firstOrNull()
     }
 
+    // endregion
+    // region XQueryNamespaceResolver
+
     override fun resolveNamespace(prefix: CharSequence?): XQueryNamespace? {
         val ns = namespace
         return if (ns?.prefix?.text == prefix) ns else null
     }
+
+    // endregion
 }
--- src/test/java/uk/co/reecedunn/intellij/plugin/xquery/tests/xdm/XQueryXdmTest.kt
+++ src/test/java/uk/co/reecedunn/intellij/plugin/xquery/tests/xdm/XQueryXdmTest.kt
@@ -390,6 +390,45 @@ class XQueryXdmTest : ParserTestCase() {
     }
 
     // endregion
+    // region SchemaImport (XdmNamespaceDeclaration)
+
+    fun testSchemaImport() {
+        val expr = parse<XQuerySchemaImport>("import schema namespace test = 'http://www.example.com';")[0] as XdmNamespaceDeclaration
+
+        assertThat(expr.namespacePrefix, `is`(notNullValue()))
+        assertThat(expr.namespacePrefix?.cacheable, `is`(CachingBehaviour.Cache))
+        assertThat(expr.namespacePrefix?.staticType, `is`(XsNCName as XdmSequenceType))
+        assertThat(expr.namespacePrefix?.lexicalRepresentation, `is`("test"))
+
+        assertThat(expr.namespaceUri, `is`(notNullValue()))
+        assertThat(expr.namespaceUri?.cacheable, `is`(CachingBehaviour.Cache))
+        assertThat(expr.namespaceUri?.staticType, `is`(XsAnyURI as XdmSequenceType))
+        assertThat(expr.namespaceUri?.lexicalRepresentation, `is`("http://www.example.com"))
+    }
+
+    fun testSchemaImport_NoNamespacePrefix() {
+        val expr = parse<XQuerySchemaImport>("import schema namespace = 'http://www.example.com';")[0] as XdmNamespaceDeclaration
+
+        assertThat(expr.namespacePrefix, `is`(nullValue()))
+
+        assertThat(expr.namespaceUri, `is`(notNullValue()))
+        assertThat(expr.namespaceUri?.cacheable, `is`(CachingBehaviour.Cache))
+        assertThat(expr.namespaceUri?.staticType, `is`(XsAnyURI as XdmSequenceType))
+        assertThat(expr.namespaceUri?.lexicalRepresentation, `is`("http://www.example.com"))
+    }
+
+    fun testSchemaImport_NoNamespaceUri() {
+        val expr = parse<XQuerySchemaImport>("import schema namespace test = ;")[0] as XdmNamespaceDeclaration
+
+        assertThat(expr.namespacePrefix, `is`(notNullValue()))
+        assertThat(expr.namespacePrefix?.cacheable, `is`(CachingBehaviour.Cache))
+        assertThat(expr.namespacePrefix?.staticType, `is`(XsNCName as XdmSequenceType))
+        assertThat(expr.namespacePrefix?.lexicalRepresentation, `is`("test"))
+
+        assertThat(expr.namespaceUri, `is`(nullValue()))
+    }
+
+    // endregion
     // endregion
     // region Variables
     // region CaseClause (XdmVariableDeclaration)
