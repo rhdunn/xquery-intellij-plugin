83dcdbeedd09ddcc3830c4f78703234a734dbcc7
Reece H. Dunn <msclrhd@gmail.com>
2018-01-26 07:50:47 +0000

    Convert XQueryFindUsagesProvider to Kotlin.

---
+++ src/main/java/uk/co/reecedunn/intellij/plugin/xquery/findUsages/XQueryFindUsagesProvider.kt
@@ -0,0 +1,51 @@
+/*
+ * Copyright (C) 2016 Reece H. Dunn
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package uk.co.reecedunn.intellij.plugin.xquery.findUsages
+
+import com.intellij.lang.HelpID
+import com.intellij.lang.cacheBuilder.WordsScanner
+import com.intellij.lang.findUsages.FindUsagesProvider
+import com.intellij.psi.PsiElement
+import com.intellij.psi.PsiNamedElement
+import uk.co.reecedunn.intellij.plugin.xquery.resources.XQueryBundle
+
+class XQueryFindUsagesProvider : FindUsagesProvider {
+    override fun getWordsScanner(): WordsScanner? {
+        return XQueryWordsScanner()
+    }
+
+    override fun canFindUsagesFor(psiElement: PsiElement): Boolean {
+        return psiElement is PsiNamedElement
+    }
+
+    override fun getHelpId(psiElement: PsiElement): String? {
+        return HelpID.FIND_OTHER_USAGES
+    }
+
+    override fun getType(element: PsiElement): String {
+        // TODO: Determine the correct identifier type (namespace, function, variable, element, attribute, data type).
+        return XQueryBundle.message("find-usages.identifier")
+    }
+
+    override fun getDescriptiveName(element: PsiElement): String {
+        val name = (element as PsiNamedElement).name
+        return name ?: ""
+    }
+
+    override fun getNodeText(element: PsiElement, useFullName: Boolean): String {
+        return getDescriptiveName(element)
+    }
+}
