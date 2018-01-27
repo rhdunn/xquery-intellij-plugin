0752c7316b5d12ac95b9ffe666d4422dca2f4ad6
Reece H. Dunn <msclrhd@gmail.com>
2018-01-27 17:17:30 +0000

    Convert BaseXParserTest to Kotlin.

---
+++ src/test/java/uk/co/reecedunn/intellij/plugin/xquery/tests/parser/BaseXParserTest.kt
@@ -0,0 +1,85 @@
+/*
+ * Copyright (C) 2017 Reece H. Dunn
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package uk.co.reecedunn.intellij.plugin.xquery.tests.parser
+
+import org.hamcrest.CoreMatchers.`is`
+import org.hamcrest.MatcherAssert.assertThat
+
+class BaseXParserTest : ParserTestCase() {
+    // region BaseX 6.1 :: FTFuzzyOption
+
+    fun testFTFuzzyOption() {
+        val expected = loadResource("tests/parser/basex-6.1/FTFuzzyOption.txt")
+        val actual = parseResource("tests/parser/basex-6.1/FTFuzzyOption.xq")
+        assertThat(prettyPrintASTNode(actual), `is`(expected))
+    }
+
+    fun testFTFuzzyOption_MissingUsingKeyword() {
+        val expected = loadResource("tests/parser/basex-6.1/FTFuzzyOption_MissingUsingKeyword.txt")
+        val actual = parseResource("tests/parser/basex-6.1/FTFuzzyOption_MissingUsingKeyword.xq")
+        assertThat(prettyPrintASTNode(actual), `is`(expected))
+    }
+
+    // endregion
+    // region BaseX 7.8 :: UpdateExpr
+
+    fun testUpdateExpr() {
+        val expected = loadResource("tests/parser/basex-7.8/UpdateExpr.txt")
+        val actual = parseResource("tests/parser/basex-7.8/UpdateExpr.xq")
+        assertThat(prettyPrintASTNode(actual), `is`(expected))
+    }
+
+    fun testUpdateExpr_MissingExpr() {
+        val expected = loadResource("tests/parser/basex-7.8/UpdateExpr_MissingExpr.txt")
+        val actual = parseResource("tests/parser/basex-7.8/UpdateExpr_MissingExpr.xq")
+        assertThat(prettyPrintASTNode(actual), `is`(expected))
+    }
+
+    fun testUpdateExpr_Multiple() {
+        val expected = loadResource("tests/parser/basex-7.8/UpdateExpr_Multiple.txt")
+        val actual = parseResource("tests/parser/basex-7.8/UpdateExpr_Multiple.xq")
+        assertThat(prettyPrintASTNode(actual), `is`(expected))
+    }
+
+    // endregion
+    // region BaseX 8.5 :: UpdateExpr
+
+    fun testUpdateExpr_Block() {
+        val expected = loadResource("tests/parser/basex-8.5/UpdateExpr.txt")
+        val actual = parseResource("tests/parser/basex-8.5/UpdateExpr.xq")
+        assertThat(prettyPrintASTNode(actual), `is`(expected))
+    }
+
+    fun testUpdateExpr_Block_MissingExpr() {
+        val expected = loadResource("tests/parser/basex-8.5/UpdateExpr_MissingExpr.txt")
+        val actual = parseResource("tests/parser/basex-8.5/UpdateExpr_MissingExpr.xq")
+        assertThat(prettyPrintASTNode(actual), `is`(expected))
+    }
+
+    fun testUpdateExpr_Block_MissingClosingBrace() {
+        val expected = loadResource("tests/parser/basex-8.5/UpdateExpr_MissingClosingBrace.txt")
+        val actual = parseResource("tests/parser/basex-8.5/UpdateExpr_MissingClosingBrace.xq")
+        assertThat(prettyPrintASTNode(actual), `is`(expected))
+    }
+
+    fun testUpdateExpr_Block_Multiple() {
+        val expected = loadResource("tests/parser/basex-8.5/UpdateExpr_Multiple.txt")
+        val actual = parseResource("tests/parser/basex-8.5/UpdateExpr_Multiple.xq")
+        assertThat(prettyPrintASTNode(actual), `is`(expected))
+    }
+
+    // endregion
+}
