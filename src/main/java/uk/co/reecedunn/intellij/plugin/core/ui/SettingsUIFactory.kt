e939d04829b25fd11ab48901ec0f5f8e9f0ec38d
Reece H. Dunn <msclrhd@gmail.com>
2018-01-28 11:24:28 +0000

    Convert SettingsUIFactory to Kotlin.

--- src/main/java/uk/co/reecedunn/intellij/plugin/core/ui/SettingsUIFactory.kt
+++ src/main/java/uk/co/reecedunn/intellij/plugin/core/ui/SettingsUIFactory.kt
@@ -13,8 +13,8 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package uk.co.reecedunn.intellij.plugin.core.ui;
+package uk.co.reecedunn.intellij.plugin.core.ui
 
-public interface SettingsUIFactory<Configuration> {
-    SettingsUI<Configuration> createSettingsUI();
+interface SettingsUIFactory<in Configuration> {
+    fun createSettingsUI(): SettingsUI<Configuration>
 }
