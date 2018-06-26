acacca7f8bd6e497109e7be1415466fb15307982
Reece H. Dunn <msclrhd@gmail.com>
2018-06-26 21:52:42 +0100

    Create a wrapper around MarkdownProcessor.

---
+++ src/main/java/uk/co/reecedunn/intellij/plugin/core/parser/Markdown.kt
@@ -0,0 +1,45 @@
+/*
+ * Copyright (C) 2018 Reece H. Dunn
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package uk.co.reecedunn.intellij.plugin.core.parser
+
+import com.petebevin.markdown.MarkdownProcessor
+import org.apache.xmlbeans.impl.common.IOUtil
+import java.io.InputStream
+import java.io.InputStreamReader
+import java.io.StringWriter
+
+/**
+ * Convert Markdown text to HTML.
+ *
+ * This provides a consistent API that can be used in the plugin, allowing the
+ * Markdown processor to be switched out.
+ *
+ * NOTE: Markdown4jProcessor adds `<br/>` elements for each newline, instead of
+ * joining them when used as part of a paragraph.
+ */
+object Markdown {
+    val md = MarkdownProcessor()
+
+    fun parse(text: String): String = md.markdown(text)
+
+    fun parse(text: InputStream): String = md.markdown(streamToString(text))
+
+    private fun streamToString(stream: InputStream): String {
+        val writer = StringWriter()
+        IOUtil.copyCompletely(InputStreamReader(stream), writer)
+        return writer.toString()
+    }
+}
---
+++ src/test/java/uk/co/reecedunn/intellij/plugin/core/tests/parser/MarkdownTest.kt
@@ -0,0 +1,85 @@
+/*
+ * Copyright (C) 2018 Reece H. Dunn
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package uk.co.reecedunn.intellij.plugin.core.tests.parser
+
+import org.hamcrest.CoreMatchers.`is`
+import org.junit.jupiter.api.Test
+import uk.co.reecedunn.intellij.plugin.core.parser.Markdown
+import uk.co.reecedunn.intellij.plugin.core.tests.assertion.assertThat
+
+// NOTE: This tests the Markdown generation to ensure that it is consistent.
+class MarkdownTest {
+    @Test
+    fun onelineParagraph() {
+        assertThat(Markdown.parse("Lorem ipsum dolor."), `is`("<p>Lorem ipsum dolor.</p>\n"))
+    }
+
+    @Test
+    fun multilineParagraph() {
+        assertThat(
+            Markdown.parse("Lorem ipsum dolor sit amet,\r\nconsectetur adipiscing elit."),
+            `is`("<p>Lorem ipsum dolor sit amet,\nconsectetur adipiscing elit.</p>\n")
+        )
+    }
+
+    @Test
+    fun multipleParagraphs() {
+        assertThat(
+            Markdown.parse("Lorem ipsum dolor sit amet,\r\n\r\nconsectetur adipiscing elit."),
+            `is`("<p>Lorem ipsum dolor sit amet,</p>\n\n<p>consectetur adipiscing elit.</p>\n")
+        )
+    }
+
+    @Test
+    fun code() {
+        assertThat(
+            Markdown.parse("Lorem `ipsum` dolor sit amet."),
+            `is`("<p>Lorem <code>ipsum</code> dolor sit amet.</p>\n")
+        )
+    }
+
+    @Test
+    fun emphasis() {
+        assertThat(
+            Markdown.parse("Lorem *ipsum* dolor sit amet."),
+            `is`("<p>Lorem <em>ipsum</em> dolor sit amet.</p>\n")
+        )
+    }
+
+    @Test
+    fun strong() {
+        assertThat(
+            Markdown.parse("Lorem __ipsum__ dolor sit amet."),
+            `is`("<p>Lorem <strong>ipsum</strong> dolor sit amet.</p>\n")
+        )
+    }
+
+    @Test
+    fun onelinePre() {
+        assertThat(
+            Markdown.parse("    Lorem ipsum dolor sit amet."),
+            `is`("<pre><code>    Lorem ipsum dolor sit amet.\n</code></pre>\n")
+        )
+    }
+
+    @Test
+    fun multilinePre() {
+        assertThat(
+            Markdown.parse("    Lorem ipsum dolor sit amet,\r\n    consectetur adipiscing elit."),
+            `is`("<pre><code>    Lorem ipsum dolor sit amet,\n    consectetur adipiscing elit.\n</code></pre>\n")
+        )
+    }
+}
