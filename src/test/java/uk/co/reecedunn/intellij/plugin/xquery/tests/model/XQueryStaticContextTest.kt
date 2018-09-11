657e416db9e163a164a45e96dc2129fde85bcd69
Reece H. Dunn <msclrhd@gmail.com>
2018-09-11 12:33:05 +0100

    Remove the xdm package.

--- src/test/java/uk/co/reecedunn/intellij/plugin/xquery/tests/model/XQueryStaticContextTest.kt
+++ src/test/java/uk/co/reecedunn/intellij/plugin/xquery/tests/model/XQueryStaticContextTest.kt
@@ -19,7 +19,6 @@ import org.hamcrest.CoreMatchers.`is`
 import org.hamcrest.CoreMatchers.nullValue
 import org.junit.jupiter.api.Test
 import uk.co.reecedunn.intellij.plugin.core.tests.assertion.assertThat
-import uk.co.reecedunn.intellij.plugin.xdm.XsAnyURI
 import uk.co.reecedunn.intellij.plugin.xpath.ast.xpath.XPathFunctionCall
 import uk.co.reecedunn.intellij.plugin.xpath.model.XPathStaticContext
 import uk.co.reecedunn.intellij.plugin.xpath.model.inScopeVariablesForFile
