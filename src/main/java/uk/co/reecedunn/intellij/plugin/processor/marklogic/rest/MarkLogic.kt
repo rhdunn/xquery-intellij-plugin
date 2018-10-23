76f9e86df6527951e3fc146ebd9872a7af450ca2
Reece H. Dunn <msclrhd@gmail.com>
2018-10-23 22:37:15 +0100

    MarkLogic: Create a new REST connection.

---
+++ src/main/java/uk/co/reecedunn/intellij/plugin/processor/marklogic/rest/MarkLogic.kt
@@ -0,0 +1,46 @@
+/*
+ * Copyright (C) 2018 Reece H. Dunn
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package uk.co.reecedunn.intellij.plugin.processor.marklogic.rest
+
+import org.apache.http.auth.AuthScope
+import org.apache.http.auth.UsernamePasswordCredentials
+import org.apache.http.impl.client.BasicCredentialsProvider
+import org.apache.http.impl.client.HttpClients
+import uk.co.reecedunn.intellij.plugin.processor.ConnectionSettings
+import uk.co.reecedunn.intellij.plugin.processor.QueryProcessor
+import uk.co.reecedunn.intellij.plugin.processor.QueryProcessorInstanceManager
+
+class MarkLogic : QueryProcessorInstanceManager {
+    override fun create(): QueryProcessor {
+        // MarkLogic does not provide support for running as an in-memory instance.
+        throw UnsupportedOperationException()
+    }
+
+    override fun connect(settings: ConnectionSettings): QueryProcessor {
+        val baseUrl = "http://${settings.hostname}:${settings.port}"
+
+        if (settings.username == null || settings.password == null) {
+            return MarkLogicQueryProcessor(baseUrl, HttpClients.createDefault())
+        }
+
+        val credentials = BasicCredentialsProvider()
+        credentials.setCredentials(
+            AuthScope(settings.hostname, settings.port),
+            UsernamePasswordCredentials(settings.username, settings.password)
+        )
+        return MarkLogicQueryProcessor(baseUrl, HttpClients.custom().setDefaultCredentialsProvider(credentials).build())
+    }
+}
