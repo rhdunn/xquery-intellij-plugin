a42eeee1020e432ba565ac4dc6d29995148a2997
Reece H. Dunn <msclrhd@gmail.com>
2018-11-06 07:28:48 +0000

    Use a TextFieldWithVrowseButton for the jar file query processor option UI.

--- src/main/java/uk/co/reecedunn/intellij/plugin/intellij/settings/QueryProcessorSettingsDialog.kt
+++ src/main/java/uk/co/reecedunn/intellij/plugin/intellij/settings/QueryProcessorSettingsDialog.kt
@@ -23,6 +23,7 @@ import com.intellij.openapi.project.Project
 import com.intellij.openapi.ui.ComboBox
 import com.intellij.openapi.ui.ComponentWithBrowseButton
 import com.intellij.openapi.ui.TextComponentAccessor
+import com.intellij.openapi.ui.TextFieldWithBrowseButton
 import com.intellij.ui.ColoredListCellRenderer
 import uk.co.reecedunn.intellij.plugin.core.ui.Dialog
 import uk.co.reecedunn.intellij.plugin.core.ui.SettingsUI
@@ -109,15 +110,14 @@ class QueryProcessorSettingsDialogUI(private val project: Project) : SettingsUI<
     // endregion
     // region Processor JAR
 
-    private var jar: ComponentWithBrowseButton<JTextField>? = null
+    private var jar: TextFieldWithBrowseButton? = null
 
     private fun createJarUI() {
-        jar = ComponentWithBrowseButton(JTextField(), null)
+        jar = TextFieldWithBrowseButton()
         jar!!.addBrowseFolderListener(
             XQueryBundle.message("browser.choose.implementation-api-jar"), null,
             project,
-            FileTypeDescriptor(XQueryBundle.message("browser.choose.implementation-api-jar"), "jar"),
-            TextComponentAccessor.TEXT_FIELD_WHOLE_TEXT
+            FileTypeDescriptor(XQueryBundle.message("browser.choose.implementation-api-jar"), "jar")
         )
     }
 
@@ -205,7 +205,7 @@ class QueryProcessorSettingsDialogUI(private val project: Project) : SettingsUI<
     override fun reset(configuration: QueryProcessorSettings) {
         name!!.text = configuration.name
         api!!.selectedItem = configuration.api
-        jar!!.childComponent.text = configuration.jar
+        jar!!.textField.text = configuration.jar
         this.configuration!!.childComponent.text = configuration.configurationPath
         if (configuration.connection != null) {
             standalone!!.isSelected = false
@@ -230,7 +230,7 @@ class QueryProcessorSettingsDialogUI(private val project: Project) : SettingsUI<
     override fun apply(configuration: QueryProcessorSettings) {
         configuration.name = name!!.textOrNull()
         configuration.api = api!!.selectedItem as QueryProcessorApi
-        configuration.jar = jar!!.childComponent.textOrNull()
+        configuration.jar = jar!!.textField.textOrNull()
         configuration.configurationPath = this.configuration!!.childComponent.textOrNull()
         if (!standalone!!.isSelected) {
             val dbPort = databasePort!!.text.toInt()
