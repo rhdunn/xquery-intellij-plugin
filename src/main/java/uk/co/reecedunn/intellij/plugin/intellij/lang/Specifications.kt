bd8f053240d8405f536160cbe6de6fd04dc420de
Reece H. Dunn <msclrhd@gmail.com>
2018-11-16 19:31:41 +0000

    [xpath] Add an XPath language object.

--- src/main/java/uk/co/reecedunn/intellij/plugin/intellij/lang/Specifications.kt
+++ src/main/java/uk/co/reecedunn/intellij/plugin/intellij/lang/Specifications.kt
@@ -41,6 +41,47 @@ object XmlSchema : Versioned {
 }
 
 /**
+ * XPath
+ */
+object XPath : Language("XPathREC"), Versioned {
+    val REC_1_0_19991116 = Specification(
+        "1.0-19991116", 1.0, 19991116, "1.0", "https://www.w3.org/TR/1999/REC-xpath-19991116/", this
+    )
+
+    val REC_2_0_20070123 = Specification(
+        "1.0-20070123", 2.0, 20070123, "2.0", "https://www.w3.org/TR/2007/REC-xpath20-20070123/", this
+    )
+
+    val REC_2_0_20101214 = Specification(
+        "1.0-20101214", 2.0, 20101214, "2.0", "https://www.w3.org/TR/2010/REC-xpath20-20101214/", this
+    )
+
+    val REC_3_0_20140408 = Specification(
+        "3.0-20140408", 3.0, 20140408, "3.0", "https://www.w3.org/TR/2014/REC-xpath-30-20140408/", this
+    )
+
+    val REC_3_1_20170321 = Specification(
+        "3.1-20170321", 3.1, 20170321, "3.1", "https://www.w3.org/TR/2017/REC-xpath-31-20170321/", this
+    )
+
+    override fun isCaseSensitive(): Boolean = true
+
+    override fun getDisplayName(): String = "XPath"
+
+    override val id: String = "xpath"
+
+    override val name: String = displayName
+
+    override val versions: List<Version> = listOf(
+        REC_1_0_19991116,
+        REC_2_0_20070123,
+        REC_2_0_20101214,
+        REC_3_0_20140408,
+        REC_3_1_20170321
+    )
+}
+
+/**
  * XQuery
  */
 object XQuery : Language("XQuery", "application/xquery"), Versioned {
