63b165efe98b14b9b58d05b91dc0cbadeb44b909
Reece H. Dunn <msclrhd@gmail.com>
2018-12-02 11:24:51 +0000

    Move {xpath => xquery}.psi.impl.plugin.PluginAnnotatedSequenceTypePsiImpl.

--- src/main/java/uk/co/reecedunn/intellij/plugin/xquery/psi/impl/plugin/PluginAnnotatedSequenceTypePsiImpl.kt
+++ src/main/java/uk/co/reecedunn/intellij/plugin/xquery/psi/impl/plugin/PluginAnnotatedSequenceTypePsiImpl.kt
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package uk.co.reecedunn.intellij.plugin.xpath.psi.impl.plugin
+package uk.co.reecedunn.intellij.plugin.xquery.psi.impl.plugin
 
 import com.intellij.extapi.psi.ASTWrapperPsiElement
 import com.intellij.lang.ASTNode
