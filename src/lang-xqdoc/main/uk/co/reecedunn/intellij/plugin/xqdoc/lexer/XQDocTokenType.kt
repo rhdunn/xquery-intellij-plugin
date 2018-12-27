d031ebd9e88990e7eb4267df7e0391db882bdae5
Reece H. Dunn <msclrhd@gmail.com>
2018-12-27 15:25:08 +0000

    Remove the lang-xquery dependency on lang-xqdoc.

--- src/lang-xqdoc/main/uk/co/reecedunn/intellij/plugin/xqdoc/lexer/XQDocTokenType.kt
+++ src/lang-xqdoc/main/uk/co/reecedunn/intellij/plugin/xqdoc/lexer/XQDocTokenType.kt
@@ -15,49 +15,51 @@
  */
 package uk.co.reecedunn.intellij.plugin.xqdoc.lexer
 
+import com.intellij.lang.Language
 import com.intellij.psi.TokenType
 import com.intellij.psi.tree.IElementType
-import uk.co.reecedunn.intellij.plugin.intellij.lang.XQuery
 
 object XQDocTokenType {
+    val XQDoc = Language.ANY
+
     val WHITE_SPACE: IElementType = TokenType.WHITE_SPACE
-    val TRIM = IElementType("XQDOC_TRIM", XQuery)
+    val TRIM = IElementType("XQDOC_TRIM", XQDoc)
 
-    val XQDOC_COMMENT_MARKER = IElementType("XQDOC_COMMENT_MARKER", XQuery)
-    val CONTENTS = IElementType("XQDOC_CONTENTS", XQuery)
+    val XQDOC_COMMENT_MARKER = IElementType("XQDOC_COMMENT_MARKER", XQDoc)
+    val CONTENTS = IElementType("XQDOC_CONTENTS", XQDoc)
 
-    val TAG_MARKER = IElementType("XQDOC_TAG_MARKER", XQuery)
-    val TAG = IElementType("XQDOC_TAG", XQuery)
+    val TAG_MARKER = IElementType("XQDOC_TAG_MARKER", XQDoc)
+    val TAG = IElementType("XQDOC_TAG", XQDoc)
 
-    val T_AUTHOR = IElementType("XQDOC_T_AUTHOR", XQuery)
-    val T_DEPRECATED = IElementType("XQDOC_T_DEPRECATED", XQuery)
-    val T_ERROR = IElementType("XQDOC_T_ERROR", XQuery)
-    val T_PARAM = IElementType("XQDOC_T_PARAM", XQuery)
-    val T_RETURN = IElementType("XQDOC_T_RETURN", XQuery)
-    val T_SEE = IElementType("XQDOC_T_SEE", XQuery)
-    val T_SINCE = IElementType("XQDOC_T_SINCE", XQuery)
-    val T_VERSION = IElementType("XQDOC_T_VERSION", XQuery)
+    val T_AUTHOR = IElementType("XQDOC_T_AUTHOR", XQDoc)
+    val T_DEPRECATED = IElementType("XQDOC_T_DEPRECATED", XQDoc)
+    val T_ERROR = IElementType("XQDOC_T_ERROR", XQDoc)
+    val T_PARAM = IElementType("XQDOC_T_PARAM", XQDoc)
+    val T_RETURN = IElementType("XQDOC_T_RETURN", XQDoc)
+    val T_SEE = IElementType("XQDOC_T_SEE", XQDoc)
+    val T_SINCE = IElementType("XQDOC_T_SINCE", XQDoc)
+    val T_VERSION = IElementType("XQDOC_T_VERSION", XQDoc)
 
-    val VARIABLE_INDICATOR = IElementType("XQDOC_VARIABLE_INDICATOR", XQuery)
-    val NCNAME = IElementType("XQDOC_NCNAME", XQuery)
+    val VARIABLE_INDICATOR = IElementType("XQDOC_VARIABLE_INDICATOR", XQDoc)
+    val NCNAME = IElementType("XQDOC_NCNAME", XQDoc)
 
-    val OPEN_XML_TAG = IElementType("XQDOC_OPEN_XML_TAG_TOKEN", XQuery)
-    val END_XML_TAG = IElementType("XQUERY_END_XML_TAG_TOKEN", XQuery)
-    val CLOSE_XML_TAG = IElementType("XQUERY_CLOSE_XML_TAG_TOKEN", XQuery)
-    val SELF_CLOSING_XML_TAG = IElementType("XQDOC_SELF_CLOSING_XML_TAG_TOKEN", XQuery)
+    val OPEN_XML_TAG = IElementType("XQDOC_OPEN_XML_TAG_TOKEN", XQDoc)
+    val END_XML_TAG = IElementType("XQUERY_END_XML_TAG_TOKEN", XQDoc)
+    val CLOSE_XML_TAG = IElementType("XQUERY_CLOSE_XML_TAG_TOKEN", XQDoc)
+    val SELF_CLOSING_XML_TAG = IElementType("XQDOC_SELF_CLOSING_XML_TAG_TOKEN", XQDoc)
 
-    val XML_TAG = IElementType("XQDOC_XML_TAG", XQuery)
-    val XML_EQUAL = IElementType("XQDOC_XML_EQUAL_TOKEN", XQuery)
-    val XML_ATTRIBUTE_VALUE_START = IElementType("XQDOC_XML_ATTRIBUTE_VALUE_START_TOKEN", XQuery)
-    val XML_ATTRIBUTE_VALUE_CONTENTS = IElementType("XQDOC_XML_ATTRIBUTE_VALUE_CONTENTS_TOKEN", XQuery)
-    val XML_ATTRIBUTE_VALUE_END = IElementType("XQDOC_XML_ATTRIBUTE_VALUE_END_TOKEN", XQuery)
+    val XML_TAG = IElementType("XQDOC_XML_TAG", XQDoc)
+    val XML_EQUAL = IElementType("XQDOC_XML_EQUAL_TOKEN", XQDoc)
+    val XML_ATTRIBUTE_VALUE_START = IElementType("XQDOC_XML_ATTRIBUTE_VALUE_START_TOKEN", XQDoc)
+    val XML_ATTRIBUTE_VALUE_CONTENTS = IElementType("XQDOC_XML_ATTRIBUTE_VALUE_CONTENTS_TOKEN", XQDoc)
+    val XML_ATTRIBUTE_VALUE_END = IElementType("XQDOC_XML_ATTRIBUTE_VALUE_END_TOKEN", XQDoc)
 
-    val XML_ELEMENT_CONTENTS = IElementType("XQDOC_XML_ELEMENT_CONTENTS_TOKEN", XQuery)
+    val XML_ELEMENT_CONTENTS = IElementType("XQDOC_XML_ELEMENT_CONTENTS_TOKEN", XQDoc)
 
-    val PREDEFINED_ENTITY_REFERENCE = IElementType("XQDOC_PREDEFINED_ENTITY_REFERENCE", XQuery)
-    val PARTIAL_ENTITY_REFERENCE = IElementType("XQDOC_PARTIAL_ENTITY_REFERENCE", XQuery)
-    val EMPTY_ENTITY_REFERENCE = IElementType("XQDOC_EMPTY_ENTITY_REFERENCE", XQuery)
-    val CHARACTER_REFERENCE = IElementType("XQDOC_CHARACTER_REFERENCE", XQuery)
+    val PREDEFINED_ENTITY_REFERENCE = IElementType("XQDOC_PREDEFINED_ENTITY_REFERENCE", XQDoc)
+    val PARTIAL_ENTITY_REFERENCE = IElementType("XQDOC_PARTIAL_ENTITY_REFERENCE", XQDoc)
+    val EMPTY_ENTITY_REFERENCE = IElementType("XQDOC_EMPTY_ENTITY_REFERENCE", XQDoc)
+    val CHARACTER_REFERENCE = IElementType("XQDOC_CHARACTER_REFERENCE", XQDoc)
 
-    val INVALID = IElementType("XQDOC_INVALID_TOKEN", XQuery)
+    val INVALID = IElementType("XQDOC_INVALID_TOKEN", XQDoc)
 }
