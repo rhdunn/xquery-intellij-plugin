e4b9e9df129274521f7c76f471403857d5637d80
Reece H. Dunn <msclrhd@gmail.com>
2019-02-03 09:06:15 +0000

    xsl:attribute/@select = expression.

--- src/lang-xslt/test/resources/tests/xslt/xslt-2.0-stylesheet.xsl
+++ src/lang-xslt/test/resources/tests/xslt/xslt-2.0-stylesheet.xsl
@@ -1,6 +1,7 @@
 <xsl:stylesheet xmlns:xsl="http://www.w3.org/1999/XSL/Transform" version="2.0">
 
     <xsl:template match="lorem/ipsum" name="test">
+        <xsl:attribute name="test" select="@value"/>
         <xsl:for-each-group select="test"
                             group-starting-with="@start"
                             group-ending-with="@end"
--- src/lang-xslt/test/uk/co/reecedunn/intellij/plugin/xslt/psi/XsltTest.kt
+++ src/lang-xslt/test/uk/co/reecedunn/intellij/plugin/xslt/psi/XsltTest.kt
@@ -85,6 +85,26 @@ private class XsltTest : ParserTestCase() {
     }
 
     @Nested
+    @DisplayName("xsl:attribute")
+    internal inner class Attribute {
+        @Test
+        @DisplayName("@select = expression")
+        fun select() {
+            val ss = attribute("tests/xslt/xslt-2.0-stylesheet.xsl", qname("xsl:attribute"), qname("select"))
+            assertThat(ss.isXslStylesheet(), `is`(true))
+            assertThat(ss.getXslVersion(), `is`(XsltSpec.REC_2_0_20070123))
+            assertThat(ss.isXslPattern(), `is`(false))
+            assertThat(ss.isXslExpression(), `is`(true))
+
+            val tf = attribute("tests/xslt/xslt-2.0-transform.xsl", qname("xsl:attribute"), qname("select"))
+            assertThat(tf.isXslStylesheet(), `is`(true))
+            assertThat(tf.getXslVersion(), `is`(XsltSpec.REC_2_0_20070123))
+            assertThat(tf.isXslPattern(), `is`(false))
+            assertThat(tf.isXslExpression(), `is`(true))
+        }
+    }
+
+    @Nested
     @DisplayName("xsl:copy-of")
     internal inner class CopyOf {
         @Test
