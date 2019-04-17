1351dc9e43ef2370d4f99d101a26d57974b9345b
Reece H. Dunn <msclrhd@gmail.com>
2019-04-17 07:30:34 +0100

    Implement ProfileableQueryProvider for the Saxon processor.

--- src/plugin-saxon/main/uk/co/reecedunn/intellij/plugin/saxon/query/s9api/SaxonQueryProcessor.kt
+++ src/plugin-saxon/main/uk/co/reecedunn/intellij/plugin/saxon/query/s9api/SaxonQueryProcessor.kt
@@ -23,6 +23,8 @@ import uk.co.reecedunn.intellij.plugin.core.vfs.decode
 import uk.co.reecedunn.intellij.plugin.intellij.lang.XPath
 import uk.co.reecedunn.intellij.plugin.intellij.lang.XQuery
 import uk.co.reecedunn.intellij.plugin.intellij.lang.XSLT
+import uk.co.reecedunn.intellij.plugin.processor.profile.ProfileableQuery
+import uk.co.reecedunn.intellij.plugin.processor.profile.ProfileableQueryProvider
 import uk.co.reecedunn.intellij.plugin.processor.query.*
 import uk.co.reecedunn.intellij.plugin.processor.validation.ValidatableQuery
 import uk.co.reecedunn.intellij.plugin.processor.validation.ValidatableQueryProvider
@@ -30,6 +32,7 @@ import uk.co.reecedunn.intellij.plugin.saxon.query.s9api.binding.Processor
 import javax.xml.transform.Source
 
 internal class SaxonQueryProcessor(val classLoader: ClassLoader, val source: Source?) :
+    ProfileableQueryProvider,
     RunnableQueryProvider,
     ValidatableQueryProvider {
 
@@ -50,6 +53,11 @@ internal class SaxonQueryProcessor(val classLoader: ClassLoader, val source: Sou
 
     override val databases: ExecutableOnPooledThread<List<String>> = local_thread {
         listOf<String>()
+    }
+
+    override fun createProfileableQuery(query: VirtualFile, language: Language): ProfileableQuery {
+        val runner = createRunnableQuery(query, language)
+        return SaxonQueryProfiler(runner)
     }
 
     override fun createRunnableQuery(query: VirtualFile, language: Language): RunnableQuery {
---
+++ src/plugin-saxon/main/uk/co/reecedunn/intellij/plugin/saxon/query/s9api/SaxonQueryProfiler.kt
@@ -0,0 +1,77 @@
+/*
+ * Copyright (C) 2019 Reece H. Dunn
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package uk.co.reecedunn.intellij.plugin.saxon.query.s9api
+
+import com.intellij.lang.Language
+import uk.co.reecedunn.intellij.plugin.core.async.ExecutableOnPooledThread
+import uk.co.reecedunn.intellij.plugin.intellij.lang.XPathSubset
+import uk.co.reecedunn.intellij.plugin.processor.profile.ProfileQueryResult
+import uk.co.reecedunn.intellij.plugin.processor.profile.ProfileableQuery
+import uk.co.reecedunn.intellij.plugin.processor.query.RunnableQuery
+
+internal class SaxonQueryProfiler(val runner: RunnableQuery) : ProfileableQuery {
+    override var rdfOutputFormat: Language?
+        get() = runner.rdfOutputFormat
+        set(value) {
+            runner.rdfOutputFormat = value
+        }
+
+    override var updating: Boolean
+        get() = runner.updating
+        set(value) {
+            runner.updating = value
+        }
+
+    override var xpathSubset: XPathSubset
+        get() = runner.xpathSubset
+        set(value) {
+            runner.xpathSubset = value
+        }
+
+    override var server: String
+        get() = runner.server
+        set(value) {
+            runner.server = value
+        }
+
+    override var database: String
+        get() = runner.database
+        set(value) {
+            runner.database = value
+        }
+
+    override var modulePath: String
+        get() = runner.modulePath
+        set(value) {
+            runner.modulePath = value
+        }
+
+    override fun bindVariable(name: String, value: Any?, type: String?) {
+        runner.bindVariable(name, value, type)
+    }
+
+    override fun bindContextItem(value: Any?, type: String?) {
+        runner.bindContextItem(value, type)
+    }
+
+    override fun profile(): ExecutableOnPooledThread<ProfileQueryResult> {
+        TODO()
+    }
+
+    override fun close() {
+        runner.close()
+    }
+}
