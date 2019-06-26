498a8ee16f95a481fa517b577e1c51dafe87c118
Reece H. Dunn <msclrhd@gmail.com>
2019-06-26 08:06:36 +0100

    Create an XQuery version completion property.

---
+++ src/lang-xquery/main/uk/co/reecedunn/intellij/plugin/xquery/completion/property/XQueryVersion.kt
@@ -0,0 +1,37 @@
+/*
+ * Copyright (C) 2019 Reece H. Dunn
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package uk.co.reecedunn.intellij.plugin.xquery.completion.property
+
+import com.intellij.codeInsight.completion.CompletionParameters
+import com.intellij.psi.PsiElement
+import com.intellij.util.ProcessingContext
+import uk.co.reecedunn.intellij.plugin.core.completion.CompletionProperty
+import uk.co.reecedunn.intellij.plugin.intellij.lang.NullSpecification
+import uk.co.reecedunn.intellij.plugin.intellij.lang.Version
+import uk.co.reecedunn.intellij.plugin.xquery.ast.xquery.XQueryModule
+
+object XQueryVersion : CompletionProperty {
+    override fun computeProperty(parameters: CompletionParameters, context: ProcessingContext) {
+        if (context[XQueryCompletionProperty.XQUERY_VERSION] == null) {
+            context.put(XQueryCompletionProperty.XQUERY_VERSION, get(parameters.position) ?: NullSpecification)
+        }
+    }
+
+    fun get(element: PsiElement): Version? {
+        val file = element.containingFile as? XQueryModule ?: return null
+        return file.XQueryVersion.getVersionOrDefault(file.project)
+    }
+}
