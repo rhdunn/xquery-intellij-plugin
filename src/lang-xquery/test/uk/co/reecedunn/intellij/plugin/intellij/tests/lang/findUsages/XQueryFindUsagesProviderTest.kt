3dec2752f362d4ec531efd02255b8ba7ccee7202
Reece H. Dunn <msclrhd@gmail.com>
2019-07-06 22:22:17 +0100

    Extend the VarName based find usages tests.

--- src/lang-xquery/test/uk/co/reecedunn/intellij/plugin/intellij/tests/lang/findUsages/XQueryFindUsagesProviderTest.kt
+++ src/lang-xquery/test/uk/co/reecedunn/intellij/plugin/intellij/tests/lang/findUsages/XQueryFindUsagesProviderTest.kt
@@ -16,13 +16,16 @@
 package uk.co.reecedunn.intellij.plugin.intellij.tests.lang.findUsages
 
 import com.intellij.lang.HelpID
+import com.intellij.psi.PsiElement
 import org.hamcrest.CoreMatchers.`is`
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
+import uk.co.reecedunn.intellij.plugin.core.sequences.walkTree
 import uk.co.reecedunn.intellij.plugin.core.tests.assertion.assertThat
-import uk.co.reecedunn.intellij.plugin.xpath.ast.xpath.XPathNCName
 import uk.co.reecedunn.intellij.plugin.intellij.lang.findUsages.XQueryFindUsagesProvider
+import uk.co.reecedunn.intellij.plugin.xpath.ast.xpath.XPathVarRef
+import uk.co.reecedunn.intellij.plugin.xpath.model.XPathVariableReference
 import uk.co.reecedunn.intellij.plugin.xquery.tests.parser.ParserTestCase
 
 // NOTE: This class is private so the JUnit 4 test runner does not run the tests contained in it.
@@ -31,18 +34,58 @@ private class XQueryFindUsagesProviderTest : ParserTestCase() {
     @Nested
     @DisplayName("XQuery 3.1 EBNF (131) VarRef")
     internal inner class VarRef {
+        fun parse(text: String): Pair<PsiElement, PsiElement> {
+            val module = parseText(text)
+            val call = module.walkTree().filterIsInstance<XPathVarRef>().first() as XPathVariableReference
+            val element = call.variableName?.element!!
+            val references = element.references
+            return when (references.size) {
+                1 -> element to references[0].resolve()!! // NCName
+                2 -> element to references[1].resolve()!! // QName
+                else -> throw ArrayIndexOutOfBoundsException()
+            }
+        }
+
         @Test
         @DisplayName("NCName")
         fun ncname() {
             val provider = XQueryFindUsagesProvider
-            val name = parse<XPathNCName>("\$x")[0]
+            val ref = parse("declare variable \$ x := 2; \$x")
 
-            assertThat(provider.canFindUsagesFor(name), `is`(true))
-            assertThat(provider.getHelpId(name), `is`(HelpID.FIND_OTHER_USAGES))
-            assertThat(provider.getType(name), `is`("Identifier"))
-            assertThat(provider.getDescriptiveName(name), `is`("x"))
-            assertThat(provider.getNodeText(name, true), `is`("x"))
-            assertThat(provider.getNodeText(name, false), `is`("x"))
+            assertThat(provider.canFindUsagesFor(ref.second), `is`(true))
+            assertThat(provider.getHelpId(ref.second), `is`(HelpID.FIND_OTHER_USAGES))
+            assertThat(provider.getType(ref.second), `is`("Identifier"))
+            assertThat(provider.getDescriptiveName(ref.second), `is`("x"))
+            assertThat(provider.getNodeText(ref.second, true), `is`("x"))
+            assertThat(provider.getNodeText(ref.second, false), `is`("x"))
+        }
+
+        @Test
+        @DisplayName("QName")
+        fun qname() {
+            val provider = XQueryFindUsagesProvider
+            val ref = parse("declare variable \$ local:x := 2; \$local:x")
+
+            assertThat(provider.canFindUsagesFor(ref.second), `is`(true))
+            assertThat(provider.getHelpId(ref.second), `is`(HelpID.FIND_OTHER_USAGES))
+            assertThat(provider.getType(ref.second), `is`("Identifier"))
+            assertThat(provider.getDescriptiveName(ref.second), `is`("x"))
+            assertThat(provider.getNodeText(ref.second, true), `is`("x"))
+            assertThat(provider.getNodeText(ref.second, false), `is`("x"))
+        }
+
+        @Test
+        @DisplayName("with type")
+        fun withType() {
+            val provider = XQueryFindUsagesProvider
+            val ref = parse("declare variable \$ x as xs:long := 2; \$x")
+
+            assertThat(provider.canFindUsagesFor(ref.second), `is`(true))
+            assertThat(provider.getHelpId(ref.second), `is`(HelpID.FIND_OTHER_USAGES))
+            assertThat(provider.getType(ref.second), `is`("Identifier"))
+            assertThat(provider.getDescriptiveName(ref.second), `is`("x"))
+            assertThat(provider.getNodeText(ref.second, true), `is`("x"))
+            assertThat(provider.getNodeText(ref.second, false), `is`("x"))
         }
     }
 }
