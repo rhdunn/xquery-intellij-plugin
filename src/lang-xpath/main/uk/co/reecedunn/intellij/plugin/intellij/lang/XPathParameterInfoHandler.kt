8ce6249997f4d44cf80c6b59c0842f93531a7e41
Reece H. Dunn <msclrhd@gmail.com>
2019-07-28 10:09:45 +0100

    Create an XPathParameterInfoHanlder object.

---
+++ src/lang-xpath/main/uk/co/reecedunn/intellij/plugin/intellij/lang/XPathParameterInfoHandler.kt
@@ -0,0 +1,45 @@
+/*
+ * Copyright (C) 2019 Reece H. Dunn
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package uk.co.reecedunn.intellij.plugin.intellij.lang
+
+import com.intellij.codeInsight.lookup.LookupElement
+import com.intellij.lang.parameterInfo.*
+import uk.co.reecedunn.intellij.plugin.xpath.ast.xpath.XPathFunctionCall
+
+object XPathParameterInfoHandler : ParameterInfoHandler<XPathFunctionCall, Any> {
+    override fun couldShowInLookup(): Boolean = true
+
+    override fun getParametersForLookup(item: LookupElement?, context: ParameterInfoContext?): Array<Any>? {
+        return null
+    }
+
+    override fun findElementForParameterInfo(context: CreateParameterInfoContext): XPathFunctionCall? {
+        return null
+    }
+
+    override fun showParameterInfo(element: XPathFunctionCall, context: CreateParameterInfoContext) {
+    }
+
+    override fun findElementForUpdatingParameterInfo(context: UpdateParameterInfoContext): XPathFunctionCall? {
+        return null
+    }
+
+    override fun updateParameterInfo(parameterOwner: XPathFunctionCall, context: UpdateParameterInfoContext) {
+    }
+
+    override fun updateUI(p: Any?, context: ParameterInfoUIContext) {
+    }
+}
