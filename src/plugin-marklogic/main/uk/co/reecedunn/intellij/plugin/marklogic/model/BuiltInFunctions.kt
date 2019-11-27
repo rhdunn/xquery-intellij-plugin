12d52a0cf3827b21bbfe59b7f82ec9813883bbec
Reece H. Dunn <msclrhd@gmail.com>
2019-11-27 13:02:29 +0000

    Move the MarkLogic built-in functions to com/marklogic, reflecting the BaseX URI mapping.

--- src/plugin-marklogic/main/uk/co/reecedunn/intellij/plugin/marklogic/model/BuiltInFunctions.kt
+++ src/plugin-marklogic/main/uk/co/reecedunn/intellij/plugin/marklogic/model/BuiltInFunctions.kt
@@ -34,20 +34,20 @@ object BuiltInFunctions : ImportPathResolver {
     }
 
     private val MODULES = mapOf(
-        "http://marklogic.com/cts" to "builtin/marklogic.com/cts.xqy",
-        "http://marklogic.com/geospatial" to "builtin/marklogic.com/geospatial.xqy",
-        "http://marklogic.com/xdmp" to "builtin/marklogic.com/xdmp.xqy",
-        "http://marklogic.com/xdmp/dbg" to "builtin/marklogic.com/xdmp/dbg.xqy",
-        "http://marklogic.com/xdmp/json" to "builtin/marklogic.com/xdmp/json.xqy",
-        "http://marklogic.com/xdmp/map" to "builtin/marklogic.com/xdmp/map.xqy",
-        "http://marklogic.com/xdmp/math" to "builtin/marklogic.com/xdmp/math.xqy",
-        "http://marklogic.com/xdmp/profile" to "builtin/marklogic.com/xdmp/profile.xqy",
-        "http://marklogic.com/xdmp/schema-components" to "builtin/marklogic.com/xdmp/schema-components.xqy",
-        "http://marklogic.com/xdmp/semantics" to "builtin/marklogic.com/xdmp/semantics.xqy",
-        "http://marklogic.com/xdmp/spell" to "builtin/marklogic.com/xdmp/spell.xqy",
-        "http://marklogic.com/xdmp/sql" to "builtin/marklogic.com/xdmp/sql.xqy",
-        "http://marklogic.com/xdmp/tde" to "builtin/marklogic.com/xdmp/tde.xqy",
-        "http://marklogic.com/xdmp/temporal" to "builtin/marklogic.com/xdmp/temporal.xqy",
-        "http://www.w3.org/1999/02/22-rdf-syntax-ns#" to "builtin/www.w3.org/1999/02/22-rdf-syntax-ns.xqy"
+        "http://marklogic.com/cts" to "com/marklogic/cts.xqy",
+        "http://marklogic.com/geospatial" to "com/marklogic/geospatial.xqy",
+        "http://marklogic.com/xdmp" to "com/marklogic/xdmp.xqy",
+        "http://marklogic.com/xdmp/dbg" to "com/marklogic/xdmp/dbg.xqy",
+        "http://marklogic.com/xdmp/json" to "com/marklogic/xdmp/json.xqy",
+        "http://marklogic.com/xdmp/map" to "com/marklogic/xdmp/map.xqy",
+        "http://marklogic.com/xdmp/math" to "com/marklogic/xdmp/math.xqy",
+        "http://marklogic.com/xdmp/profile" to "com/marklogic/xdmp/profile.xqy",
+        "http://marklogic.com/xdmp/schema-components" to "com/marklogic/xdmp/schema-components.xqy",
+        "http://marklogic.com/xdmp/semantics" to "com/marklogic/xdmp/semantics.xqy",
+        "http://marklogic.com/xdmp/spell" to "com/marklogic/xdmp/spell.xqy",
+        "http://marklogic.com/xdmp/sql" to "com/marklogic/xdmp/sql.xqy",
+        "http://marklogic.com/xdmp/tde" to "com/marklogic/xdmp/tde.xqy",
+        "http://marklogic.com/xdmp/temporal" to "com/marklogic/xdmp/temporal.xqy",
+        "http://www.w3.org/1999/02/22-rdf-syntax-ns#" to "org/w3/www/1999/02/22-rdf-syntax-ns.xqy"
     )
 }
