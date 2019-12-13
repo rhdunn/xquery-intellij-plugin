0f96b8af18b3d644f7dbdaa8ce89c082cdffaaf3
Reece H. Dunn <msclrhd@gmail.com>
2019-12-13 07:10:48 +0000

    Create an XdmModulePathFactory interface.

--- src/lang-java/main/uk/co/reecedunn/intellij/plugin/xdm/java/JavaModulePath.kt
+++ src/lang-java/main/uk/co/reecedunn/intellij/plugin/xdm/java/JavaModulePath.kt
@@ -18,12 +18,13 @@ package uk.co.reecedunn.intellij.plugin.xdm.java
 import com.intellij.openapi.project.Project
 import com.intellij.psi.PsiElement
 import uk.co.reecedunn.intellij.plugin.xdm.module.XdmModulePath
+import uk.co.reecedunn.intellij.plugin.xdm.module.XdmModulePathFactory
 
 class JavaModulePath private constructor(val project: Project, val classPath: String) : XdmModulePath {
     override fun resolve(): PsiElement? = JavaModuleManager.getInstance(project).findClass(classPath)
 
-    companion object {
-        fun create(project: Project, path: String): JavaModulePath? {
+    companion object : XdmModulePathFactory {
+        override fun create(project: Project, path: String): JavaModulePath? {
             if (path.startsWith("java:")) return JavaModulePath(project, path.substring(5))
             return null
         }
