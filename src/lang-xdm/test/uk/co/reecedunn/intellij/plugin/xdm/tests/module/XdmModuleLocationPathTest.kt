c7e1e8ae71c63909dc09ecb0e51ce6b63766137e
Reece H. Dunn <msclrhd@gmail.com>
2019-12-18 15:19:07 +0000

    Add an XdmModuleLocationPath class.

---
+++ src/lang-xdm/test/uk/co/reecedunn/intellij/plugin/xdm/tests/module/XdmModuleLocationPathTest.kt
@@ -0,0 +1,159 @@
+/*
+ * Copyright (C) 2019 Reece H. Dunn
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package uk.co.reecedunn.intellij.plugin.xdm.tests.module
+
+import com.intellij.mock.MockProjectEx
+import com.intellij.psi.PsiElement
+import org.hamcrest.CoreMatchers.*
+import org.junit.jupiter.api.*
+import uk.co.reecedunn.compat.testFramework.PlatformLiteFixture
+import uk.co.reecedunn.intellij.plugin.core.tests.assertion.assertThat
+import uk.co.reecedunn.intellij.plugin.xdm.model.XdmUriContext
+import uk.co.reecedunn.intellij.plugin.xdm.model.XsAnyUri
+import uk.co.reecedunn.intellij.plugin.xdm.module.XdmModuleLocationPath
+
+// NOTE: This class is private so the JUnit 4 test runner does not run the tests contained in it.
+@DisplayName("Modules - Location Paths")
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+private class XdmModuleLocationPathTest : PlatformLiteFixture() {
+    @BeforeAll
+    override fun setUp() {
+        super.setUp()
+        initApplication()
+        myProjectEx = MockProjectEx(testRootDisposable)
+    }
+
+    @AfterAll
+    override fun tearDown() {
+        super.tearDown()
+    }
+
+    @Test
+    @DisplayName("empty")
+    fun empty() {
+        XdmUriContext.values().forEach { context ->
+            val uri = XsAnyUri("", context, null as PsiElement?)
+            val path = XdmModuleLocationPath.create(myProject, uri)
+            assertThat(path, `is`(nullValue()))
+        }
+    }
+
+    @Test
+    @DisplayName("HTTP scheme URL")
+    fun httpScheme() {
+        XdmUriContext.values().forEach { context ->
+            val uri = XsAnyUri("http://www.example.com/lorem/ipsum", context, null as PsiElement?)
+            val path = XdmModuleLocationPath.create(myProject, uri)
+            assertThat(path, `is`(nullValue()))
+        }
+    }
+
+    @Test
+    @DisplayName("HTTPS scheme URL")
+    fun httpsScheme() {
+        XdmUriContext.values().forEach { context ->
+            val uri = XsAnyUri("https://www.example.com/lorem/ipsum", context, null as PsiElement?)
+            val path = XdmModuleLocationPath.create(myProject, uri)
+            assertThat(path, `is`(nullValue()))
+        }
+    }
+
+    @Test
+    @DisplayName("file scheme URL")
+    fun fileScheme() {
+        XdmUriContext.values().forEach { context ->
+            val uri = XsAnyUri("file:///C:/lorem/ipsum", context, null as PsiElement?)
+            val path = XdmModuleLocationPath.create(myProject, uri)
+            assertThat(path, `is`(nullValue()))
+        }
+    }
+
+    @Test
+    @DisplayName("URN scheme")
+    fun urnScheme() {
+        XdmUriContext.values().forEach { context ->
+            val uri = XsAnyUri("urn:lorem:ipsum", context, null as PsiElement?)
+            val path = XdmModuleLocationPath.create(myProject, uri)
+            assertThat(path, `is`(nullValue()))
+        }
+    }
+
+    @Nested
+    @DisplayName("Java scheme")
+    internal inner class JavaScheme {
+        @Test
+        @DisplayName("classpath")
+        fun classpath() {
+            XdmUriContext.values().forEach { context ->
+                val uri = XsAnyUri("java:java.lang.String", context, null as PsiElement?)
+                val path = XdmModuleLocationPath.create(myProject, uri)
+                assertThat(path, `is`(nullValue()))
+            }
+        }
+
+        @Test
+        @DisplayName("classpath with void=this")
+        fun voidThis() {
+            XdmUriContext.values().forEach { context ->
+                val uri = XsAnyUri("java:java.lang.String?void=this", context, null as PsiElement?)
+                val path = XdmModuleLocationPath.create(myProject, uri)
+                assertThat(path, `is`(nullValue()))
+            }
+        }
+    }
+
+    @Test
+    @DisplayName("relative path")
+    fun relativePath() {
+        XdmUriContext.values().forEach { context ->
+            val uri = XsAnyUri("lorem/ipsum", context, null as PsiElement?)
+            val path = XdmModuleLocationPath.create(myProject, uri)
+            when (context) {
+                XdmUriContext.Location -> {
+                    assertThat(path, `is`(notNullValue()))
+                    assertThat(path!!.path, `is`("lorem/ipsum"))
+                }
+                else -> assertThat(path, `is`(nullValue()))
+            }
+        }
+    }
+
+    @Test
+    @DisplayName("Java class path")
+    fun javaClassPath() {
+        XdmUriContext.values().forEach { context ->
+            val uri = XsAnyUri("java.lang.String", context, null as PsiElement?)
+            val path = XdmModuleLocationPath.create(myProject, uri)
+            when (context) {
+                XdmUriContext.Location -> {
+                    assertThat(path, `is`(notNullValue()))
+                    assertThat(path!!.path, `is`("java.lang.String"))
+                }
+                else -> assertThat(path, `is`(nullValue()))
+            }
+        }
+    }
+
+    @Test
+    @DisplayName("Saxon java-type namespace")
+    fun javaType() {
+        XdmUriContext.values().forEach { context ->
+            val uri = XsAnyUri("http://saxon.sf.net/java-type", context, null as PsiElement?)
+            val path = XdmModuleLocationPath.create(myProject, uri)
+            assertThat(path, `is`(nullValue()))
+        }
+    }
+}
