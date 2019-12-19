497c7e679c81bb696114a45801e3c8d43ec67132
Reece H. Dunn <msclrhd@gmail.com>
2019-12-19 20:53:22 +0000

    Implement a JavaModuleLoader helper class.

---
+++ src/lang-java/main/uk/co/reecedunn/intellij/plugin/xdm/java/JavaModuleLoader.kt
@@ -0,0 +1,46 @@
+/*
+ * Copyright (C) 2019 Reece H. Dunn
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package uk.co.reecedunn.intellij.plugin.xdm.java
+
+import com.intellij.psi.PsiElement
+import uk.co.reecedunn.intellij.plugin.xdm.context.XdmStaticContext
+import uk.co.reecedunn.intellij.plugin.xdm.module.loader.XdmModuleLoader
+import uk.co.reecedunn.intellij.plugin.xdm.module.loader.XdmModuleLoaderFactory
+import uk.co.reecedunn.intellij.plugin.xdm.module.path.XdmModulePath
+
+object JavaModuleLoader : XdmModuleLoaderFactory, XdmModuleLoader {
+    // region XdmModuleLoaderFactory
+
+    override val id: String = "java"
+
+    override fun loader(context: String?): XdmModuleLoader = this
+
+    // endregion
+    // region XdmModuleLoaderFactory
+
+    override fun resolve(path: XdmModulePath): PsiElement? {
+        return when (path) {
+            is JavaModulePath -> JavaTypePath.getInstance(path.project).findClass(path.classPath)
+            else -> null
+        }
+    }
+
+    override fun context(path: XdmModulePath): XdmStaticContext? {
+        return null
+    }
+
+    // endregion
+}
