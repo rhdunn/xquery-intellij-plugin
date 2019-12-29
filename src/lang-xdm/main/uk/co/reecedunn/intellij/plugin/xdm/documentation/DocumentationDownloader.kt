155710e42b797018dfa476773a179390be611db6
Reece H. Dunn <msclrhd@gmail.com>
2019-12-29 10:54:36 +0000

    Support downloding the documentation source via XdmDocumentationDownloader#download.

--- src/lang-xdm/main/uk/co/reecedunn/intellij/plugin/xdm/documentation/DocumentationDownloader.kt
+++ src/lang-xdm/main/uk/co/reecedunn/intellij/plugin/xdm/documentation/DocumentationDownloader.kt
@@ -15,8 +15,16 @@
  */
 package uk.co.reecedunn.intellij.plugin.xdm.documentation
 
+import com.intellij.openapi.application.PathManager
 import com.intellij.openapi.components.ServiceManager
+import com.intellij.openapi.progress.ProgressIndicator
+import com.intellij.openapi.progress.ProgressManager
+import com.intellij.openapi.progress.Task
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.project.ProjectManager
+import com.intellij.util.io.HttpRequests
 import uk.co.reecedunn.intellij.plugin.intellij.resources.XdmBundle
+import java.io.File
 
 enum class XdmDocumentationDownloadStatus(val label: String) {
     NotDownloaded(XdmBundle.message("download-status.not-downloaded")),
@@ -25,6 +33,20 @@ enum class XdmDocumentationDownloadStatus(val label: String) {
 }
 
 class XdmDocumentationDownloader {
+    var basePath: String? = null
+        get() = field ?: "${PathManager.getSystemPath()}/xdm-cache/documentation"
+
+    fun download(source: XdmDocumentationSource) {
+        val file = File("$basePath/${source.path}")
+        val project = ProjectManager.getInstance().defaultProject
+        val title = XdmBundle.message("documentation-source.download.title")
+        ProgressManager.getInstance().run(object : Task.Backgroundable(project, title) {
+            override fun run(indicator: ProgressIndicator) {
+                HttpRequests.request(source.href).saveToFile(file, indicator)
+            }
+        })
+    }
+
     fun status(source: XdmDocumentationSource): XdmDocumentationDownloadStatus {
         return XdmDocumentationDownloadStatus.NotDownloaded
     }
