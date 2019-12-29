ade733d5fcb4d3de85e3ce0a109b874979e733de
Reece H. Dunn <msclrhd@gmail.com>
2019-12-29 12:51:17 +0000

    Add a cache path field to the documentation sources settings.

--- src/lang-xdm/main/uk/co/reecedunn/intellij/plugin/intellij/settings/documentation/XdmDocumentationSources.kt
+++ src/lang-xdm/main/uk/co/reecedunn/intellij/plugin/intellij/settings/documentation/XdmDocumentationSources.kt
@@ -16,22 +16,55 @@
 package uk.co.reecedunn.intellij.plugin.intellij.settings.documentation
 
 import com.intellij.openapi.options.Configurable
+import com.intellij.openapi.ui.TextFieldWithBrowseButton
+import com.intellij.ui.components.JBLabel
 import com.intellij.ui.components.JBScrollPane
+import com.intellij.util.ui.JBUI
 import uk.co.reecedunn.intellij.plugin.intellij.resources.XdmBundle
 import uk.co.reecedunn.intellij.plugin.xdm.documentation.XdmDocumentationSourceProvider
+import java.awt.GridBagConstraints
+import java.awt.GridBagLayout
 import javax.swing.JComponent
 import javax.swing.JPanel
 
 class XdmDocumentationSources : Configurable {
     // region Configurable
 
+    private val cachePath = TextFieldWithBrowseButton()
     private val sources = XdmDocumentationSourcesTable()
 
     override fun getDisplayName(): String = XdmBundle.message("settings.document-sources.title")
 
     override fun createComponent(): JComponent? {
         XdmDocumentationSourceProvider.allSources.forEach { source -> sources.add(source) }
-        return JBScrollPane(sources)
+
+        val panel = JPanel(GridBagLayout())
+        val constraints = GridBagConstraints()
+
+        constraints.gridx = 0
+        constraints.gridy = 0
+        constraints.fill = GridBagConstraints.NONE
+        constraints.weightx = 0.0
+        constraints.insets = JBUI.insets(0, 0, 4, 8)
+        panel.add(JBLabel(XdmBundle.message("documentation-source.cache-path.label")), constraints)
+
+        constraints.gridx = 1
+        constraints.gridy = 0
+        constraints.fill = GridBagConstraints.HORIZONTAL
+        constraints.weightx = 0.0
+        constraints.insets = JBUI.insetsBottom(4)
+        panel.add(cachePath, constraints)
+
+        constraints.gridx = 0
+        constraints.gridy = 1
+        constraints.gridwidth = GridBagConstraints.REMAINDER
+        constraints.fill = GridBagConstraints.BOTH
+        constraints.weightx = 1.0
+        constraints.weighty = 1.0
+        constraints.insets = JBUI.emptyInsets()
+        panel.add(JBScrollPane(sources), constraints)
+
+        return panel
     }
 
     override fun isModified(): Boolean {
