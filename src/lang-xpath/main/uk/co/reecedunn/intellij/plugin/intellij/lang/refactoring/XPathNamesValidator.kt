27db8a65bdb23791a55c1f53bd2bb2a3b847b06c
Reece H. Dunn <msclrhd@gmail.com>
2020-01-20 07:41:48 +0000

    Rename refactoring -- names validator.

---
+++ src/lang-xpath/main/uk/co/reecedunn/intellij/plugin/intellij/lang/refactoring/XPathNamesValidator.kt
@@ -0,0 +1,40 @@
+/*
+ * Copyright (C) 2020 Reece H. Dunn
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package uk.co.reecedunn.intellij.plugin.intellij.lang.refactoring
+
+import com.intellij.lang.refactoring.NamesValidator
+import com.intellij.openapi.project.Project
+import uk.co.reecedunn.intellij.plugin.core.lexer.CodePointRangeImpl
+import uk.co.reecedunn.intellij.plugin.xpath.lexer.INCNameType
+import uk.co.reecedunn.intellij.plugin.xpath.lexer.XPathLexer
+import uk.co.reecedunn.intellij.plugin.xpath.lexer.XPathTokenType
+
+object XPathNamesValidator : NamesValidator {
+    private val lexer = XPathLexer(CodePointRangeImpl())
+
+    override fun isKeyword(name: String, project: Project?): Boolean = false
+
+    override fun isIdentifier(name: String, project: Project?): Boolean {
+        lexer.start(name)
+        if (lexer.tokenType === XPathTokenType.WHITE_SPACE) lexer.advance()
+
+        val isNCName = lexer.tokenType is INCNameType
+        lexer.advance()
+
+        if (lexer.tokenType === XPathTokenType.WHITE_SPACE) lexer.advance()
+        return lexer.tokenType == null && isNCName
+    }
+}
---
+++ src/lang-xpath/test/uk/co/reecedunn/intellij/plugin/intellij/tests/lang/XPathNamesValidatorTest.kt
@@ -0,0 +1,81 @@
+/*
+ * Copyright (C) 2020 Reece H. Dunn
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package uk.co.reecedunn.intellij.plugin.intellij.tests.lang
+
+import org.hamcrest.CoreMatchers.`is`
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
+import uk.co.reecedunn.intellij.plugin.core.tests.assertion.assertThat
+import uk.co.reecedunn.intellij.plugin.intellij.lang.refactoring.XPathNamesValidator
+
+@DisplayName("IntelliJ - Custom Language Support - Rename Refactoring - Names Validator")
+class XPathNamesValidatorTest {
+    @Test
+    @DisplayName("empty")
+    fun empty() {
+        assertThat(XPathNamesValidator.isKeyword("", null), `is`(false))
+        assertThat(XPathNamesValidator.isIdentifier("", null), `is`(false))
+    }
+
+    @Test
+    @DisplayName("keyword")
+    fun keyword() {
+        assertThat(XPathNamesValidator.isKeyword("for", null), `is`(false)) // Is a valid identifier name.
+        assertThat(XPathNamesValidator.isIdentifier("for", null), `is`(true))
+    }
+
+    @Test
+    @DisplayName("NCName")
+    fun ncname() {
+        assertThat(XPathNamesValidator.isKeyword("abc2def.GHI-jkl\u0330mno", null), `is`(false))
+        assertThat(XPathNamesValidator.isIdentifier("abc2def.GHI-jkl\u0330mno", null), `is`(true))
+    }
+
+    @Test
+    @DisplayName("whitespace before")
+    fun whitespaceBefore() {
+        assertThat(XPathNamesValidator.isKeyword(" \t  test", null), `is`(false))
+        assertThat(XPathNamesValidator.isIdentifier(" \t  test", null), `is`(true))
+    }
+
+    @Test
+    @DisplayName("whitespace after")
+    fun whitespaceAfter() {
+        assertThat(XPathNamesValidator.isKeyword("test \t  ", null), `is`(false))
+        assertThat(XPathNamesValidator.isIdentifier("test \t  ", null), `is`(true))
+    }
+
+    @Test
+    @DisplayName("invalid NCName start character")
+    fun invalidNCNameStartChar() {
+        assertThat(XPathNamesValidator.isKeyword("123test", null), `is`(false))
+        assertThat(XPathNamesValidator.isIdentifier("123test", null), `is`(false))
+    }
+
+    @Test
+    @DisplayName("number (not an identifier)")
+    fun number() {
+        assertThat(XPathNamesValidator.isKeyword("123", null), `is`(false))
+        assertThat(XPathNamesValidator.isIdentifier("123", null), `is`(false))
+    }
+
+    @Test
+    @DisplayName("VarRef")
+    fun varRef() {
+        assertThat(XPathNamesValidator.isKeyword("\$test", null), `is`(false))
+        assertThat(XPathNamesValidator.isIdentifier("\$test", null), `is`(false))
+    }
+}
