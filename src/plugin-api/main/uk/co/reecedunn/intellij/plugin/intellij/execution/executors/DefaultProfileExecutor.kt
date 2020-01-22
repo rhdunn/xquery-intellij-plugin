2a6e0096bf6485ba615a46daed3229ca3ea673f0
Reece H. Dunn <msclrhd@gmail.com>
2020-01-22 20:19:40 +0000

    IntelliJ 2020.1 marks getExecutorById as nullable.

--- src/plugin-api/main/uk/co/reecedunn/intellij/plugin/intellij/execution/executors/DefaultProfileExecutor.kt
+++ src/plugin-api/main/uk/co/reecedunn/intellij/plugin/intellij/execution/executors/DefaultProfileExecutor.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2019 Reece H. Dunn
+ * Copyright (C) 2019-2020 Reece H. Dunn
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -29,7 +29,7 @@ class DefaultProfileExecutor : Executor() {
         @NonNls
         const val EXECUTOR_ID: String = "XIJPProfile"
 
-        fun getInstance(): Executor = ExecutorRegistry.getInstance().getExecutorById(EXECUTOR_ID)
+        fun getInstance(): Executor = ExecutorRegistry.getInstance().getExecutorById(EXECUTOR_ID)!!
     }
 
     override fun getToolWindowId(): String = ToolWindowId.RUN
