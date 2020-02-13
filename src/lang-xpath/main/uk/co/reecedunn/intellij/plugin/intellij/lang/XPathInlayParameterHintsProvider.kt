de1458339ae965a0107bea473fda8328f876083a
Reece H. Dunn <msclrhd@gmail.com>
2020-02-13 22:08:53 +0000

    IntelliJ 2020.1 marks InlayParameterHintsProvider element param an non-null.

--- src/lang-xpath/main/uk/co/reecedunn/intellij/plugin/intellij/lang/XPathInlayParameterHintsProvider.kt
+++ src/lang-xpath/main/uk/co/reecedunn/intellij/plugin/intellij/lang/XPathInlayParameterHintsProvider.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2019 Reece H. Dunn
+ * Copyright (C) 2019-2020 Reece H. Dunn
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -26,7 +26,7 @@ import uk.co.reecedunn.intellij.plugin.xpath.ast.xpath.XPathArgumentList
 
 @Suppress("UnstableApiUsage", "UnstableTypeUsedInSignature")
 object XPathInlayParameterHintsProvider : InlayParameterHintsProvider {
-    override fun getParameterHints(element: PsiElement?): List<InlayInfo> {
+    override fun getParameterHints(element: PsiElement): List<InlayInfo> {
         if (element !is XPathArgumentList) return emptyList()
         return element.bindings.mapIndexedNotNull { index, binding ->
             when {
@@ -42,7 +42,7 @@ object XPathInlayParameterHintsProvider : InlayParameterHintsProvider {
 
     override fun getDefaultBlackList(): Set<String> = defaultBlackList
 
-    override fun getHintInfo(element: PsiElement?): HintInfo.MethodInfo? {
+    override fun getHintInfo(element: PsiElement): HintInfo.MethodInfo? {
         if (element !is XPathArgumentList) return null
         val eqname = element.functionReference?.functionName?.let { op_qname_presentation(it, true) } ?: return null
         val params = element.bindings.mapNotNull {
