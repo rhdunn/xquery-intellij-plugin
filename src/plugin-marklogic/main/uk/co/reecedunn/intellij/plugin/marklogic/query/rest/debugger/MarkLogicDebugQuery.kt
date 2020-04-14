375ac22854d42e151337d8a004614cedf11de770
Reece H. Dunn <msclrhd@gmail.com>
2020-04-14 18:41:19 +0100

    Implement DebugSession for MarkLogic.

--- src/plugin-marklogic/main/uk/co/reecedunn/intellij/plugin/marklogic/query/rest/debugger/MarkLogicDebugQuery.kt
+++ src/plugin-marklogic/main/uk/co/reecedunn/intellij/plugin/marklogic/query/rest/debugger/MarkLogicDebugQuery.kt
@@ -110,7 +110,7 @@ internal class MarkLogicDebugQuery(
     // endregion
     // region DebuggableQuery
 
-    override val session: DebugSession get() = TODO()
+    override val session: DebugSession = MarkLogicDebugSession()
 
     // endregion
     // region Closeable
