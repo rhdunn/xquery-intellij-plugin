f1bf8d82c988e4dfdcd4ee15493dba5ed0225f43
Reece H. Dunn <msclrhd@gmail.com>
2020-04-15 22:01:09 +0100

    Move {Xdm => Xpm}ModuleUriPath to lang-xpm.

--- src/lang-xpm/main/uk/co/reecedunn/intellij/plugin/xpm/module/path/impl/XpmModuleUriPath.kt
+++ src/lang-xpm/main/uk/co/reecedunn/intellij/plugin/xpm/module/path/impl/XpmModuleUriPath.kt
@@ -13,12 +13,14 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package uk.co.reecedunn.intellij.plugin.xdm.module.path
+package uk.co.reecedunn.intellij.plugin.xpm.module.path.impl
 
 import com.intellij.openapi.project.Project
+import uk.co.reecedunn.intellij.plugin.xdm.module.path.XdmModulePath
+import uk.co.reecedunn.intellij.plugin.xdm.module.path.XdmModulePathFactory
 import uk.co.reecedunn.intellij.plugin.xdm.types.XsAnyUriValue
 
-object XdmModuleUriPath : XdmModulePathFactory {
+object XpmModuleUriPath : XdmModulePathFactory {
     override fun create(project: Project, uri: XsAnyUriValue): XdmModulePath? {
         return uri as? XdmModulePath
     }
