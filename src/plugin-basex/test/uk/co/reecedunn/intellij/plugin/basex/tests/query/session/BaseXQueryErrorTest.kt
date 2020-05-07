71549d280941eec0d1e4dcea404149847760a561
Reece H. Dunn <msclrhd@gmail.com>
2020-05-07 17:45:46 +0100

    Don't use DatabaseModule in the BaseX QueryError tests.

--- src/plugin-basex/test/uk/co/reecedunn/intellij/plugin/basex/tests/query/session/BaseXQueryErrorTest.kt
+++ src/plugin-basex/test/uk/co/reecedunn/intellij/plugin/basex/tests/query/session/BaseXQueryErrorTest.kt
@@ -16,14 +16,18 @@
 package uk.co.reecedunn.intellij.plugin.basex.tests.query.session
 
 import com.intellij.compat.testFramework.PlatformLiteFixture
+import com.intellij.mock.MockFileTypeManager
+import com.intellij.mock.MockLanguageFileType
+import com.intellij.openapi.fileTypes.FileTypeManager
+import com.intellij.testFramework.LightVirtualFile
 import com.intellij.xdebugger.XDebuggerUtil
 import com.intellij.xdebugger.impl.XDebuggerUtilImpl
 import org.hamcrest.CoreMatchers.*
 import org.junit.jupiter.api.*
 import uk.co.reecedunn.intellij.plugin.basex.query.session.toBaseXQueryError
 import uk.co.reecedunn.intellij.plugin.core.tests.assertion.assertThat
+import uk.co.reecedunn.intellij.plugin.intellij.lang.XQuery
 import uk.co.reecedunn.intellij.plugin.intellij.xdebugger.QuerySourcePosition
-import uk.co.reecedunn.intellij.plugin.processor.database.DatabaseModule
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 @DisplayName("IntelliJ - Base Platform - Run Configuration - XQuery Processor - BaseXQueryError")
@@ -31,9 +35,12 @@ class BaseXQueryErrorTest : PlatformLiteFixture() {
     @BeforeAll
     override fun setUp() {
         super.setUp()
-        initApplication()
+        val app = initApplication()
 
         registerApplicationService(XDebuggerUtil::class.java, XDebuggerUtilImpl())
+
+        val fileType = MockLanguageFileType(XQuery, "xq")
+        app.registerService(FileTypeManager::class.java, MockFileTypeManager(fileType))
     }
 
     @AfterAll
@@ -44,13 +51,12 @@ class BaseXQueryErrorTest : PlatformLiteFixture() {
     @Test
     @DisplayName("at runtime; BaseX 8.0")
     fun runtime80() {
-        // Query: ```1 div```
-        val e = "Stopped at ., 1/6:\r\n[XPST0003] Calculation is incomplete."
-            .toBaseXQueryError(DatabaseModule("test.xqy"))
+        val testFile = LightVirtualFile("test.xq", XQuery, "1 div")
+        val e = "Stopped at ., 1/6:\r\n[XPST0003] Calculation is incomplete.".toBaseXQueryError(testFile)
         assertThat(e.standardCode, `is`("XPST0003"))
         assertThat(e.vendorCode, `is`(nullValue()))
         assertThat(e.description, `is`("Calculation is incomplete."))
-        assertThat(e.frames[0].sourcePosition?.file, `is`(DatabaseModule("test.xqy")))
+        assertThat(e.frames[0].sourcePosition?.file, `is`(sameInstance(testFile)))
         assertThat(e.frames[0].sourcePosition?.line, `is`(0))
         assertThat((e.frames[0].sourcePosition as QuerySourcePosition).column, `is`(5))
     }
@@ -58,13 +64,12 @@ class BaseXQueryErrorTest : PlatformLiteFixture() {
     @Test
     @DisplayName("with context; BaseX 7.0")
     fun runtime70() {
-        // Query: ```1 div```
-        val e = "Stopped at line 1, column 5:\r\n[XPST0003] Calculation is incomplete."
-            .toBaseXQueryError(DatabaseModule("test.xqy"))
+        val testFile = LightVirtualFile("test.xq", XQuery, "1 div")
+        val e = "Stopped at line 1, column 5:\r\n[XPST0003] Calculation is incomplete.".toBaseXQueryError(testFile)
         assertThat(e.standardCode, `is`("XPST0003"))
         assertThat(e.vendorCode, `is`(nullValue()))
         assertThat(e.description, `is`("Calculation is incomplete."))
-        assertThat(e.frames[0].sourcePosition?.file, `is`(DatabaseModule("test.xqy")))
+        assertThat(e.frames[0].sourcePosition?.file, `is`(sameInstance(testFile)))
         assertThat(e.frames[0].sourcePosition?.line, `is`(0))
         assertThat((e.frames[0].sourcePosition as QuerySourcePosition).column, `is`(4))
     }
@@ -73,11 +78,12 @@ class BaseXQueryErrorTest : PlatformLiteFixture() {
     @DisplayName("bind context; unknown type")
     fun bindContextUnknownType() {
         // Bind context to unknown type -- "[]" as "array-node()".
-        val e = "[XPST0003] Unknown type: array-node().".toBaseXQueryError(DatabaseModule("test.xqy"))
+        val testFile = LightVirtualFile("test.xq", XQuery, "1")
+        val e = "[XPST0003] Unknown type: array-node().".toBaseXQueryError(testFile)
         assertThat(e.standardCode, `is`("XPST0003"))
         assertThat(e.vendorCode, `is`(nullValue()))
         assertThat(e.description, `is`("Unknown type: array-node()."))
-        assertThat(e.frames[0].sourcePosition?.file, `is`(DatabaseModule("test.xqy")))
+        assertThat(e.frames[0].sourcePosition?.file, `is`(sameInstance(testFile)))
         assertThat(e.frames[0].sourcePosition?.line, `is`(0))
         assertThat((e.frames[0].sourcePosition as QuerySourcePosition).column, `is`(0))
     }
