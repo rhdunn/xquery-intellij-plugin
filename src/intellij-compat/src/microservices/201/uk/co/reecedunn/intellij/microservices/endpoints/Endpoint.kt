36607269719f482f2a9c1969ddf118907cd1951e
Reece H. Dunn <msclrhd@gmail.com>
2020-05-24 11:35:45 +0100

    Move Endpoint to the IntelliJ versioned sources.

---
+++ src/intellij-compat/src/microservices/201/uk/co/reecedunn/intellij/microservices/endpoints/Endpoint.kt
@@ -0,0 +1,32 @@
+/*
+ * Copyright (C) 2020 Reece H. Dunn
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package uk.co.reecedunn.intellij.microservices.endpoints
+
+import com.intellij.navigation.ItemPresentation
+import com.intellij.psi.PsiElement
+import com.intellij.psi.PsiReference
+
+abstract class Endpoint {
+    abstract val presentation: ItemPresentation
+
+    abstract val reference: PsiReference?
+
+    abstract val element: PsiElement
+
+    abstract val method: String?
+
+    abstract val path: String?
+}
