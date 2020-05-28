d509ead14f87ff3412dd9a130b2d47b7ef2d5cd0
Reece H. Dunn <msclrhd@gmail.com>
2020-05-28 08:36:55 +0100

    Move lang-xslt's intellij.spellchecker package to xslt.intellij.

--- src/lang-xslt/main/uk/co/reecedunn/intellij/plugin/xslt/intellij/spellchecker/XsltBundledDictionaryProvider.kt
+++ src/lang-xslt/main/uk/co/reecedunn/intellij/plugin/xslt/intellij/spellchecker/XsltBundledDictionaryProvider.kt
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package uk.co.reecedunn.intellij.plugin.intellij.spellchecker
+package uk.co.reecedunn.intellij.plugin.xslt.intellij.spellchecker
 
 import com.intellij.spellchecker.BundledDictionaryProvider
 
