6457b925bbe05a830323c37ed13bd8c01bb6938c
Reece H. Dunn <msclrhd@gmail.com>
2020-05-28 12:52:07 +0100

    Move plugin-existdb's intellij.resources package to existdb.intellij.

--- src/plugin-existdb/main/uk/co/reecedunn/intellij/plugin/existdb/intellij/resources/EXistDBIcons.kt
+++ src/plugin-existdb/main/uk/co/reecedunn/intellij/plugin/existdb/intellij/resources/EXistDBIcons.kt
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package uk.co.reecedunn.intellij.plugin.intellij.resources
+package uk.co.reecedunn.intellij.plugin.existdb.intellij.resources
 
 import com.intellij.openapi.util.IconLoader
 import javax.swing.Icon
