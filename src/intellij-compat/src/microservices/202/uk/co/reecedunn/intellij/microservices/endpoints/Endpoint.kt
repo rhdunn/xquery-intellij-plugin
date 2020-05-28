83aaacfd8bc30143427869f8bc57f1fb9065a824
Reece H. Dunn <msclrhd@gmail.com>
2020-05-28 17:58:40 +0100

    Support Endpoints in IntelliJ Ultimate 2020.2.

---
+++ src/intellij-compat/src/microservices/202/uk/co/reecedunn/intellij/microservices/endpoints/Endpoint.kt
@@ -0,0 +1,57 @@
+/*
+ * Copyright (C) 2020 Reece H. Dunn
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package uk.co.reecedunn.intellij.microservices.endpoints
+
+import com.intellij.microservices.endpoints.presentation.EndpointMethodPresentation
+import com.intellij.microservices.endpoints.presentation.HttpMethodPresentation
+import com.intellij.navigation.ItemPresentation
+import com.intellij.psi.PsiElement
+import com.intellij.psi.PsiReference
+import javax.swing.JPanel
+
+@Suppress("UnstableApiUsage")
+abstract class Endpoint : ItemPresentation, EndpointMethodPresentation {
+    // region ItemPresentation
+
+    override fun getLocationString(): String? = null
+
+    override fun getPresentableText(): String? = path
+
+    // endregion
+    // region EndpointMethodPresentation
+
+    override val endpointMethod: String? get() = method
+
+    override val endpointMethodOrder: Int
+        get() = HttpMethodPresentation.getHttpMethodOrder(method?.split("\\s+")?.get(0))
+
+    // endregion
+    // region Endpoint
+
+    open val presentation: ItemPresentation get() = this
+
+    abstract val details: JPanel
+
+    abstract val reference: PsiReference?
+
+    abstract val element: PsiElement
+
+    abstract val method: String?
+
+    abstract val path: String?
+
+    // endregion
+}
