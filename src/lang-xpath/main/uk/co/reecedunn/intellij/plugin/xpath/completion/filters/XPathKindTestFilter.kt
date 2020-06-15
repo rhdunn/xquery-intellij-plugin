3de3b81df7909508196a6f9c1d944e26cd8c5e90
Reece H. Dunn <msclrhd@gmail.com>
2020-06-15 12:55:59 +0100

    Move the classes in SequenceType.kt to their own files.

--- src/lang-xpath/main/uk/co/reecedunn/intellij/plugin/xpath/completion/filters/XPathKindTestFilter.kt
+++ src/lang-xpath/main/uk/co/reecedunn/intellij/plugin/xpath/completion/filters/XPathKindTestFilter.kt
@@ -23,47 +23,8 @@ import uk.co.reecedunn.intellij.plugin.xdm.functions.XdmFunctionReference
 import uk.co.reecedunn.intellij.plugin.xpath.ast.xpath.XPathAtomicOrUnionType
 import uk.co.reecedunn.intellij.plugin.xpath.ast.xpath.XPathFunctionCall
 import uk.co.reecedunn.intellij.plugin.xpath.ast.xpath.XPathNodeTest
-import uk.co.reecedunn.intellij.plugin.xpath.ast.xpath.XPathSimpleTypeName
 import uk.co.reecedunn.intellij.plugin.xdm.types.XsQNameValue
 import uk.co.reecedunn.intellij.plugin.xdm.types.isPrefixOrNCName
-
-object XPathSequenceTypeFilter : CompletionFilter {
-    override fun accepts(element: PsiElement, context: ProcessingContext): Boolean {
-        return element.ancestors().find {
-            when (it) {
-                is XPathAtomicOrUnionType -> { // SequenceType without '()'
-                    (element.parent as XsQNameValue).isPrefixOrNCName(element)
-                }
-                else -> false
-            }
-        } != null
-    }
-}
-
-object XPathItemTypeFilter : CompletionFilter {
-    override fun accepts(element: PsiElement, context: ProcessingContext): Boolean {
-        return element.ancestors().find {
-            when (it) {
-                is XPathAtomicOrUnionType -> { // ItemType without '()'
-                    (element.parent as XsQNameValue).isPrefixOrNCName(element)
-                }
-                else -> false
-            }
-        } != null
-    }
-}
-
-object XPathAtomicOrUnionTypeFilter : CompletionFilter {
-    override fun accepts(element: PsiElement, context: ProcessingContext): Boolean {
-        return element.ancestors().find {
-            when (it) {
-                is XPathAtomicOrUnionType -> true // SequenceType / ItemType
-                is XPathSimpleTypeName -> true // SingleType
-                else -> false
-            }
-        } != null
-    }
-}
 
 object XPathKindTestFilter : CompletionFilter {
     override fun accepts(element: PsiElement, context: ProcessingContext): Boolean {
