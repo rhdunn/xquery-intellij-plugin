0a7c999eacda4745dbb431306fa6123613c7fd5e
Reece H. Dunn <msclrhd@gmail.com>
2020-06-26 12:42:11 +0100

    MockPsiManager is final in IntelliJ 2020.2 EAP 5.

--- src/intellij-mock/main/uk/co/reecedunn/intellij/plugin/core/tests/psi/MockPsiManager.kt
+++ src/intellij-mock/main/uk/co/reecedunn/intellij/plugin/core/tests/psi/MockPsiManager.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2016-2019 Reece H. Dunn
+ * Copyright (C) 2016-2020 Reece H. Dunn
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,14 +16,31 @@
 package uk.co.reecedunn.intellij.plugin.core.tests.psi
 
 import com.intellij.lang.LanguageUtil
+import com.intellij.mock.MockFileManager
+import com.intellij.openapi.Disposable
 import com.intellij.openapi.project.Project
+import com.intellij.openapi.util.Comparing
 import com.intellij.openapi.vfs.VirtualFile
-import com.intellij.psi.PsiFile
-import com.intellij.psi.PsiFileFactory
+import com.intellij.openapi.vfs.VirtualFileFilter
+import com.intellij.psi.*
+import com.intellij.psi.impl.PsiManagerEx
+import com.intellij.psi.impl.PsiModificationTrackerImpl
+import com.intellij.psi.impl.PsiTreeChangeEventImpl
+import com.intellij.psi.impl.file.impl.FileManager
+import com.intellij.psi.util.PsiModificationTracker
 import uk.co.reecedunn.intellij.plugin.core.vfs.decode
 import java.io.IOException
 
-class MockPsiManager(project: Project) : com.intellij.mock.MockPsiManager(project) {
+// NOTE: The IntelliJ version of MockPsiManager is final in IntelliJ 2020.2, so
+// it cannot be used as the base class.
+class MockPsiManager(project: Project) : PsiManagerEx() {
+    // region PsiManager
+
+    private val myProject = project
+    private val myModificationTracker: PsiModificationTracker by lazy { PsiModificationTrackerImpl(project) }
+
+    override fun getProject(): Project = myProject
+
     override fun findFile(file: VirtualFile): PsiFile? {
         try {
             val language = LanguageUtil.getLanguageForPsi(project, file) ?: return null
@@ -34,4 +51,82 @@ class MockPsiManager(project: Project) : com.intellij.mock.MockPsiManager(projec
             return null
         }
     }
+
+    override fun findViewProvider(file: VirtualFile): FileViewProvider? = null
+
+    override fun findDirectory(file: VirtualFile): PsiDirectory? = null
+
+    override fun areElementsEquivalent(element1: PsiElement?, element2: PsiElement?): Boolean {
+        return Comparing.equal(element1, element2)
+    }
+
+    override fun reloadFromDisk(file: PsiFile) {
+    }
+
+    override fun addPsiTreeChangeListener(listener: PsiTreeChangeListener) {
+    }
+
+    override fun addPsiTreeChangeListener(listener: PsiTreeChangeListener, parentDisposable: Disposable) {
+    }
+
+    override fun removePsiTreeChangeListener(listener: PsiTreeChangeListener) {
+    }
+
+    override fun getModificationTracker(): PsiModificationTracker = myModificationTracker
+
+    override fun startBatchFilesProcessingMode() {
+    }
+
+    override fun finishBatchFilesProcessingMode() {
+    }
+
+    override fun isDisposed(): Boolean = false
+
+    override fun dropResolveCaches() = fileManager.cleanupForNextTest()
+
+    override fun dropPsiCaches() = dropResolveCaches()
+
+    override fun isInProject(element: PsiElement): Boolean = false
+
+    // endregion
+    // region PsiManagerEx
+
+    private val myFileManager: FileManager by lazy { MockFileManager(this) }
+
+    override fun isBatchFilesProcessingMode(): Boolean = false
+
+    override fun setAssertOnFileLoadingFilter(filter: VirtualFileFilter, parentDisposable: Disposable) {
+    }
+
+    override fun isAssertOnFileLoading(file: VirtualFile): Boolean = false
+
+    override fun registerRunnableToRunOnChange(runnable: Runnable) {
+    }
+
+    override fun registerRunnableToRunOnAnyChange(runnable: Runnable) {
+    }
+
+    override fun registerRunnableToRunAfterAnyChange(runnable: Runnable) {
+    }
+
+    override fun getFileManager(): FileManager = myFileManager
+
+    override fun beforeChildAddition(event: PsiTreeChangeEventImpl) {
+    }
+
+    override fun beforeChildRemoval(event: PsiTreeChangeEventImpl) {
+    }
+
+    override fun beforeChildReplacement(event: PsiTreeChangeEventImpl) {
+    }
+
+    override fun beforeChange(isPhysical: Boolean) {
+        throw UnsupportedOperationException()
+    }
+
+    override fun afterChange(isPhysical: Boolean) {
+        throw UnsupportedOperationException()
+    }
+
+    // endregion
 }
