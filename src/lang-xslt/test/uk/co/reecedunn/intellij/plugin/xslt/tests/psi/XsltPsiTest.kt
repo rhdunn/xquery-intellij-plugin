3304f11f64ff77fa54deb14d3d0cc5b8a65e40a7
Reece H. Dunn <msclrhd@gmail.com>
2020-07-12 13:50:48 +0100

    XSLT 1.0 (7.1.3) xsl:attribute

--- src/lang-xslt/test/uk/co/reecedunn/intellij/plugin/xslt/tests/psi/XsltPsiTest.kt
+++ src/lang-xslt/test/uk/co/reecedunn/intellij/plugin/xslt/tests/psi/XsltPsiTest.kt
@@ -281,5 +281,31 @@ private class XsltPsiTest : ParserTestCase() {
                 assertThat(parent.children[0], `is`(sameInstance(psi)))
             }
         }
+
+        @Nested
+        @DisplayName("XSLT 3.0 (11.3) xsl:attribute")
+        internal inner class Attribute {
+            @Test
+            @DisplayName("hierarchy")
+            fun hierarchy() {
+                @Language("XML") val xml = """
+                    <xsl:stylesheet xmlns:xsl="http://www.w3.org/1999/XSL/Transform" version="1.0">
+                        <xsl:template match="lorem">
+                            <xsl:attribute name="ipsum"/>
+                        </xsl:template>
+                    </xsl:stylesheet>
+                """
+                val psi = parse<XsltAttribute>(xml, XSLT.NAMESPACE, "attribute")[0]
+
+                assertThat(psi.parent, `is`(instanceOf(XsltTemplate::class.java)))
+                assertThat(psi.children.size, `is`(0))
+                assertThat(psi.prevSibling, `is`(nullValue()))
+                assertThat(psi.nextSibling, `is`(nullValue()))
+
+                val parent = psi.parent!!
+                assertThat(parent.children.size, `is`(1))
+                assertThat(parent.children[0], `is`(sameInstance(psi)))
+            }
+        }
     }
 }
