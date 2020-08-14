448f3d61637181a7293ade2ecb264f324e098731
Reece H. Dunn <msclrhd@gmail.com>
2020-08-14 15:48:08 +0100

    Split out the XSLT schema type parser tests into a file per schema type.

---
+++ src/lang-xslt/test/uk/co/reecedunn/intellij/plugin/xslt/tests/parser/XslPrefixesTest.kt
@@ -0,0 +1,60 @@
+/*
+ * Copyright (C) 2020 Reece H. Dunn
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package uk.co.reecedunn.intellij.plugin.xslt.tests.parser
+
+import org.hamcrest.CoreMatchers.*
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
+import uk.co.reecedunn.intellij.plugin.core.tests.assertion.assertThat
+import uk.co.reecedunn.intellij.plugin.core.vfs.ResourceVirtualFile
+import uk.co.reecedunn.intellij.plugin.core.vfs.decode
+import uk.co.reecedunn.intellij.plugin.core.vfs.toPsiFile
+import uk.co.reecedunn.intellij.plugin.xdm.psi.tree.ISchemaType
+import uk.co.reecedunn.intellij.plugin.xpath.parser.XPathParserDefinition
+import uk.co.reecedunn.intellij.plugin.xslt.ast.schema.XsltSchemaType
+import uk.co.reecedunn.intellij.plugin.xslt.intellij.lang.XsltSchemaTypes
+import uk.co.reecedunn.intellij.plugin.xslt.parser.XsltSchemaTypesParserDefinition
+
+// NOTE: This class is private so the JUnit 4 test runner does not run the tests contained in it.
+@Suppress("Reformat")
+@DisplayName("XSLT 3.0 - Schema Types - xsl:prefixes")
+private class XslPrefixesTest : ParserTestCase(XsltSchemaTypesParserDefinition(), XPathParserDefinition()) {
+    fun parseResource(resource: String): XsltSchemaType {
+        val file = ResourceVirtualFile.create(this::class.java.classLoader, resource)
+        file.putUserData(ISchemaType.XDM_SCHEMA_TYPE, XsltSchemaTypes.Prefixes)
+        return file.toPsiFile(myProject) as XsltSchemaType
+    }
+
+    fun loadResource(resource: String): String? {
+        return ResourceVirtualFile.create(this::class.java.classLoader, resource).decode()
+    }
+
+    @Test
+    @DisplayName("one")
+    fun one() {
+        val expected = loadResource("tests/parser/schema-type/qname/NCName.txt")
+        val actual = parseResource("tests/parser/schema-type/qname/NCName.input")
+        assertThat(prettyPrintASTNode(actual), `is`(expected))
+    }
+
+    @Test
+    @DisplayName("multiple")
+    fun multiple() {
+        val expected = loadResource("tests/parser/schema-type/prefixes/NCName_List.txt")
+        val actual = parseResource("tests/parser/schema-type/prefixes/NCName_List.input")
+        assertThat(prettyPrintASTNode(actual), `is`(expected))
+    }
+}
