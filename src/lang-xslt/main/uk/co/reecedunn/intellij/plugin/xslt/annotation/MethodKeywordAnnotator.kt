3683c0510be2b4c2f2907c1dd3a3e3ef250b5372
Reece H. Dunn <msclrhd@gmail.com>
2020-08-24 21:51:11 +0100

    Fix the highlighting for xsl:method keywords in XSLT.

---
+++ src/lang-xslt/main/uk/co/reecedunn/intellij/plugin/xslt/annotation/MethodKeywordAnnotator.kt
@@ -0,0 +1,44 @@
+/*
+ * Copyright (C) 2020 Reece H. Dunn
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package uk.co.reecedunn.intellij.plugin.xslt.annotation
+
+import com.intellij.compat.lang.annotation.AnnotationHolder
+import com.intellij.compat.lang.annotation.Annotator
+import com.intellij.lang.annotation.HighlightSeverity
+import com.intellij.openapi.editor.XmlHighlighterColors
+import com.intellij.psi.PsiElement
+import com.intellij.psi.xml.XmlAttributeValue
+import uk.co.reecedunn.intellij.plugin.core.psi.contextOfType
+import uk.co.reecedunn.intellij.plugin.core.sequences.children
+import uk.co.reecedunn.intellij.plugin.core.xml.attribute
+import uk.co.reecedunn.intellij.plugin.core.xml.schemaType
+import uk.co.reecedunn.intellij.plugin.xdm.types.XsQNameValue
+
+class MethodKeywordAnnotator : Annotator() {
+    override fun annotateElement(element: PsiElement, holder: AnnotationHolder) {
+        element.children().filterIsInstance<XsQNameValue>().forEach { qname ->
+            if (qname.isLexicalQName && qname.namespace == null) {
+                val attr = element.contextOfType<XmlAttributeValue>(false)?.attribute ?: return
+                if (attr.schemaType == "xsl:method") {
+                    holder.newSilentAnnotation(HighlightSeverity.INFORMATION)
+                        .range(element)
+                        .textAttributes(XmlHighlighterColors.XML_ATTRIBUTE_VALUE)
+                        .create()
+                }
+            }
+        }
+    }
+}
