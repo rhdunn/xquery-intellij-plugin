3a5bd61491cb2dffff8343f534d63e6186fa1d95
Reece H. Dunn <msclrhd@gmail.com>
2020-09-05 11:11:12 +0100

    Implement PsiLanguageInjectionHost on DirTextConstructor elements.

--- src/lang-xquery/main/uk/co/reecedunn/intellij/plugin/xquery/ast/plugin/PluginDirTextConstructor.kt
+++ src/lang-xquery/main/uk/co/reecedunn/intellij/plugin/xquery/ast/plugin/PluginDirTextConstructor.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2016 Reece H. Dunn
+ * Copyright (C) 2020 Reece H. Dunn
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -15,9 +15,9 @@
  */
 package uk.co.reecedunn.intellij.plugin.xquery.ast.plugin
 
-import com.intellij.psi.PsiElement
+import com.intellij.psi.PsiLanguageInjectionHost
 
 /**
  * A `DirTextConstructor` node in the XQuery AST.
  */
-interface PluginDirTextConstructor : PsiElement
+interface PluginDirTextConstructor : PsiLanguageInjectionHost
--- src/lang-xquery/main/uk/co/reecedunn/intellij/plugin/xquery/psi/impl/plugin/PluginDirTextConstructorPsiImpl.kt
+++ src/lang-xquery/main/uk/co/reecedunn/intellij/plugin/xquery/psi/impl/plugin/PluginDirTextConstructorPsiImpl.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2016 Reece H. Dunn
+ * Copyright (C) 2020 Reece H. Dunn
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -17,6 +17,37 @@ package uk.co.reecedunn.intellij.plugin.xquery.psi.impl.plugin
 
 import com.intellij.extapi.psi.ASTWrapperPsiElement
 import com.intellij.lang.ASTNode
+import com.intellij.openapi.util.TextRange
+import com.intellij.psi.LiteralTextEscaper
+import com.intellij.psi.PsiLanguageInjectionHost
+import uk.co.reecedunn.intellij.plugin.core.lang.injection.LiteralTextEscaperImpl
+import uk.co.reecedunn.intellij.plugin.core.lang.injection.LiteralTextHost
 import uk.co.reecedunn.intellij.plugin.xquery.ast.plugin.PluginDirTextConstructor
 
-class PluginDirTextConstructorPsiImpl(node: ASTNode) : ASTWrapperPsiElement(node), PluginDirTextConstructor
+class PluginDirTextConstructorPsiImpl(node: ASTNode) :
+    ASTWrapperPsiElement(node), PluginDirTextConstructor, LiteralTextHost {
+    // region PsiLanguageInjectionHost
+
+    override fun isValidHost(): Boolean = true
+
+    override fun updateText(text: String): PsiLanguageInjectionHost {
+        return this
+    }
+
+    override fun createLiteralTextEscaper(): LiteralTextEscaper<out PsiLanguageInjectionHost> {
+        return LiteralTextEscaperImpl(this)
+    }
+
+    // endregion
+    // region LiteralTextHost
+
+    override val isOneLine: Boolean = false
+
+    override val relevantTextRange: TextRange
+        get() = TODO()
+
+    override val decoded: String
+        get() = TODO()
+
+    // endregion
+}
