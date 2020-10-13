e8d29b0f7cc96d16a0bf45f58f73616e82765f14
Reece H. Dunn <msclrhd@gmail.com>
2020-10-13 21:27:40 +0100

    Inherit the ScriptingConcatExpr interface from XPathExpr.

--- src/lang-xquery/main/uk/co/reecedunn/intellij/plugin/xquery/ast/scripting/ScriptingConcatExpr.kt
+++ src/lang-xquery/main/uk/co/reecedunn/intellij/plugin/xquery/ast/scripting/ScriptingConcatExpr.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2016 Reece H. Dunn
+ * Copyright (C) 2016, 2020 Reece H. Dunn
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -15,9 +15,9 @@
  */
 package uk.co.reecedunn.intellij.plugin.xquery.ast.scripting
 
-import com.intellij.psi.PsiElement
+import uk.co.reecedunn.intellij.plugin.xpath.ast.xpath.XPathExpr
 
 /**
  * An XQuery Scripting Extension 1.0 `ConcatExpr` node in the XQuery AST.
  */
-interface ScriptingConcatExpr : PsiElement
+interface ScriptingConcatExpr : XPathExpr
--- src/lang-xquery/main/uk/co/reecedunn/intellij/plugin/xquery/psi/impl/scripting/ScriptingConcatExprPsiImpl.kt
+++ src/lang-xquery/main/uk/co/reecedunn/intellij/plugin/xquery/psi/impl/scripting/ScriptingConcatExprPsiImpl.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2016 Reece H. Dunn
+ * Copyright (C) 2016, 2020 Reece H. Dunn
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -17,6 +17,9 @@ package uk.co.reecedunn.intellij.plugin.xquery.psi.impl.scripting
 
 import com.intellij.extapi.psi.ASTWrapperPsiElement
 import com.intellij.lang.ASTNode
+import com.intellij.psi.PsiElement
 import uk.co.reecedunn.intellij.plugin.xquery.ast.scripting.ScriptingConcatExpr
 
-class ScriptingConcatExprPsiImpl(node: ASTNode) : ASTWrapperPsiElement(node), ScriptingConcatExpr
+class ScriptingConcatExprPsiImpl(node: ASTNode) : ASTWrapperPsiElement(node), ScriptingConcatExpr {
+    override val expressionElement: PsiElement? = null
+}
