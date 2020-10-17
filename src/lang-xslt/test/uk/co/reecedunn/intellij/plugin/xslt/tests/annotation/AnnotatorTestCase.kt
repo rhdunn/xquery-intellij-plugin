f7a0f5eb4611947479f4a9670461dc67b3f616d4
Reece H. Dunn <msclrhd@gmail.com>
2020-10-17 13:35:01 +0100

    Add a SchemaTypeAnnotator class with xsl:sequence-type tests.

---
+++ src/lang-xslt/test/uk/co/reecedunn/intellij/plugin/xslt/tests/annotation/AnnotatorTestCase.kt
@@ -0,0 +1,76 @@
+/*
+ * Copyright (C) 2019-2020 Reece H. Dunn
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package uk.co.reecedunn.intellij.plugin.xslt.tests.annotation
+
+import com.intellij.lang.LanguageASTFactory
+import com.intellij.lang.ParserDefinition
+import com.intellij.lang.injection.InjectedLanguageManager
+import com.intellij.lang.xml.XMLLanguage
+import com.intellij.lang.xml.XmlASTFactory
+import com.intellij.openapi.extensions.DefaultPluginDescriptor
+import com.intellij.openapi.extensions.PluginId
+import com.intellij.openapi.module.ModuleManager
+import com.intellij.openapi.roots.ProjectRootManager
+import com.intellij.psi.PsiFile
+import com.intellij.psi.xml.StartTagEndTokenProvider
+import com.intellij.psi.xml.XmlFile
+import com.intellij.xml.XmlExtension
+import org.junit.jupiter.api.AfterAll
+import org.junit.jupiter.api.BeforeAll
+import org.junit.jupiter.api.TestInstance
+import uk.co.reecedunn.intellij.plugin.core.tests.injecton.MockInjectedLanguageManager
+import uk.co.reecedunn.intellij.plugin.core.tests.module.MockModuleManager
+import uk.co.reecedunn.intellij.plugin.core.tests.roots.MockProjectRootsManager
+import uk.co.reecedunn.intellij.plugin.xpm.psi.shadow.XpmShadowPsiElementFactory
+import uk.co.reecedunn.intellij.plugin.xpm.psi.shadow.XpmShadowPsiElementFactoryBean
+import uk.co.reecedunn.intellij.plugin.xslt.psi.impl.XsltShadowPsiElementFactory
+
+@Suppress("MemberVisibilityCanBePrivate", "SameParameterValue")
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+abstract class AnnotatorTestCase(vararg definitions: ParserDefinition) :
+    uk.co.reecedunn.intellij.plugin.core.tests.parser.AnnotatorTestCase<PsiFile>(null, *definitions) {
+
+    @BeforeAll
+    override fun setUp() {
+        super.setUp()
+        addExplicitExtension(LanguageASTFactory.INSTANCE, XMLLanguage.INSTANCE, XmlASTFactory())
+
+        registerExtensionPoint(StartTagEndTokenProvider.EP_NAME, StartTagEndTokenProvider::class.java)
+        registerExtensionPoint(XmlExtension.EP_NAME, XmlExtension::class.java)
+
+        myProject.registerService(ProjectRootManager::class.java, MockProjectRootsManager())
+        myProject.registerService(ModuleManager::class.java, MockModuleManager(myProject))
+        myProject.registerService(InjectedLanguageManager::class.java, MockInjectedLanguageManager())
+
+        registerExtensionPoint(XpmShadowPsiElementFactory.EP_NAME, XpmShadowPsiElementFactoryBean::class.java)
+        registerShadowPsiElementFactory(XsltShadowPsiElementFactory, "INSTANCE")
+    }
+
+    @AfterAll
+    override fun tearDown() {
+        super.tearDown()
+    }
+
+    @Suppress("UsePropertyAccessSyntax")
+    private fun registerShadowPsiElementFactory(factory: XpmShadowPsiElementFactory, fieldName: String) {
+        val classLoader = AnnotatorTestCase::class.java.classLoader
+        val bean = XpmShadowPsiElementFactoryBean()
+        bean.implementationClass = factory.javaClass.name
+        bean.fieldName = fieldName
+        bean.setPluginDescriptor(DefaultPluginDescriptor(PluginId.getId("shadowPsiElementFactory"), classLoader))
+        registerExtension(XpmShadowPsiElementFactory.EP_NAME, bean)
+    }
+}
---
+++ src/lang-xslt/test/uk/co/reecedunn/intellij/plugin/xslt/tests/annotation/XslSequenceTypeTest.kt
@@ -0,0 +1,74 @@
+/*
+ * Copyright (C) 2020 Reece H. Dunn
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package uk.co.reecedunn.intellij.plugin.xslt.tests.annotation
+
+import org.hamcrest.CoreMatchers.`is`
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.Test
+import uk.co.reecedunn.intellij.plugin.core.tests.assertion.assertThat
+import uk.co.reecedunn.intellij.plugin.core.tests.parser.prettyPrint
+import uk.co.reecedunn.intellij.plugin.xpath.parser.XPathParserDefinition
+import uk.co.reecedunn.intellij.plugin.xslt.annotation.SchemaTypeAnnotator
+import uk.co.reecedunn.intellij.plugin.xslt.ast.schema.XsltSchemaType
+import uk.co.reecedunn.intellij.plugin.xslt.intellij.lang.SequenceType
+
+// NOTE: This class is private so the JUnit 4 test runner does not run the tests contained in it.
+@Suppress("Reformat")
+@DisplayName("IntelliJ - Custom Language Support - Syntax Highlighting - SequenceType Schema Type Annotator")
+private class XslSequenceTypeTest : AnnotatorTestCase(SequenceType.ParserDefinition(), XPathParserDefinition()) {
+    @Nested
+    @DisplayName("xsl:sequence-type")
+    internal inner class SequenceTypeTest {
+        val annotator = SchemaTypeAnnotator()
+
+        @Test
+        @DisplayName("XPath 3.1 EBNF (81) ItemType")
+        fun itemType() {
+            val file = parse<XsltSchemaType>("item()")[0]
+            val annotations = annotateTree(file, annotator).prettyPrint()
+            assertThat(annotations, `is`(""))
+        }
+
+        @Nested
+        @DisplayName("XPath 3.1 EBNF (79) SequenceType")
+        internal inner class SequenceTypeTest {
+            @Test
+            @DisplayName("zero or more")
+            fun zeroOrMore() {
+                val file = parse<XsltSchemaType>("item()*")[0]
+                val annotations = annotateTree(file, annotator).prettyPrint()
+                assertThat(annotations, `is`(""))
+            }
+
+            @Test
+            @DisplayName("one or more")
+            fun oneOrMore() {
+                val file = parse<XsltSchemaType>("item()+")[0]
+                val annotations = annotateTree(file, annotator).prettyPrint()
+                assertThat(annotations, `is`(""))
+            }
+
+            @Test
+            @DisplayName("zero or one")
+            fun zeroOrOne() {
+                val file = parse<XsltSchemaType>("item()?")[0]
+                val annotations = annotateTree(file, annotator).prettyPrint()
+                assertThat(annotations, `is`(""))
+            }
+        }
+    }
+}
