89fe622aeef08c506d0900986bb1a22a6aee9173
Reece H. Dunn <msclrhd@gmail.com>
2020-11-07 11:54:30 +0000

    Implement XpmVariableBinding for SimpleForBinding.

--- src/lang-xpath/main/uk/co/reecedunn/intellij/plugin/xpath/psi/impl/xpath/XPathSimpleForBindingPsiImpl.kt
+++ src/lang-xpath/main/uk/co/reecedunn/intellij/plugin/xpath/psi/impl/xpath/XPathSimpleForBindingPsiImpl.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2019 Reece H. Dunn
+ * Copyright (C) 2019-2020 Reece H. Dunn
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -17,8 +17,11 @@ package uk.co.reecedunn.intellij.plugin.xpath.psi.impl.xpath
 
 import com.intellij.extapi.psi.ASTWrapperPsiElement
 import com.intellij.lang.ASTNode
+import uk.co.reecedunn.intellij.plugin.core.sequences.children
+import uk.co.reecedunn.intellij.plugin.xdm.types.XsQNameValue
 import uk.co.reecedunn.intellij.plugin.xpath.ast.xpath.XPathSimpleForBinding
 
-class XPathSimpleForBindingPsiImpl(node: ASTNode) :
-    ASTWrapperPsiElement(node),
-    XPathSimpleForBinding
+class XPathSimpleForBindingPsiImpl(node: ASTNode) : ASTWrapperPsiElement(node), XPathSimpleForBinding {
+    override val variableName: XsQNameValue?
+        get() = children().filterIsInstance<XsQNameValue>().firstOrNull()
+}
