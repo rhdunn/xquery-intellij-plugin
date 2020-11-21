c171113905ec04997768f7e23c71d9d406764c29
Reece H. Dunn <msclrhd@gmail.com>
2020-11-21 20:35:45 +0000

    Use IdeaPlatformTestCase in MarkLogicProfileTest.

--- src/plugin-marklogic/test/uk/co/reecedunn/intellij/plugin/marklogic/tests/profile/MarkLogicProfileTest.kt
+++ src/plugin-marklogic/test/uk/co/reecedunn/intellij/plugin/marklogic/tests/profile/MarkLogicProfileTest.kt
@@ -15,10 +15,11 @@
  */
 package uk.co.reecedunn.intellij.plugin.marklogic.tests.profile
 
-import com.intellij.compat.testFramework.PlatformLiteFixture
 import com.intellij.compat.testFramework.registerServiceInstance
 import com.intellij.mock.MockFileTypeManager
 import com.intellij.mock.MockLanguageFileType
+import com.intellij.openapi.application.ApplicationManager
+import com.intellij.openapi.extensions.PluginId
 import com.intellij.openapi.fileTypes.FileTypeManager
 import com.intellij.testFramework.LightVirtualFile
 import com.intellij.xdebugger.XDebuggerUtil
@@ -28,6 +29,7 @@ import org.hamcrest.CoreMatchers.sameInstance
 import org.intellij.lang.annotations.Language
 import org.junit.jupiter.api.*
 import uk.co.reecedunn.intellij.plugin.core.tests.assertion.assertThat
+import uk.co.reecedunn.intellij.plugin.core.tests.testFramework.IdeaPlatformTestCase
 import uk.co.reecedunn.intellij.plugin.xquery.intellij.lang.XQuery
 import uk.co.reecedunn.intellij.plugin.processor.intellij.xdebugger.QuerySourcePosition
 import uk.co.reecedunn.intellij.plugin.marklogic.profile.toMarkLogicProfileReport
@@ -35,22 +37,16 @@ import uk.co.reecedunn.intellij.plugin.xdm.types.impl.values.XsDecimal
 import uk.co.reecedunn.intellij.plugin.xdm.types.impl.values.XsInteger
 
 @Suppress("XmlPathReference")
-@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 @DisplayName("IntelliJ - Base Platform - Run Configuration - Query Profiler - MarkLogicProfile")
-class MarkLogicProfileTest : PlatformLiteFixture() {
-    @BeforeAll
-    override fun setUp() {
-        super.setUp()
-        val app = initApplication()
+class MarkLogicProfileTest : IdeaPlatformTestCase() {
+    override val pluginId: PluginId = PluginId.getId("MarkLogicProfileTest")
+
+    override fun registerServicesAndExtensions() {
+        val app = ApplicationManager.getApplication()
         app.registerServiceInstance(XDebuggerUtil::class.java, XDebuggerUtilImpl())
 
         val fileType = MockLanguageFileType(XQuery, "xq")
         app.registerServiceInstance(FileTypeManager::class.java, MockFileTypeManager(fileType))
-    }
-
-    @AfterAll
-    override fun tearDown() {
-        super.tearDown()
     }
 
     @Test
