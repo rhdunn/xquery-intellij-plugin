3c8a27ca1af932eff8c2d8ada1f3387c9a30df6d
Reece H. Dunn <msclrhd@gmail.com>
2020-12-17 12:47:38 +0000

    Move the XSLT namespace logic into XsltNamespaceProvider.

--- src/lang-xslt/main/uk/co/reecedunn/intellij/plugin/xslt/optree/XsltNamespaceProvider.kt
+++ src/lang-xslt/main/uk/co/reecedunn/intellij/plugin/xslt/optree/XsltNamespaceProvider.kt
@@ -16,6 +16,17 @@
 package uk.co.reecedunn.intellij.plugin.xslt.optree
 
 import com.intellij.psi.PsiElement
+import com.intellij.psi.xml.XmlAttribute
+import com.intellij.psi.xml.XmlAttributeValue
+import com.intellij.psi.xml.XmlTag
+import uk.co.reecedunn.intellij.plugin.core.psi.contextOfType
+import uk.co.reecedunn.intellij.plugin.core.sequences.ancestors
+import uk.co.reecedunn.intellij.plugin.xdm.module.path.XdmModuleType
+import uk.co.reecedunn.intellij.plugin.xdm.types.XdmUriContext
+import uk.co.reecedunn.intellij.plugin.xdm.types.XsAnyUriValue
+import uk.co.reecedunn.intellij.plugin.xdm.types.XsNCNameValue
+import uk.co.reecedunn.intellij.plugin.xdm.types.impl.psi.XsAnyUri
+import uk.co.reecedunn.intellij.plugin.xdm.types.impl.psi.XsNCName
 import uk.co.reecedunn.intellij.plugin.xpath.ast.xpath.XPath
 import uk.co.reecedunn.intellij.plugin.xpm.optree.namespace.XdmNamespaceType
 import uk.co.reecedunn.intellij.plugin.xpm.optree.namespace.XpmNamespaceDeclaration
@@ -24,19 +35,71 @@ import uk.co.reecedunn.intellij.plugin.xpm.optree.namespace.XpmNamespaceProvider
 object XsltNamespaceProvider : XpmNamespaceProvider {
     override fun staticallyKnownNamespaces(context: PsiElement): Sequence<XpmNamespaceDeclaration> {
         if (context.containingFile !is XPath) return emptySequence()
-        return context.staticallyKnownXPathNamespaces()
+        return staticallyKnownXPathNamespaces(context)
     }
 
     override fun defaultNamespace(context: PsiElement, type: XdmNamespaceType): Sequence<XpmNamespaceDeclaration> {
         if (context.containingFile !is XPath) return emptySequence()
         return when (type) {
-            XdmNamespaceType.DefaultElement -> context.defaultElementOrTypeXPathNamespace()
+            XdmNamespaceType.DefaultElement -> defaultElementOrTypeXPathNamespace(context)
             XdmNamespaceType.DefaultFunctionDecl -> DefaultFunctionDeclOrRefNamespace
             XdmNamespaceType.DefaultFunctionRef -> DefaultFunctionDeclOrRefNamespace
-            XdmNamespaceType.DefaultType -> context.defaultElementOrTypeXPathNamespace()
+            XdmNamespaceType.DefaultType -> defaultElementOrTypeXPathNamespace(context)
             else -> emptySequence()
         }
     }
 
     private val DefaultFunctionDeclOrRefNamespace = sequenceOf(DefaultFunctionXPathNamespace)
+
+    private fun defaultElementOrTypeXPathNamespace(context: PsiElement): Sequence<XpmNamespaceDeclaration> {
+        return context.contextOfType<XmlAttributeValue>(false)?.ancestors()?.filterIsInstance<XmlTag>()?.flatMap { tag ->
+            tag.attributes.asSequence().map { attribute -> toDefaultNamespaceDeclaration(attribute) }
+        }?.filterNotNull() ?: sequenceOf()
+    }
+
+    private fun toDefaultNamespaceDeclaration(attribute: XmlAttribute): XpmNamespaceDeclaration? {
+        if (!attribute.isNamespaceDeclaration) return null
+        val prefix = attribute.namespacePrefix
+        val value = attribute.value ?: return null
+        return if (prefix.isEmpty()) {
+            object : XpmNamespaceDeclaration {
+                override val namespacePrefix: XsNCNameValue? = null
+                override val namespaceUri: XsAnyUriValue =
+                    XsAnyUri(value, XdmUriContext.Namespace, XdmModuleType.NONE, attribute.originalElement)
+
+                override fun accepts(namespaceType: XdmNamespaceType): Boolean = when (namespaceType) {
+                    XdmNamespaceType.DefaultElement, XdmNamespaceType.DefaultType -> true
+                    else -> false
+                }
+            }
+        } else {
+            null
+        }
+    }
+
+    private fun staticallyKnownXPathNamespaces(context: PsiElement): Sequence<XpmNamespaceDeclaration> {
+        return context.contextOfType<XmlAttributeValue>(false)?.ancestors()?.filterIsInstance<XmlTag>()?.flatMap { tag ->
+            tag.attributes.asSequence().map { attribute -> toNamespaceDeclaration(attribute) }
+        }?.filterNotNull() ?: sequenceOf()
+    }
+
+    private fun toNamespaceDeclaration(attribute: XmlAttribute): XpmNamespaceDeclaration? {
+        if (!attribute.isNamespaceDeclaration) return null
+        val prefix = attribute.namespacePrefix
+        val localName = attribute.localName
+        val value = attribute.value ?: return null
+        return if (prefix.isEmpty()) {
+            null
+        } else {
+            object : XpmNamespaceDeclaration {
+                override val namespacePrefix: XsNCNameValue = XsNCName(localName, attribute.originalElement)
+                override val namespaceUri: XsAnyUriValue =
+                    XsAnyUri(value, XdmUriContext.Namespace, XdmModuleType.MODULE, attribute.originalElement)
+
+                override fun accepts(namespaceType: XdmNamespaceType): Boolean {
+                    return namespaceType === XdmNamespaceType.Prefixed
+                }
+            }
+        }
+    }
 }
