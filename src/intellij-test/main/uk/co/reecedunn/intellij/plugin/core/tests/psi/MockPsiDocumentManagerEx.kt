9019d3a9d1f4161d525181c7110e1eda55fac7e9
Reece H. Dunn <msclrhd@gmail.com>
2021-01-27 21:09:46 +0000

    Simplify MockPsiDocumentManagerEx.

--- src/intellij-test/main/uk/co/reecedunn/intellij/plugin/core/tests/psi/MockPsiDocumentManagerEx.kt
+++ src/intellij-test/main/uk/co/reecedunn/intellij/plugin/core/tests/psi/MockPsiDocumentManagerEx.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2017, 2020 Reece H. Dunn
+ * Copyright (C) 2017, 2020-2021 Reece H. Dunn
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,69 +16,15 @@
 package uk.co.reecedunn.intellij.plugin.core.tests.psi
 
 import com.intellij.compat.psi.impl.PsiDocumentManagerBase
-import com.intellij.openapi.application.ModalityState
 import com.intellij.openapi.editor.Document
-import com.intellij.openapi.fileEditor.FileDocumentManager
 import com.intellij.openapi.project.Project
-import com.intellij.openapi.util.Computable
-import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.psi.PsiFile
 
 // NOTE: PSI document modifications rely on PsiDocumentManagerBase, which
 // MockPsiDocumentManager does not derive from.
 class MockPsiDocumentManagerEx(project: Project) : PsiDocumentManagerBase(project) {
-
-    override fun getPsiFile(document: Document): PsiFile = TODO()
-
-    override fun getDocument(file: PsiFile): Document? {
-        val vFile = file.viewProvider.virtualFile
-        return FileDocumentManager.getInstance().getDocument(vFile)
-    }
-
-    override fun getCachedDocument(file: PsiFile): Document? {
-        val vFile = file.viewProvider.virtualFile
-        return FileDocumentManager.getInstance().getCachedDocument(vFile)
-    }
-
-    override fun commitAllDocuments() {}
-
     override fun commitAllDocumentsUnderProgress(): Boolean = true
 
-    override fun performForCommittedDocument(document: Document, action: Runnable): Unit = action.run()
-
-    override fun commitDocument(document: Document) {}
-
-    override fun getLastCommittedText(document: Document): CharSequence = document.immutableCharSequence
-
-    override fun getLastCommittedStamp(document: Document): Long = document.modificationStamp
-
-    override fun getLastCommittedDocument(file: PsiFile): Document? = null
-
-    override fun getUncommittedDocuments(): Array<Document> = TODO()
-
-    override fun isUncommited(document: Document): Boolean = TODO()
-
-    override fun isCommitted(document: Document): Boolean = TODO()
-
-    override fun hasUncommitedDocuments(): Boolean = TODO()
-
-    override fun commitAndRunReadAction(runnable: Runnable): Unit = TODO()
-
-    override fun <T> commitAndRunReadAction(computation: Computable<T>): T = TODO()
-
-    override fun addListener(listener: Listener): Unit = TODO()
-
-    override fun removeListener(listener: Listener): Unit = TODO()
-
-    override fun isDocumentBlockedByPsi(doc: Document): Boolean = TODO()
-
-    override fun doPostponedOperationsAndUnblockDocument(doc: Document): Unit = TODO()
-
-    override fun performWhenAllCommitted(action: Runnable): Boolean = TODO()
-
-    override fun reparseFiles(files: Collection<VirtualFile?>, includeOpenFiles: Boolean): Unit = TODO()
-
-    override fun performLaterWhenAllCommitted(runnable: Runnable): Unit = TODO()
-
-    override fun performLaterWhenAllCommitted(runnable: Runnable, modalityState: ModalityState?): Unit = TODO()
+    override fun associatePsi(document: Document, file: PsiFile?) {
+    }
 }
