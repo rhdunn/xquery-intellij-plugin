6afb6a888153df3655da7c14ba08c6604ecd30b2
Reece H. Dunn <msclrhd@gmail.com>
2021-02-07 12:26:01 +0000

    Create an XQueryIntelliJPluginSyntaxValidator class.

---
+++ src/plugin-xijp/test/uk/co/reecedunn/intellij/plugin/xijp/tests/lang/XQueryIntelliJPluginSyntaxValidatorTests.kt
@@ -0,0 +1,105 @@
+/*
+ * Copyright (C) 2021 Reece H. Dunn
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package uk.co.reecedunn.intellij.plugin.xijp.tests.lang
+
+import com.intellij.compat.testFramework.registerServiceInstance
+import com.intellij.lang.LanguageASTFactory
+import com.intellij.openapi.Disposable
+import com.intellij.openapi.extensions.DefaultPluginDescriptor
+import com.intellij.openapi.extensions.PluginDescriptor
+import com.intellij.openapi.extensions.PluginId
+import com.intellij.psi.PsiElement
+import org.hamcrest.CoreMatchers.*
+import org.junit.jupiter.api.*
+import uk.co.reecedunn.intellij.plugin.core.extensions.PluginDescriptorProvider
+import uk.co.reecedunn.intellij.plugin.core.tests.assertion.assertThat
+import uk.co.reecedunn.intellij.plugin.core.tests.parser.ParsingTestCase
+import uk.co.reecedunn.intellij.plugin.xijp.lang.XQueryIntelliJPluginSyntaxValidator
+import uk.co.reecedunn.intellij.plugin.xpath.intellij.lang.XPath
+import uk.co.reecedunn.intellij.plugin.xquery.intellij.lang.XQuery
+import uk.co.reecedunn.intellij.plugin.xquery.ast.xquery.XQueryModule
+import uk.co.reecedunn.intellij.plugin.xquery.parser.XQueryASTFactory
+import uk.co.reecedunn.intellij.plugin.xquery.parser.XQueryParserDefinition
+import uk.co.reecedunn.intellij.plugin.xquery.intellij.settings.XQueryProjectSettings
+import uk.co.reecedunn.intellij.plugin.xpath.parser.XPathASTFactory
+import uk.co.reecedunn.intellij.plugin.xpath.parser.XPathParserDefinition
+import uk.co.reecedunn.intellij.plugin.xpm.lang.diagnostics.XpmDiagnostics
+import uk.co.reecedunn.intellij.plugin.xpm.lang.validation.XpmSyntaxValidation
+import uk.co.reecedunn.intellij.plugin.xpm.lang.validation.XpmSyntaxValidator
+
+@Suppress("ClassName")
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@DisplayName("XQuery IntelliJ Plugin - Syntax Validation - XQuery IntelliJ Plugin")
+class XQueryIntelliJPluginSyntaxValidatorTest :
+    ParsingTestCase<XQueryModule>("xqy", XQueryParserDefinition(), XPathParserDefinition()),
+    PluginDescriptorProvider,
+    XpmDiagnostics {
+    // region ParsingTestCase
+
+    @BeforeAll
+    override fun setUp() {
+        super.setUp()
+        project.registerServiceInstance(XQueryProjectSettings::class.java, XQueryProjectSettings())
+        addExplicitExtension(LanguageASTFactory.INSTANCE, XPath, XPathASTFactory())
+        addExplicitExtension(LanguageASTFactory.INSTANCE, XQuery, XQueryASTFactory())
+
+        XpmSyntaxValidator.register(this, XQueryIntelliJPluginSyntaxValidator)
+    }
+
+    @AfterAll
+    override fun tearDown() {
+        super.tearDown()
+    }
+
+    // endregion
+    // region PluginDescriptorProvider
+
+    override val pluginId: PluginId = PluginId.getId("XQueryIntelliJPluginSyntaxValidatorTest")
+
+    override val pluginDescriptor: PluginDescriptor
+        get() = DefaultPluginDescriptor(pluginId, this::class.java.classLoader)
+
+    override val pluginDisposable: Disposable
+        get() = testRootDisposable
+
+    // endregion
+    // region XpmDiagnostics
+
+    val report: StringBuffer = StringBuffer()
+
+    @BeforeEach
+    fun reset() {
+        report.delete(0, report.length)
+    }
+
+    override fun error(element: PsiElement, code: String, description: String) {
+        if (report.isNotEmpty()) {
+            report.append('\n')
+        }
+        report.append("E $code(${element.textOffset}:${element.textOffset + element.textLength}): $description")
+    }
+
+    override fun warning(element: PsiElement, code: String, description: String) {
+        if (report.isNotEmpty()) {
+            report.append('\n')
+        }
+        report.append("W $code(${element.textOffset}:${element.textOffset + element.textLength}): $description")
+    }
+
+    val validator: XpmSyntaxValidation = XpmSyntaxValidation()
+
+    // endregion
+}
