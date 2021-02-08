f5b22fd4ae0063c7f3074a24532a9f7c645ada9d
Reece H. Dunn <msclrhd@gmail.com>
2021-02-08 08:51:08 +0000

    Add a ScriptingSpec object using XpmSpecificationType.

---
+++ src/lang-xquery/main/uk/co/reecedunn/intellij/plugin/xquery/lang/ScriptingSpec.kt
@@ -0,0 +1,58 @@
+/*
+ * Copyright (C) 2021 Reece H. Dunn
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package uk.co.reecedunn.intellij.plugin.xquery.lang
+
+import com.intellij.navigation.ItemPresentation
+import uk.co.reecedunn.intellij.plugin.xpm.intellij.resources.XpmIcons
+import uk.co.reecedunn.intellij.plugin.xpm.lang.XpmSpecificationType
+import uk.co.reecedunn.intellij.plugin.xpm.lang.XpmSpecificationVersion
+import uk.co.reecedunn.intellij.plugin.xpm.lang.impl.W3CSpecification
+import javax.swing.Icon
+
+@Suppress("MemberVisibilityCanBePrivate")
+object ScriptingSpec : ItemPresentation, XpmSpecificationType {
+    // region ItemPresentation
+
+    override fun getPresentableText(): String = "XQuery Scripting Extension"
+
+    override fun getLocationString(): String? = null
+
+    override fun getIcon(unused: Boolean): Icon = XpmIcons.W3.Product
+
+    // endregion
+    // region XpmSpecificationType
+
+    override val id: String = "xquery-sx"
+
+    override val presentation: ItemPresentation
+        get() = this
+
+    // endregion
+    // region Versions
+
+    val NOTE_1_0_20140918: XpmSpecificationVersion = W3CSpecification(
+        this,
+        "1.0-20140918",
+        "1.0 (Working Group Note 18 September 2014)",
+        "http://www.w3.org/TR/2014/NOTE-xquery-sx-10-20140918/"
+    )
+
+    val versions: List<XpmSpecificationVersion> = listOf(
+        NOTE_1_0_20140918
+    )
+
+    // endregion
+}
