a9a34a7dbc25abaa0373366acbefcbdbad598401
Reece H. Dunn <msclrhd@gmail.com>
2021-02-10 08:28:07 +0000

    Add an XQuerySyntaxValidator class.

---
+++ src/lang-xquery/main/uk/co/reecedunn/intellij/plugin/xquery/lang/XQuerySyntaxValidator.kt
@@ -0,0 +1,30 @@
+/*
+ * Copyright (C) 2021 Reece H. Dunn
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package uk.co.reecedunn.intellij.plugin.xquery.lang
+
+import uk.co.reecedunn.intellij.plugin.xpm.lang.validation.XpmSyntaxErrorReporter
+import uk.co.reecedunn.intellij.plugin.xpm.lang.validation.XpmSyntaxValidationElement
+import uk.co.reecedunn.intellij.plugin.xpm.lang.validation.XpmSyntaxValidator
+
+object XQuerySyntaxValidator : XpmSyntaxValidator {
+    override fun validate(
+        element: XpmSyntaxValidationElement,
+        reporter: XpmSyntaxErrorReporter
+    ): Unit = when (element) {
+        else -> {
+        }
+    }
+}
