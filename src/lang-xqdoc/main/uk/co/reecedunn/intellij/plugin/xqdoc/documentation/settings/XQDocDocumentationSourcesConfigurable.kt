cbd1ba833269ee33ad3d1769eeea77f2cc5df042
Reece H. Dunn <msclrhd@gmail.com>
2021-03-08 19:42:39 +0000

    Move intellij.settings.documentation to documentation.settings.

--- src/lang-xqdoc/main/uk/co/reecedunn/intellij/plugin/xqdoc/documentation/settings/XQDocDocumentationSourcesConfigurable.kt
+++ src/lang-xqdoc/main/uk/co/reecedunn/intellij/plugin/xqdoc/documentation/settings/XQDocDocumentationSourcesConfigurable.kt
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package uk.co.reecedunn.intellij.plugin.xqdoc.intellij.settings.documentation
+package uk.co.reecedunn.intellij.plugin.xqdoc.documentation.settings
 
 import com.intellij.icons.AllIcons
 import com.intellij.openapi.fileChooser.FileChooserDescriptorFactory
