aa762d9ff8cb210834d97404778a4181160a02b3
Reece H. Dunn <msclrhd@gmail.com>
2021-03-08 19:57:54 +0000

    Move intellij.lang.injection to lang.injection.

--- src/lang-xpath/main/uk/co/reecedunn/intellij/plugin/xpath/lang/injection/XPathLanguageInjectionSupport.kt
+++ src/lang-xpath/main/uk/co/reecedunn/intellij/plugin/xpath/lang/injection/XPathLanguageInjectionSupport.kt
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package uk.co.reecedunn.intellij.plugin.xpath.intellij.lang.injection
+package uk.co.reecedunn.intellij.plugin.xpath.lang.injection
 
 import com.intellij.psi.PsiLanguageInjectionHost
 import org.intellij.plugins.intelliLang.inject.AbstractLanguageInjectionSupport
--- src/lang-xslt/main/uk/co/reecedunn/intellij/plugin/xslt/lang/injection/XsltSchemaTypeLanguageInjection.kt
+++ src/lang-xslt/main/uk/co/reecedunn/intellij/plugin/xslt/lang/injection/XsltSchemaTypeLanguageInjection.kt
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package uk.co.reecedunn.intellij.plugin.xslt.intellij.lang.injection
+package uk.co.reecedunn.intellij.plugin.xslt.lang.injection
 
 import com.intellij.lang.injection.MultiHostInjector
 import com.intellij.lang.injection.MultiHostRegistrar
