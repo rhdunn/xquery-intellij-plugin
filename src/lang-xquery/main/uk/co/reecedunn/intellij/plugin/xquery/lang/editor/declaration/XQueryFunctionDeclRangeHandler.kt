8f02bb25e645116ee77ea28c3c72288b1dd20c3b
Reece H. Dunn <msclrhd@gmail.com>
2021-03-09 08:36:08 +0000

    Move intellij.codeInsight.hint to lang.editor.declaration.

--- src/lang-xquery/main/uk/co/reecedunn/intellij/plugin/xquery/lang/editor/declaration/XQueryFunctionDeclRangeHandler.kt
+++ src/lang-xquery/main/uk/co/reecedunn/intellij/plugin/xquery/lang/editor/declaration/XQueryFunctionDeclRangeHandler.kt
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package uk.co.reecedunn.intellij.plugin.xquery.intellij.codeInsight.hint
+package uk.co.reecedunn.intellij.plugin.xquery.lang.editor.declaration
 
 import com.intellij.codeInsight.hint.DeclarationRangeHandler
 import com.intellij.openapi.util.TextRange
--- src/lang-xquery/test/uk/co/reecedunn/intellij/plugin/xquery/tests/lang/editor/declaration/XQueryDeclarationRangeHandlerTest.kt
+++ src/lang-xquery/test/uk/co/reecedunn/intellij/plugin/xquery/tests/lang/editor/declaration/XQueryDeclarationRangeHandlerTest.kt
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package uk.co.reecedunn.intellij.plugin.xquery.tests.intellij.codeInsight.hint
+package uk.co.reecedunn.intellij.plugin.xquery.tests.lang.editor.declaration
 
 import com.intellij.openapi.extensions.PluginId
 import org.hamcrest.CoreMatchers.`is`
@@ -22,7 +22,7 @@ import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 import uk.co.reecedunn.intellij.plugin.core.tests.assertion.assertThat
-import uk.co.reecedunn.intellij.plugin.xquery.intellij.codeInsight.hint.XQueryFunctionDeclRangeHandler
+import uk.co.reecedunn.intellij.plugin.xquery.lang.editor.declaration.XQueryFunctionDeclRangeHandler
 import uk.co.reecedunn.intellij.plugin.xquery.ast.xquery.XQueryFunctionDecl
 import uk.co.reecedunn.intellij.plugin.xquery.tests.parser.ParserTestCase
 
