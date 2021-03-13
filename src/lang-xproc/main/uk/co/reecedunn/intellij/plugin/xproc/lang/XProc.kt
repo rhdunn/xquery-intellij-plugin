4753669c220d60cda518ff6c1cecc7e8931cf07d
Reece H. Dunn <msclrhd@gmail.com>
2021-03-13 13:55:41 +0000

    Add an XProc language class.

---
+++ src/lang-xproc/main/uk/co/reecedunn/intellij/plugin/xproc/lang/XProc.kt
@@ -0,0 +1,37 @@
+/*
+ * Copyright (C) 2021 Reece H. Dunn
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package uk.co.reecedunn.intellij.plugin.xproc.lang
+
+import com.intellij.lang.xml.XMLLanguage
+import com.intellij.openapi.fileTypes.LanguageFileType
+
+/**
+ * XML Stylesheet Language: Transform
+ */
+@Suppress("MemberVisibilityCanBePrivate")
+object XProc : XMLLanguage(INSTANCE, "XProc", "application/xproc+xml") {
+    // region Language
+
+    const val NAMESPACE: String = "http://www.w3.org/ns/xproc"
+
+    override fun isCaseSensitive(): Boolean = true
+
+    override fun getDisplayName(): String = "XProc"
+
+    override fun getAssociatedFileType(): LanguageFileType? = null
+
+    // endregion
+}
