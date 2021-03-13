4b3db13ee577cebc22fc82cb341cac5097293126
Reece H. Dunn <msclrhd@gmail.com>
2021-03-13 17:51:43 +0000

    Add an XProc file icon patcher for xml extension XProc files.

--- src/lang-xproc/main/uk/co/reecedunn/intellij/plugin/xproc/lang/fileTypes/XProcFileIconPatcher.kt
+++ src/lang-xproc/main/uk/co/reecedunn/intellij/plugin/xproc/lang/fileTypes/XProcFileIconPatcher.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2020-2021 Reece H. Dunn
+ * Copyright (C) 2021 Reece H. Dunn
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,26 +13,24 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package uk.co.reecedunn.intellij.plugin.xslt.lang.fileTypes
+package uk.co.reecedunn.intellij.plugin.xproc.lang.fileTypes
 
 import com.intellij.ide.FileIconPatcher
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.psi.PsiManager
 import com.intellij.psi.xml.XmlFile
-import uk.co.reecedunn.intellij.plugin.xslt.lang.XSLT
-import uk.co.reecedunn.intellij.plugin.xslt.lang.isIntellijXPathPluginEnabled
-import uk.co.reecedunn.intellij.plugin.xslt.resources.XsltIcons
+import uk.co.reecedunn.intellij.plugin.xproc.lang.XProc
+import uk.co.reecedunn.intellij.plugin.xproc.resources.XProcIcons
 import javax.swing.Icon
 
-class XsltFileIconPatcher : FileIconPatcher {
+class XProcFileIconPatcher : FileIconPatcher {
     override fun patchIcon(baseIcon: Icon, file: VirtualFile?, flags: Int, project: Project?): Icon = when {
-        isIntellijXPathPluginEnabled() -> baseIcon
         project == null || file == null -> baseIcon
         else -> {
             (PsiManager.getInstance(project).findFile(file) as? XmlFile)?.rootTag?.let {
-                if (it.namespace == XSLT.NAMESPACE) {
-                    XsltIcons.FileType
+                if (it.namespace == XProc.NAMESPACE) {
+                    XProcIcons.FileType
                 } else {
                     null
                 }
