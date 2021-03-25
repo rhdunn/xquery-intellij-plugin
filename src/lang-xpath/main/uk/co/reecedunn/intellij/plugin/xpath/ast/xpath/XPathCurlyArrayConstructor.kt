bcd482ec4536282a560c04c419abac254bf2f3ac
Reece H. Dunn <msclrhd@gmail.com>
2021-03-25 23:18:50 +0000

    Move XpmExpression to XPathArrayConstructor.

--- src/lang-xpath/main/uk/co/reecedunn/intellij/plugin/xpath/ast/xpath/XPathCurlyArrayConstructor.kt
+++ src/lang-xpath/main/uk/co/reecedunn/intellij/plugin/xpath/ast/xpath/XPathCurlyArrayConstructor.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2016-2017, 2020 Reece H. Dunn
+ * Copyright (C) 2016-2017 Reece H. Dunn
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -15,9 +15,7 @@
  */
 package uk.co.reecedunn.intellij.plugin.xpath.ast.xpath
 
-import uk.co.reecedunn.intellij.plugin.xpm.optree.expr.XpmExpression
-
 /**
  * An XPath 3.1 and XQuery 3.1 `CurlyArrayConstructor` node in the XQuery AST.
  */
-interface XPathCurlyArrayConstructor : XPathArrayConstructor, XpmExpression
+interface XPathCurlyArrayConstructor : XPathArrayConstructor
