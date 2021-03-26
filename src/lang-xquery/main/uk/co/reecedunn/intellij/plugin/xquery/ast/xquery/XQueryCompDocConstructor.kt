089899ed30600fee95f14bea2885958e93217501
Reece H. Dunn <msclrhd@gmail.com>
2021-03-26 08:21:58 +0000

    Don't derive AST nodes from XdmDocumentNode.

--- src/lang-xquery/main/uk/co/reecedunn/intellij/plugin/xquery/ast/xquery/XQueryCompDocConstructor.kt
+++ src/lang-xquery/main/uk/co/reecedunn/intellij/plugin/xquery/ast/xquery/XQueryCompDocConstructor.kt
@@ -15,10 +15,9 @@
  */
 package uk.co.reecedunn.intellij.plugin.xquery.ast.xquery
 
-import uk.co.reecedunn.intellij.plugin.xdm.types.XdmDocumentNode
 import uk.co.reecedunn.intellij.plugin.xpm.optree.expr.XpmExpression
 
 /**
  * An XQuery 1.0 `CompDocConstructor` node in the XQuery AST.
  */
-interface XQueryCompDocConstructor : XQueryComputedConstructor, XdmDocumentNode, XpmExpression
+interface XQueryCompDocConstructor : XQueryComputedConstructor, XpmExpression
