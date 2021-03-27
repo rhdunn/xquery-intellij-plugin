a5aa5309f4839360cd7f7fef068b97e4ed6eb835
Reece H. Dunn <msclrhd@gmail.com>
2021-03-27 18:43:11 +0000

    Use user data for configurations properties.

--- src/lang-xpm/main/uk/co/reecedunn/intellij/plugin/xpm/project/configuration/XpmProjectConfigurations.kt
+++ src/lang-xpm/main/uk/co/reecedunn/intellij/plugin/xpm/project/configuration/XpmProjectConfigurations.kt
@@ -22,24 +22,33 @@ import com.intellij.openapi.extensions.ExtensionPointListener
 import com.intellij.openapi.extensions.PluginDescriptor
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.roots.ProjectRootManager
+import com.intellij.openapi.util.Key
 import com.intellij.openapi.vfs.LocalFileSystem
 import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.psi.PsiDirectory
-import uk.co.reecedunn.intellij.plugin.core.data.CacheableProperty
+import uk.co.reecedunn.intellij.plugin.core.util.UserDataHolderBase
 import uk.co.reecedunn.intellij.plugin.core.vfs.isAncestorOf
 import uk.co.reecedunn.intellij.plugin.core.vfs.relativePathTo
 
 class XpmProjectConfigurations(private val project: Project) :
+    UserDataHolderBase(),
     ExtensionPointListener<XpmProjectConfigurationFactoryBean>,
     Disposable {
+    companion object {
+        private val CONFIGURATIONS = Key.create<List<XpmProjectConfiguration>>("CONFIGURATIONS")
+
+        fun getInstance(project: Project): XpmProjectConfigurations {
+            return ServiceManager.getService(project, XpmProjectConfigurations::class.java)
+        }
+    }
     // region ExtensionPointListener
 
     override fun extensionAdded(extension: XpmProjectConfigurationFactoryBean, pluginDescriptor: PluginDescriptor) {
-        cachedConfigurations.invalidate()
+        clearUserData(CONFIGURATIONS)
     }
 
     override fun extensionRemoved(extension: XpmProjectConfigurationFactoryBean, pluginDescriptor: PluginDescriptor) {
-        cachedConfigurations.invalidate()
+        clearUserData(CONFIGURATIONS)
     }
 
     // endregion
@@ -50,20 +59,18 @@ class XpmProjectConfigurations(private val project: Project) :
 
     // endregion
 
-    private val cachedConfigurations = CacheableProperty {
-        val configurations = ArrayList<XpmProjectConfiguration>()
-        ProjectRootManager.getInstance(project).fileIndex.iterateContent { vf ->
-            if (!vf.isDirectory) return@iterateContent true
-            XpmProjectConfigurationFactory.EP_NAME.extensionList.forEach {
-                it.getInstance().create(project, vf)?.let { configuration -> configurations.add(configuration) }
-            }
-            true
-        }
-        configurations
-    }
-
     val configurations: Sequence<XpmProjectConfiguration>
-        get() = cachedConfigurations.get()!!.asSequence()
+        get() = computeUserDataIfAbsent(CONFIGURATIONS) {
+            val configurations = ArrayList<XpmProjectConfiguration>()
+            ProjectRootManager.getInstance(project).fileIndex.iterateContent { vf ->
+                if (!vf.isDirectory) return@iterateContent true
+                XpmProjectConfigurationFactory.EP_NAME.extensionList.forEach {
+                    it.getInstance().create(project, vf)?.let { configuration -> configurations.add(configuration) }
+                }
+                true
+            }
+            configurations
+        }.asSequence()
 
     val applicationName: String?
         get() = configurations.mapNotNull { it.applicationName }.firstOrNull()
@@ -94,11 +101,5 @@ class XpmProjectConfigurations(private val project: Project) :
 
     init {
         XpmProjectConfigurationFactory.EP_NAME.extensionPoint.addExtensionPointListener(this, false, this)
-    }
-
-    companion object {
-        fun getInstance(project: Project): XpmProjectConfigurations {
-            return ServiceManager.getService(project, XpmProjectConfigurations::class.java)
-        }
     }
 }
