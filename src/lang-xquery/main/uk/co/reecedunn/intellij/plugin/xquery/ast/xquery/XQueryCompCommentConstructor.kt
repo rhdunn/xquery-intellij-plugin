a1fe9422583d7095e36073d92717a919fb5390d7
Reece H. Dunn <msclrhd@gmail.com>
2021-08-13 08:16:24 +0100

    Implement XdmCommentNode on XQueryCompCommentConstructor.

--- src/lang-xquery/main/uk/co/reecedunn/intellij/plugin/xquery/ast/xquery/XQueryCompCommentConstructor.kt
+++ src/lang-xquery/main/uk/co/reecedunn/intellij/plugin/xquery/ast/xquery/XQueryCompCommentConstructor.kt
@@ -15,7 +15,9 @@
  */
 package uk.co.reecedunn.intellij.plugin.xquery.ast.xquery
 
+import uk.co.reecedunn.intellij.plugin.xdm.types.XdmCommentNode
+
 /**
  * An XQuery 1.0 `CompCommentConstructor` node in the XQuery AST.
  */
-interface XQueryCompCommentConstructor : XQueryComputedConstructor
+interface XQueryCompCommentConstructor : XQueryComputedConstructor, XdmCommentNode
