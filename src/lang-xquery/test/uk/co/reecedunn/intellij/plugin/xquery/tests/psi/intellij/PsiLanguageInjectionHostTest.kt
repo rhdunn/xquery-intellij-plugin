b473229bdc13e6287c1c006d900df4e632872574
Reece H. Dunn <msclrhd@gmail.com>
2021-08-21 11:09:12 +0100

    Rename/move XQuery psi.LanguageInjectionPsiTest to psi.intellij.PsiLanguageInjectionHostTest.

--- src/lang-xquery/test/uk/co/reecedunn/intellij/plugin/xquery/tests/psi/intellij/PsiLanguageInjectionHostTest.kt
+++ src/lang-xquery/test/uk/co/reecedunn/intellij/plugin/xquery/tests/psi/intellij/PsiLanguageInjectionHostTest.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2020 Reece H. Dunn
+ * Copyright (C) 2020-2021 Reece H. Dunn
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package uk.co.reecedunn.intellij.plugin.xquery.tests.psi
+package uk.co.reecedunn.intellij.plugin.xquery.tests.psi.intellij
 
 import com.intellij.openapi.extensions.PluginId
 import com.intellij.openapi.util.TextRange
@@ -31,9 +31,9 @@ import uk.co.reecedunn.intellij.plugin.xquery.ast.xquery.XQueryDirAttributeValue
 import uk.co.reecedunn.intellij.plugin.xquery.tests.parser.ParserTestCase
 
 @Suppress("RedundantVisibilityModifier")
-@DisplayName("XQuery IntelliJ Plugin - Language Injection Host - XPath")
-class LanguageInjectionPsiTest : ParserTestCase() {
-    override val pluginId: PluginId = PluginId.getId("LanguageInjectionPsiTest")
+@DisplayName("IntelliJ Program Structure Interface (PSI) - PsiLanguageInjectionHost - XQuery")
+class PsiLanguageInjectionHostTest : ParserTestCase() {
+    override val pluginId: PluginId = PluginId.getId("PsiLanguageInjectionHostTest")
 
     @Nested
     @DisplayName("relevant text range")
