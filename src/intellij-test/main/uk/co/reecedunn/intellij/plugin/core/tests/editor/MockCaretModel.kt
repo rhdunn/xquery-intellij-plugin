6498c7b7865d9332dd92fd6f525ec1094fb0545a
Reece H. Dunn <msclrhd@gmail.com>
2021-09-13 18:11:06 +0100

    Remove the CaretModel compatibility API.

--- src/intellij-test/main/uk/co/reecedunn/intellij/plugin/core/tests/editor/MockCaretModel.kt
+++ src/intellij-test/main/uk/co/reecedunn/intellij/plugin/core/tests/editor/MockCaretModel.kt
@@ -15,7 +15,6 @@
  */
 package uk.co.reecedunn.intellij.plugin.core.tests.editor
 
-import com.intellij.compat.openapi.editor.CaretModel
 import com.intellij.openapi.Disposable
 import com.intellij.openapi.editor.*
 import com.intellij.openapi.editor.event.CaretListener
