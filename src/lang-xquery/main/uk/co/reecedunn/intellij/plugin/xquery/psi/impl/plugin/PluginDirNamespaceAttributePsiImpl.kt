b168c711d7bbaa68dca158533f5f4c951c3354dd
Reece H. Dunn <msclrhd@gmail.com>
2021-09-16 13:28:10 +0100

    Simplify the DirNamespaceAttribute implementation, now it is not part of DirAttribute.

--- src/lang-xquery/main/uk/co/reecedunn/intellij/plugin/xquery/psi/impl/plugin/PluginDirNamespaceAttributePsiImpl.kt
+++ src/lang-xquery/main/uk/co/reecedunn/intellij/plugin/xquery/psi/impl/plugin/PluginDirNamespaceAttributePsiImpl.kt
@@ -23,8 +23,6 @@ import uk.co.reecedunn.intellij.plugin.core.sequences.children
 import uk.co.reecedunn.intellij.plugin.xdm.module.path.XdmModuleType
 import uk.co.reecedunn.intellij.plugin.xdm.types.*
 import uk.co.reecedunn.intellij.plugin.xdm.types.impl.psi.XsAnyUri
-import uk.co.reecedunn.intellij.plugin.xdm.types.impl.psi.XsID
-import uk.co.reecedunn.intellij.plugin.xdm.types.impl.psi.XsUntypedAtomic
 import uk.co.reecedunn.intellij.plugin.xpm.module.loader.resolve
 import uk.co.reecedunn.intellij.plugin.xpm.module.resolveUri
 import uk.co.reecedunn.intellij.plugin.xpm.optree.namespace.XdmNamespaceType
@@ -71,20 +69,9 @@ class PluginDirNamespaceAttributePsiImpl(node: ASTNode) :
     override val typedValue: XsAnyAtomicType?
         get() = computeUserDataIfAbsent(TYPED_VALUE) {
             val contents = stringValue ?: return@computeUserDataIfAbsent Optional.empty()
-            val qname = nodeName ?: return@computeUserDataIfAbsent Optional.empty()
-            val ret = when {
-                qname.prefix?.data == "xmlns" -> {
-                    XsAnyUri(contents, XdmUriContext.NamespaceDeclaration, XdmModuleType.MODULE_OR_SCHEMA, this)
-                }
-                qname.localName?.data == "xmlns" && qname.prefix == null -> {
-                    XsAnyUri(contents, XdmUriContext.NamespaceDeclaration, XdmModuleType.MODULE_OR_SCHEMA, this)
-                }
-                qname.prefix?.data == "xml" && qname.localName?.data == "id" -> {
-                    XsID(contents, this)
-                }
-                else -> XsUntypedAtomic(contents, this)
-            }
-            Optional.of(ret)
+            Optional.of(
+                XsAnyUri(contents, XdmUriContext.NamespaceDeclaration, XdmModuleType.MODULE_OR_SCHEMA, this)
+            )
         }.orElse(null)
 
     // endregion
@@ -102,16 +89,12 @@ class PluginDirNamespaceAttributePsiImpl(node: ASTNode) :
     // endregion
     // region XQueryNamespaceDeclaration
 
-    override fun accepts(namespaceType: XdmNamespaceType): Boolean {
-        val qname = nodeName ?: return false
-        return when {
-            qname.prefix?.data == "xmlns" -> namespaceType === XdmNamespaceType.Prefixed
-            qname.localName?.data == "xmlns" && qname.prefix == null -> when (namespaceType) {
-                XdmNamespaceType.DefaultElement, XdmNamespaceType.DefaultType -> true
-                else -> false
-            }
-            else -> namespaceType === XdmNamespaceType.Undefined
+    override fun accepts(namespaceType: XdmNamespaceType): Boolean = when (namespacePrefix) {
+        null -> when (namespaceType) {
+            XdmNamespaceType.DefaultElement, XdmNamespaceType.DefaultType -> true
+            else -> false
         }
+        else -> namespaceType === XdmNamespaceType.Prefixed
     }
 
     override val namespacePrefix: XsNCNameValue?
