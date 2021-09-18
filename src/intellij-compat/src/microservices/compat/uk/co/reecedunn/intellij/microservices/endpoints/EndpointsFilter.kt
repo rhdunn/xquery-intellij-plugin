20b5ef127fb25936e798e9f92d31ebbeec4ed9bf
Reece H. Dunn <msclrhd@gmail.com>
2021-09-18 14:23:50 +0100

    Create an EndpointsFilter compatibility API.

--- src/intellij-compat/src/microservices/compat/uk/co/reecedunn/intellij/microservices/endpoints/EndpointsFilter.kt
+++ src/intellij-compat/src/microservices/compat/uk/co/reecedunn/intellij/microservices/endpoints/EndpointsFilter.kt
@@ -15,13 +15,4 @@
  */
 package uk.co.reecedunn.intellij.microservices.endpoints
 
-typealias EndpointsFilter = com.intellij.microservices.endpoints.EndpointsFilter
-
-typealias EndpointsProviderStatus = com.intellij.microservices.endpoints.EndpointsProvider.Status
-
-typealias EndpointType = com.intellij.microservices.endpoints.EndpointType
-
-typealias FrameworkPresentation = com.intellij.microservices.endpoints.FrameworkPresentation
-
-val HTTP_SERVER_TYPE: EndpointType
-    get() = com.intellij.microservices.endpoints.HTTP_SERVER_TYPE
+interface EndpointsFilter
