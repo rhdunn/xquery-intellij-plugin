bb3dda80b30ccad548ad5853d2fc792aa67351a4
Reece H. Dunn <msclrhd@gmail.com>
2021-09-18 17:07:35 +0100

    Cache the endpoint groups in RestXqEndpointsFramework.

--- src/plugin-exquery/main/uk/co/reecedunn/intellij/plugin/exquery/restxq/endpoints/RestXqEndpointsFramework.kt
+++ src/plugin-exquery/main/uk/co/reecedunn/intellij/plugin/exquery/restxq/endpoints/RestXqEndpointsFramework.kt
@@ -17,14 +17,18 @@ package uk.co.reecedunn.intellij.plugin.exquery.restxq.endpoints
 
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.roots.ProjectRootManager
+import com.intellij.psi.util.CachedValueProvider
+import com.intellij.psi.util.CachedValuesManager
+import com.intellij.psi.util.PsiModificationTracker
 import uk.co.reecedunn.intellij.microservices.endpoints.FrameworkPresentation
+import uk.co.reecedunn.intellij.plugin.core.util.UserDataHolderBase
 import uk.co.reecedunn.intellij.plugin.core.vfs.toPsiFile
 import uk.co.reecedunn.intellij.plugin.exquery.resources.EXQueryBundle
 import uk.co.reecedunn.intellij.plugin.exquery.resources.EXQueryIcons
 import uk.co.reecedunn.intellij.plugin.xquery.ast.xquery.XQueryLibraryModule
 import uk.co.reecedunn.intellij.plugin.xquery.ast.xquery.XQueryModule
 
-object RestXqEndpointsFramework {
+object RestXqEndpointsFramework : UserDataHolderBase() {
     val presentation: FrameworkPresentation = FrameworkPresentation(
         "xijp.exquery-restxq",
         EXQueryBundle.message("endpoints.restxq.label"),
@@ -32,17 +36,19 @@ object RestXqEndpointsFramework {
     )
 
     fun groups(project: Project): List<RestXqEndpointsGroup> {
-        val groups = ArrayList<RestXqEndpointsGroup>()
-        ProjectRootManager.getInstance(project).fileIndex.iterateContent {
-            val module = it.toPsiFile(project) as? XQueryModule
-            (module?.mainOrLibraryModule as? XQueryLibraryModule)?.prolog?.forEach { prolog ->
-                val group = RestXqEndpointsGroup(prolog)
-                if (group.endpoints.any()) {
-                    groups.add(group)
+        return CachedValuesManager.getManager(project).getCachedValue(this, RestXqEndpointsProvider.GROUPS, {
+            val groups = ArrayList<RestXqEndpointsGroup>()
+            ProjectRootManager.getInstance(project).fileIndex.iterateContent {
+                val module = it.toPsiFile(project) as? XQueryModule
+                (module?.mainOrLibraryModule as? XQueryLibraryModule)?.prolog?.forEach { prolog ->
+                    val group = RestXqEndpointsGroup(prolog)
+                    if (group.endpoints.any()) {
+                        groups.add(group)
+                    }
                 }
+                true
             }
-            true
-        }
-        return groups
+            CachedValueProvider.Result.create(groups, PsiModificationTracker.MODIFICATION_COUNT)
+        }, false)
     }
 }
