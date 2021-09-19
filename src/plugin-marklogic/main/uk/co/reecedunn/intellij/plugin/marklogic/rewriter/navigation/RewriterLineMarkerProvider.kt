d9d337cbbda8157c7619f213de999ddb97801831
Reece H. Dunn <msclrhd@gmail.com>
2021-09-19 12:18:20 +0100

    Place the rewriter line markers on the first leaf element of the QueryBody.

--- src/plugin-marklogic/main/uk/co/reecedunn/intellij/plugin/marklogic/rewriter/navigation/RewriterLineMarkerProvider.kt
+++ src/plugin-marklogic/main/uk/co/reecedunn/intellij/plugin/marklogic/rewriter/navigation/RewriterLineMarkerProvider.kt
@@ -19,6 +19,7 @@ import com.intellij.codeInsight.daemon.LineMarkerInfo
 import com.intellij.codeInsight.daemon.LineMarkerProvider
 import com.intellij.codeInsight.navigation.NavigationGutterIconBuilder
 import com.intellij.psi.PsiElement
+import com.intellij.psi.impl.source.tree.LeafElement
 import uk.co.reecedunn.intellij.plugin.marklogic.resources.MarkLogicIcons
 import uk.co.reecedunn.intellij.plugin.processor.resources.PluginApiBundle
 import uk.co.reecedunn.intellij.plugin.marklogic.rewriter.endpoints.Rewriter
@@ -26,9 +27,10 @@ import uk.co.reecedunn.intellij.plugin.xquery.ast.xquery.XQueryQueryBody
 
 class RewriterLineMarkerProvider : LineMarkerProvider {
     override fun getLineMarkerInfo(element: PsiElement): LineMarkerInfo<*>? {
-        if (element !is XQueryQueryBody) return null
+        if (element !is LeafElement) return null
 
-        val targets = getModuleUriElements(element)
+        val body = getQueryBody(element) ?: return null
+        val targets = getModuleUriElements(body)
         if (targets.isEmpty()) return null
 
         return NavigationGutterIconBuilder.create(MarkLogicIcons.Markers.Endpoint)
@@ -36,6 +38,12 @@ class RewriterLineMarkerProvider : LineMarkerProvider {
             .setTooltipText(PluginApiBundle.message("line-marker.rewriter-endpoint.tooltip-text"))
             .setCellRenderer(RewriterListCellRenderer)
             .createLineMarkerInfo(element)
+    }
+
+    private fun getQueryBody(element: PsiElement): XQueryQueryBody? = when {
+        element is XQueryQueryBody -> element
+        element.prevSibling != null -> null
+        else -> element.parent?.let { getQueryBody(it) }
     }
 
     private fun getModuleUriElements(element: XQueryQueryBody): List<PsiElement> {
