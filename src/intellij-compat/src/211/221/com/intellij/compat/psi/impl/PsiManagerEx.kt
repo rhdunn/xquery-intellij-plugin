5807838282fcf4b6a30ac25d1704dca29d4be98d
Reece H. Dunn <msclrhd@gmail.com>
2022-01-29 09:57:51 +0000

    Suppress the deprecated warning for Computable in the PsiManagerEx compatibility API.

--- src/intellij-compat/src/211/221/com/intellij/compat/psi/impl/PsiManagerEx.kt
+++ src/intellij-compat/src/211/221/com/intellij/compat/psi/impl/PsiManagerEx.kt
@@ -13,6 +13,8 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+@file:Suppress("DEPRECATION") // Computable is deprecated by IntelliJ.
+
 package com.intellij.compat.psi.impl
 
 import com.intellij.openapi.util.Computable
