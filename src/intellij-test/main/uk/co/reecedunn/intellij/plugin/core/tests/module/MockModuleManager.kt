ade7afdcb9258a25a2178e769130e8e8d1950e5c
Reece H. Dunn <msclrhd@gmail.com>
2022-09-24 18:56:13 +0100

    Create a MockModuleManager compat class for IntelliJ 2022.3 EAP 1.

--- src/intellij-test/main/uk/co/reecedunn/intellij/plugin/core/tests/module/MockModuleManager.kt
+++ src/intellij-test/main/uk/co/reecedunn/intellij/plugin/core/tests/module/MockModuleManager.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2018 Reece H. Dunn
+ * Copyright (C) 2018, 2022 Reece H. Dunn
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -15,73 +15,20 @@
  */
 package uk.co.reecedunn.intellij.plugin.core.tests.module
 
+import com.intellij.compat.openapi.module.MockModuleManager
 import uk.co.reecedunn.intellij.plugin.core.extensions.registerServiceInstance
 import com.intellij.openapi.module.*
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.roots.ModuleRootManager
 import com.intellij.openapi.vfs.VirtualFile
-import com.intellij.util.graph.Graph
-import org.jetbrains.annotations.ApiStatus
+import org.jetbrains.annotations.TestOnly
 import uk.co.reecedunn.intellij.plugin.core.tests.roots.MockModuleRootsManager
-import java.nio.file.Path
 
-@Suppress("NonExtendableApiUsage")
-class MockModuleManager(private val project: Project) : ModuleManager() {
-    private var modules: Array<Module> = arrayOf()
-
-    fun addModule(moduleFile: VirtualFile): Module {
+@TestOnly
+class MockModuleManager(private val project: Project) : MockModuleManager() {
+    override fun createModule(moduleFile: VirtualFile): Module {
         val module = MockModule(project, moduleFile)
         module.registerServiceInstance(ModuleRootManager::class.java, MockModuleRootsManager(module))
-        modules = arrayOf(module, *modules)
         return module
     }
-
-    @Suppress("UnstableApiUsage")
-    override fun setUnloadedModules(unloadedModuleNames: MutableList<String>): Unit = TODO()
-
-    override fun getModifiableModel(): ModifiableModuleModel = TODO()
-
-    override fun newModule(filePath: String, moduleTypeId: String): Module = TODO()
-
-    override fun getModuleDependentModules(module: Module): MutableList<Module> = TODO()
-
-    override fun moduleDependencyComparator(): Comparator<Module> = TODO()
-
-    override fun moduleGraph(): Graph<Module> = TODO()
-
-    override fun moduleGraph(includeTests: Boolean): Graph<Module> = TODO()
-
-    @ApiStatus.Experimental
-    @Suppress("UnstableApiUsage")
-    override fun getUnloadedModuleDescriptions(): MutableCollection<UnloadedModuleDescription> = TODO()
-
-    override fun hasModuleGroups(): Boolean = TODO()
-
-    override fun isModuleDependent(module: Module, onModule: Module): Boolean = TODO()
-
-    @ApiStatus.Experimental
-    @Suppress("UnstableApiUsage")
-    override fun getAllModuleDescriptions(): MutableCollection<ModuleDescription> = TODO()
-
-    override fun getModuleGroupPath(module: Module): Array<String> = TODO()
-
-    @ApiStatus.Experimental
-    @Suppress("UnstableApiUsage")
-    override fun getModuleGrouper(model: ModifiableModuleModel?): ModuleGrouper = TODO()
-
-    override fun loadModule(filePath: String): Module = TODO()
-
-    override fun loadModule(file: Path): Module = TODO()
-
-    @ApiStatus.Experimental
-    @Suppress("UnstableApiUsage")
-    override fun getUnloadedModuleDescription(moduleName: String): UnloadedModuleDescription = TODO()
-
-    override fun getModules(): Array<Module> = modules
-
-    override fun getSortedModules(): Array<Module> = modules
-
-    override fun findModuleByName(name: String): Module = TODO()
-
-    override fun disposeModule(module: Module): Unit = TODO()
 }
