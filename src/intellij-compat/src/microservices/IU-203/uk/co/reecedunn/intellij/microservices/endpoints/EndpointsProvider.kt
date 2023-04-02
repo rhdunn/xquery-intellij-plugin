3f2cd9563d74f1ddfaacbaf2dafaf6b9325d83c7
Reece H. Dunn <msclrhd@gmail.com>
2023-04-02 17:27:04 +0100

    Implement EndpointProvider.DOCUMENTATION_ELEMENT in terms of EndpointsProvider.getDocumentationElement.

--- src/intellij-compat/src/microservices/IU-203/uk/co/reecedunn/intellij/microservices/endpoints/EndpointsProvider.kt
+++ src/intellij-compat/src/microservices/IU-203/uk/co/reecedunn/intellij/microservices/endpoints/EndpointsProvider.kt
@@ -1,13 +1,17 @@
 // Copyright (C) 2023 Reece H. Dunn. SPDX-License-Identifier: Apache-2.0
 package uk.co.reecedunn.intellij.microservices.endpoints
 
+import com.intellij.microservices.endpoints.EndpointsProvider.Companion.DOCUMENTATION_ELEMENT
 import com.intellij.psi.PsiElement
 
 @Suppress("UnstableApiUsage")
 interface EndpointsProvider<Group : Any, Endpoint : Any> :
     com.intellij.microservices.endpoints.EndpointsProvider<Group, Endpoint> {
 
-    override fun getEndpointData(group: Group, endpoint: Endpoint, dataId: String): Any? = null
+    override fun getEndpointData(group: Group, endpoint: Endpoint, dataId: String): Any? = when (dataId) {
+        DOCUMENTATION_ELEMENT.name -> getDocumentationElement(group, endpoint)
+        else -> null
+    }
 
     fun getDocumentationElement(group: Group, endpoint: Endpoint): PsiElement? = null
 
