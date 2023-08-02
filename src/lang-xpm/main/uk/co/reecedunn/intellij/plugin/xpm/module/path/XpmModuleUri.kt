d886647430228693831ef911cfe9fe25130d0083
Reece H. Dunn <msclrhd@gmail.com>
2023-08-02 18:32:27 +0100

    Fix an error when a stack trace points to a module URI that does not resolve to an IDE file.

--- src/lang-xpm/main/uk/co/reecedunn/intellij/plugin/xpm/module/path/XpmModuleUri.kt
+++ src/lang-xpm/main/uk/co/reecedunn/intellij/plugin/xpm/module/path/XpmModuleUri.kt
@@ -1,18 +1,4 @@
-/*
- * Copyright (C) 2019-2020 Reece H. Dunn
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright (C) 2019-2020, 2023 Reece H. Dunn. SPDX-License-Identifier: Apache-2.0
 package uk.co.reecedunn.intellij.plugin.xpm.module.path
 
 import com.intellij.openapi.vfs.VirtualFile
@@ -28,7 +14,7 @@ class XpmModuleUri(val contextFile: VirtualFile?, private val path: String) : Vi
 
     override fun refresh(asynchronous: Boolean, recursive: Boolean, postRunnable: Runnable?): Unit = TODO()
 
-    override fun getLength(): Long = TODO()
+    override fun getLength(): Long = 0
 
     override fun getFileSystem(): VirtualFileSystem = TODO()
 
