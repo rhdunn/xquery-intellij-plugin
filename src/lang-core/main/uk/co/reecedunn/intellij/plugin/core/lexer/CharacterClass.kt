63dcd02646c72f6c9a741b5558c68b5f61c216d6
Reece H. Dunn <msclrhd@gmail.com>
2024-08-07 18:00:11 +0100

    Import CharacterClass from xqt-platform.

---
+++ src/lang-core/main/uk/co/reecedunn/intellij/plugin/core/lexer/CharacterClass.kt
@@ -0,0 +1,19 @@
+// Copyright (C) 2022 Reece H. Dunn. SPDX-License-Identifier: Apache-2.0
+package uk.co.reecedunn.intellij.plugin.core.lexer
+
+import xqt.platform.xml.model.XmlChar
+
+/**
+ * A lexical character class, i.e. a set of codepoints.
+ */
+interface CharacterClass {
+    /**
+     * Returns true if the XML character is in this character class.
+     */
+    operator fun contains(c: XmlChar): Boolean = contains(c.codepoint)
+
+    /**
+     * Returns true if the unicode codepoint is in this character class.
+     */
+    operator fun contains(c: Int): Boolean
+}
