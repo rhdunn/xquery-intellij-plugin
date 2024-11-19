1baf57a03e772734d32ff28013b9aa5fc52ff79c
Reece H. Dunn <msclrhd@gmail.com>
2024-11-19 15:03:07 +0000

    Don't use a static Version.IntelliJ property.

--- buildSrc/src/main/kotlin/Version.kt
+++ buildSrc/src/main/kotlin/Version.kt
@@ -5,31 +5,26 @@ import org.gradle.api.GradleException
 
 object Version {
     /**
-     * The version of the IntelliJ IDE the plugin supports.
-     *
-     * `SPDX-License-Identifier: Apache-2.0`
-     */
-    val IntelliJ: IntelliJVersion = IntelliJVersion(System.getenv("IDEA_VERSION") ?: "2024.2")
-
-    /**
      * The version of the Kotlin compiler and runtime.
      *
      * `SPDX-License-Identifier: Apache-2.0`
      *
      * @see <a href="https://github.com/JetBrains/kotlin">https://github.com/JetBrains/kotlin</a>
      */
-    val Kotlin: String = when {
-        IntelliJ.buildVersion >= 242 -> "1.9.22"
-        IntelliJ.buildVersion >= 223 -> "1.8.22"
-        else -> throw GradleException("Unsupported version of IntelliJ: $IntelliJ")
+    @Suppress("FunctionName")
+    fun Kotlin(intellij: IntelliJVersion): String = when {
+        intellij.buildVersion >= 242 -> "1.9.22"
+        intellij.buildVersion >= 223 -> "1.8.22"
+        else -> throw GradleException("Unsupported version of IntelliJ: $intellij")
     }
 
     /**
      * The version of Java.
      */
-    val Java: Int = when {
-        IntelliJ.buildVersion >= 242 -> 21
-        IntelliJ.buildVersion >= 223 -> 17
-        else -> throw GradleException("Unsupported version of IntelliJ: $IntelliJ")
+    @Suppress("FunctionName")
+    fun Java(intellij: IntelliJVersion): Int = when {
+        intellij.buildVersion >= 242 -> 21
+        intellij.buildVersion >= 223 -> 17
+        else -> throw GradleException("Unsupported version of IntelliJ: $intellij")
     }
 }
