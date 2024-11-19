06411fc84c1ccafce373108c2aa056f5f882b5c1
Reece H. Dunn <msclrhd@gmail.com>
2024-11-19 10:58:22 +0000

    Move the Kotlin version detection to the Version object.

--- buildSrc/src/main/kotlin/Version.kt
+++ buildSrc/src/main/kotlin/Version.kt
@@ -1,6 +1,7 @@
 // Copyright (C) 2024 Reece H. Dunn. SPDX-License-Identifier: Apache-2.0
 
 import io.github.rhdunn.intellij.IntelliJVersion
+import org.gradle.api.GradleException
 
 object Version {
     /**
@@ -9,4 +10,18 @@ object Version {
      * `SPDX-License-Identifier: Apache-2.0`
      */
     val IntelliJ: IntelliJVersion = IntelliJVersion(System.getenv("IDEA_VERSION") ?: "2024.2")
+
+    /**
+     * The version of the Kotlin compiler and runtime.
+     *
+     * `SPDX-License-Identifier: Apache-2.0`
+     *
+     * @see <a href="https://github.com/JetBrains/kotlin">https://github.com/JetBrains/kotlin</a>
+     */
+    val Kotlin: String = when {
+        IntelliJ.buildVersion >= 242 -> "1.9.22"
+        IntelliJ.buildVersion >= 223 -> "1.8.22"
+        IntelliJ.buildVersion >= 222 -> "1.7.22"
+        else -> throw GradleException("Unsupported version of IntelliJ: $IntelliJ")
+    }
 }
