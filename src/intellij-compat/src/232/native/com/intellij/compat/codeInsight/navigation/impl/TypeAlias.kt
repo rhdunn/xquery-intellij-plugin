bcbf9ca54b9260697954e3ab8af608ff8f2abae7
Reece H. Dunn <msclrhd@gmail.com>
2024-11-21 11:29:33 +0000

    Add a PsiTargetPresentationRenderer compatibility API.

---
+++ src/intellij-compat/src/232/native/com/intellij/compat/codeInsight/navigation/impl/TypeAlias.kt
@@ -0,0 +1,6 @@
+// Copyright (C) 2023 Reece H. Dunn. SPDX-License-Identifier: Apache-2.0
+package com.intellij.compat.codeInsight.navigation.impl
+
+import com.intellij.codeInsight.navigation.impl.PsiTargetPresentationRenderer
+
+typealias PsiTargetPresentationRenderer<T> = PsiTargetPresentationRenderer<T>
