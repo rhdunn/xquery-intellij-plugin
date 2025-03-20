44eb432e516cacad5d9f4925f619e4c797f860b0
Reece H. Dunn <msclrhd@proton.me>
2025-03-20 16:24:00 +0000

    IntelliJ 2025.1 requires Kotlin 2.1.x.

--- buildSrc/src/main/kotlin/Version.kt
+++ buildSrc/src/main/kotlin/Version.kt
@@ -13,6 +13,7 @@ object Version {
      */
     @Suppress("FunctionName")
     fun Kotlin(intellij: IntelliJVersion): String = when {
+        intellij.buildVersion >= 251 -> "2.1.10"
         intellij.buildVersion >= 242 -> "1.9.22"
         intellij.buildVersion >= 223 -> "1.8.22"
         else -> throw GradleException("Unsupported version of IntelliJ: $intellij")
