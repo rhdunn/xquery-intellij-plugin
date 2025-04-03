c4ade44116d28d2825171615bf7786c4c970625b
Reece H. Dunn <msclrhd@proton.me>
2025-04-03 15:32:24 +0100

    Use the IntelliJ testAnnotator helper to avoid deprecated method warnings.

--- src/intellij-test/main/uk/co/reecedunn/intellij/plugin/core/tests/parser/AnnotatorTestCase.kt
+++ src/intellij-test/main/uk/co/reecedunn/intellij/plugin/core/tests/parser/AnnotatorTestCase.kt
@@ -1,40 +1,21 @@
 // Copyright (C) 2016-2021, 2025 Reece H. Dunn. SPDX-License-Identifier: Apache-2.0
-// Copyright 2000-2019 JetBrains s.r.o. SPDX-License-Identifier: Apache-2.0
 package uk.co.reecedunn.intellij.plugin.core.tests.parser
 
-import com.intellij.codeInsight.daemon.impl.AnnotationHolderImpl
-import com.intellij.lang.ASTNode
 import com.intellij.lang.ParserDefinition
 import com.intellij.lang.annotation.*
 import com.intellij.lang.annotation.Annotation
 import com.intellij.openapi.editor.markup.TextAttributes
 import com.intellij.psi.PsiFile
-import com.intellij.psi.impl.source.tree.CompositeElement
-import com.intellij.psi.impl.source.tree.LeafPsiElement
+import com.intellij.testFramework.fixtures.CodeInsightTestUtil.testAnnotator
+import uk.co.reecedunn.intellij.plugin.core.sequences.walkTree
 
 @Suppress("SameParameterValue")
 abstract class AnnotatorTestCase<File : PsiFile>(
     fileExt: String?,
     vararg definitions: ParserDefinition
 ) : ParsingTestCase<File>(fileExt, *definitions) {
-    @Suppress("UnstableApiUsage")
-    private fun annotateTree(node: ASTNode, annotationHolder: AnnotationHolder, annotator: Annotator) {
-        if (node is CompositeElement) {
-            (annotationHolder as AnnotationHolderImpl).runAnnotatorWithContext(node.psi, annotator)
-        } else if (node is LeafPsiElement) {
-            (annotationHolder as AnnotationHolderImpl).runAnnotatorWithContext(node, annotator)
-        }
-
-        for (child in node.getChildren(null)) {
-            annotateTree(child, annotationHolder, annotator)
-        }
-    }
-
-    @Suppress("UnstableApiUsage", "DEPRECATION")
     protected fun annotateTree(file: File, annotator: Annotator): List<Annotation> {
-        val annotationHolder = AnnotationHolderImpl(AnnotationSession(file), false)
-        annotateTree(file.node, annotationHolder, annotator)
-        return annotationHolder
+        return testAnnotator(annotator, *file.walkTree().toList().toTypedArray())
     }
 }
 
