c9564f832cd6ef2330684d8bb0d72ac5354ab449
Reece H. Dunn <msclrhd@proton.me>
2025-04-03 16:16:47 +0100

    Add a LanguageStructureViewBuilder.getInstance() compatibility API for IntelliJ 2024.2.

--- src/kotlin-intellij/main/uk/co/reecedunn/intellij/plugin/core/ide/navigationToolbar/StructureAwareNavBarModelExtension.kt
+++ src/kotlin-intellij/main/uk/co/reecedunn/intellij/plugin/core/ide/navigationToolbar/StructureAwareNavBarModelExtension.kt
@@ -1,21 +1,8 @@
-/*
- * Copyright 2000-2020 JetBrains s.r.o.
- * Copyright (C) 2020 Reece H. Dunn
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright (C) 2020, 2025 Reece H. Dunn. SPDX-License-Identifier: Apache-2.0
+// Copyright 2000-2020 JetBrains s.r.o. SPDX-License-Identifier: Apache-2.0
 package uk.co.reecedunn.intellij.plugin.core.ide.navigationToolbar
 
+import com.intellij.compat.lang.LanguageStructureViewBuilder
 import com.intellij.ide.navigationToolbar.AbstractNavBarModelExtension
 import com.intellij.ide.structureView.StructureViewModel
 import com.intellij.ide.structureView.StructureViewTreeElement
@@ -24,7 +11,6 @@ import com.intellij.ide.structureView.impl.common.PsiTreeElementBase
 import com.intellij.ide.ui.UISettings
 import com.intellij.ide.util.treeView.smartTree.NodeProvider
 import com.intellij.ide.util.treeView.smartTree.TreeElement
-import com.intellij.lang.LanguageStructureViewBuilder
 import com.intellij.openapi.actionSystem.CommonDataKeys
 import com.intellij.openapi.actionSystem.DataContext
 import com.intellij.openapi.editor.Editor
@@ -112,7 +98,7 @@ abstract class StructureAwareNavBarModelExtension : AbstractNavBarModelExtension
             currentFileStructure?.get()?.let { return it }
         }
 
-        val builder = LanguageStructureViewBuilder.INSTANCE.getStructureViewBuilder(file)
+        val builder = LanguageStructureViewBuilder.getInstance().getStructureViewBuilder(file)
         val model = (builder as? TreeBasedStructureViewBuilder)?.createStructureViewModel(editor)
         if (model != null) {
             currentFile = SoftReference(file)
