a51360ed5b5b81a49bdbefa3c06a0a383fbfbf8c
Reece H. Dunn <msclrhd@proton.me>
2025-04-05 09:02:25 +0100

    Don't use the deprecated addBrowseFolderListener method in QueryProcessorDataSource.

---
+++ src/intellij-compat/src/243/native/com/intellij/compat/openapi/ui/Extensions.kt
@@ -0,0 +1,13 @@
+// Copyright (C) 2025 Reece H. Dunn. SPDX-License-Identifier: Apache-2.0
+package com.intellij.compat.openapi.ui
+
+import com.intellij.openapi.fileChooser.FileChooserDescriptor
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.ui.TextFieldWithBrowseButton
+
+fun TextFieldWithBrowseButton.addBrowseFolderListenerEx(
+    project: Project?,
+    fileChooserDescriptor: FileChooserDescriptor
+) {
+    addBrowseFolderListener(project, fileChooserDescriptor)
+}
--- src/plugin-api/main/uk/co/reecedunn/intellij/plugin/processor/query/execution/configurations/QueryProcessorRunConfigurationEditor.kt
+++ src/plugin-api/main/uk/co/reecedunn/intellij/plugin/processor/query/execution/configurations/QueryProcessorRunConfigurationEditor.kt
@@ -1,18 +1,4 @@
-/*
- * Copyright (C) 2018-2021 Reece H. Dunn
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright (C) 2018-2021, 2025 Reece H. Dunn. SPDX-License-Identifier: Apache-2.0
 package uk.co.reecedunn.intellij.plugin.processor.query.execution.configurations
 
 import com.intellij.lang.Language
@@ -96,7 +82,7 @@ class QueryProcessorRunConfigurationEditor(private val project: Project, private
         scriptFile = queryProcessorDataSource {
             val descriptor = FileNameMatcherDescriptor(languages.getAssociations())
             descriptor.title = PluginApiBundle.message("browser.choose.script-file")
-            addBrowseFolderListener(null, null, project, descriptor)
+            addBrowseFolderListenerEx(project, descriptor)
             addActionListener {
                 if (languages[0].getLanguageMimeTypes()[0] == "application/sparql-query") {
                     updateUI(true)
@@ -149,7 +135,7 @@ class QueryProcessorRunConfigurationEditor(private val project: Project, private
         contextItem = queryProcessorDataSource(allowUnspecified = true) {
             val descriptor = FileChooserDescriptorFactory.createSingleFileDescriptor()
             descriptor.title = PluginApiBundle.message("browser.choose.context-item")
-            addBrowseFolderListener(null, null, project, descriptor)
+            addBrowseFolderListenerEx(project, descriptor)
         }
         row {
             spacer(column.vertical())
