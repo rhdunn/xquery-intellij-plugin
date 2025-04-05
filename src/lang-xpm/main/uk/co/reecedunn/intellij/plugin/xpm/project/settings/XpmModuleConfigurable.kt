27a11e04e518265ab07dddc3fdd4800a4584b517
Reece H. Dunn <msclrhd@proton.me>
2025-04-05 09:11:11 +0100

    Don't use the deprecated addBrowseFolderListener method in XpmModuleConfigurable.

--- src/lang-xpm/main/uk/co/reecedunn/intellij/plugin/xpm/project/settings/XpmModuleConfigurable.kt
+++ src/lang-xpm/main/uk/co/reecedunn/intellij/plugin/xpm/project/settings/XpmModuleConfigurable.kt
@@ -1,20 +1,7 @@
-/*
- * Copyright (C) 2020 Reece H. Dunn
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright (C) 2020, 2025 Reece H. Dunn. SPDX-License-Identifier: Apache-2.0
 package uk.co.reecedunn.intellij.plugin.xpm.project.settings
 
+import com.intellij.compat.openapi.ui.addBrowseFolderListenerEx
 import com.intellij.openapi.fileChooser.FileChooserDescriptorFactory
 import com.intellij.openapi.options.Configurable
 import com.intellij.openapi.project.Project
@@ -41,7 +28,7 @@ class XpmModuleConfigurable(val project: Project) : Configurable {
             databasePath = textFieldWithBrowseButton(column.vgap().hgap().horizontal()) {
                 val descriptor = FileChooserDescriptorFactory.createSingleFolderDescriptor()
                 descriptor.title = XpmBundle.message("preferences.module.database-path.choose-path")
-                addBrowseFolderListener(null, null, project, descriptor)
+                addBrowseFolderListenerEx(project, descriptor)
             }
         }
         row {
