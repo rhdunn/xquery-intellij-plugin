fac93702db1dbb60f49c34142780800ecdb03f8a
Reece H. Dunn <msclrhd@proton.me>
2025-07-30 12:08:11 +0100

    Add copyright notices to the build scripts.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,5 @@
+// Copyright (C) 2016-2025 Reece H. Dunn. SPDX-License-Identifier: Apache-2.0
+
 allprojects {
     repositories {
         mavenLocal()
--- src/plugin-marklogic/build.gradle.kts
+++ src/plugin-marklogic/build.gradle.kts
@@ -1,3 +1,5 @@
+// Copyright (C) 2016-2025 Reece H. Dunn. SPDX-License-Identifier: Apache-2.0
+
 val ijVersion = BuildConfiguration.getPlatformVersion(project)
 
 sourceSets.main {
