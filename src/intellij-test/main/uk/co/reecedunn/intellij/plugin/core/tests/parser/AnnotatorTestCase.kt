f6d9fc7fc46820f7ba0fd247e8d6baa3cf1abc1d
Reece H. Dunn <msclrhd@proton.me>
2025-08-01 17:07:49 +0100

    Make annotateTree a free-standing function.

--- src/intellij-test/main/uk/co/reecedunn/intellij/plugin/core/tests/parser/AnnotatorTestCase.kt
+++ src/intellij-test/main/uk/co/reecedunn/intellij/plugin/core/tests/parser/AnnotatorTestCase.kt
@@ -1,7 +1,6 @@
 // Copyright (C) 2016-2021, 2025 Reece H. Dunn. SPDX-License-Identifier: Apache-2.0
 package uk.co.reecedunn.intellij.plugin.core.tests.parser
 
-import com.intellij.lang.ParserDefinition
 import com.intellij.lang.annotation.*
 import com.intellij.lang.annotation.Annotation
 import com.intellij.openapi.editor.markup.TextAttributes
@@ -9,14 +8,8 @@ import com.intellij.psi.PsiFile
 import com.intellij.testFramework.fixtures.CodeInsightTestUtil.testAnnotator
 import uk.co.reecedunn.intellij.plugin.core.sequences.walkTree
 
-@Suppress("SameParameterValue")
-abstract class AnnotatorTestCase<File : PsiFile>(
-    fileExt: String?,
-    vararg definitions: ParserDefinition
-) : ParsingTestCase<File>(fileExt, *definitions) {
-    protected fun annotateTree(file: File, annotator: Annotator): List<Annotation> {
-        return testAnnotator(annotator, *file.walkTree().toList().toTypedArray())
-    }
+fun <File : PsiFile> annotateTree(file: File, annotator: Annotator): List<Annotation> {
+    return testAnnotator(annotator, *file.walkTree().toList().toTypedArray())
 }
 
 fun List<Annotation>.prettyPrint(): String {
--- src/lang-xslt/test/uk/co/reecedunn/intellij/plugin/xslt/tests/lang/highlighter/AnnotatorTestCase.kt
+++ src/lang-xslt/test/uk/co/reecedunn/intellij/plugin/xslt/tests/lang/highlighter/AnnotatorTestCase.kt
@@ -1,18 +1,4 @@
-/*
- * Copyright (C) 2019-2021 Reece H. Dunn
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright (C) 2016-2021, 2025 Reece H. Dunn. SPDX-License-Identifier: Apache-2.0
 package uk.co.reecedunn.intellij.plugin.xslt.tests.lang.highlighter
 
 import uk.co.reecedunn.intellij.plugin.core.extensions.registerExtensionPointBean
@@ -30,13 +16,14 @@ import com.intellij.psi.xml.StartTagEndTokenProvider
 import com.intellij.xml.XmlExtension
 import uk.co.reecedunn.intellij.plugin.core.tests.injecton.MockInjectedLanguageManager
 import uk.co.reecedunn.intellij.plugin.core.tests.module.MockModuleManager
+import uk.co.reecedunn.intellij.plugin.core.tests.parser.ParsingTestCase
 import uk.co.reecedunn.intellij.plugin.core.tests.roots.MockProjectRootsManager
 import uk.co.reecedunn.intellij.plugin.xpm.psi.shadow.XpmShadowPsiElementFactory
 import uk.co.reecedunn.intellij.plugin.xslt.psi.impl.XsltShadowPsiElementFactory
 
 @Suppress("MemberVisibilityCanBePrivate", "SameParameterValue")
 abstract class AnnotatorTestCase(vararg definitions: ParserDefinition) :
-    uk.co.reecedunn.intellij.plugin.core.tests.parser.AnnotatorTestCase<PsiFile>(null, *definitions) {
+    ParsingTestCase<PsiFile>(null, *definitions) {
 
     override fun registerServicesAndExtensions() {
         super.registerServicesAndExtensions()
