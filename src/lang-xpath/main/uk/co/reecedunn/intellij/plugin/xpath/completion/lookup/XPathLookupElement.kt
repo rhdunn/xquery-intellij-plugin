90eee65a5dcc936454dd920d1eb6d59216a27692
Reece H. Dunn <msclrhd@proton.me>
2025-12-11 15:19:12 +0000

    Replace autoPopupMemberLookup with scheduleAutoPopup.

--- src/lang-xpath/main/uk/co/reecedunn/intellij/plugin/xpath/completion/lookup/XPathLookupElement.kt
+++ src/lang-xpath/main/uk/co/reecedunn/intellij/plugin/xpath/completion/lookup/XPathLookupElement.kt
@@ -1,18 +1,4 @@
-/*
- * Copyright (C) 2019 Reece H. Dunn
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright (C) 2019, 2025 Reece H. Dunn. SPDX-License-Identifier: Apache-2.0
 package uk.co.reecedunn.intellij.plugin.xpath.completion.lookup
 
 import com.intellij.codeInsight.AutoPopupController
@@ -49,7 +35,7 @@ open class XPathLookupElement(lookupString: String) : LookupElement() {
         if (insert === XPathInsertText.QNAME_PREFIX) {
             // Keep the auto-complete popup visible, so namespaced variables
             // and functions can be auto-completed.
-            AutoPopupController.getInstance(context.project).autoPopupMemberLookup(context.editor, null)
+            AutoPopupController.getInstance(context.project).scheduleAutoPopup(context.editor)
         }
     }
 
