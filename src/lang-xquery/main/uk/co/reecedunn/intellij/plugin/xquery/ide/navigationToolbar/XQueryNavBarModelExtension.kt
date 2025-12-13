0c9fcd27ccd2bd76d2bcdfb2501dadee6e7f5612
Reece H. Dunn <msclrhd@proton.me>
2025-12-13 09:05:29 +0000

    Use the StructureAwareNavBarModelExtension.isAcceptableLanguage API.

--- src/lang-xquery/main/uk/co/reecedunn/intellij/plugin/xquery/ide/navigationToolbar/XQueryNavBarModelExtension.kt
+++ src/lang-xquery/main/uk/co/reecedunn/intellij/plugin/xquery/ide/navigationToolbar/XQueryNavBarModelExtension.kt
@@ -1,32 +1,21 @@
-/*
- * Copyright (C) 2020 Reece H. Dunn
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright (C) 2020, 2025 Reece H. Dunn. SPDX-License-Identifier: Apache-2.0
 package uk.co.reecedunn.intellij.plugin.xquery.ide.navigationToolbar
 
+import com.intellij.ide.navigationToolbar.StructureAwareNavBarModelExtension
+import com.intellij.lang.Language
 import com.intellij.navigation.ItemPresentation
 import com.intellij.psi.PsiElement
-import uk.co.reecedunn.intellij.plugin.core.ide.navigationToolbar.StructureAwareNavBarModelExtension
 import uk.co.reecedunn.intellij.plugin.core.navigation.ItemPresentationEx
 import uk.co.reecedunn.intellij.plugin.xquery.ast.xquery.XQueryModule
 import uk.co.reecedunn.intellij.plugin.xquery.lang.XQuery
 
 class XQueryNavBarModelExtension : StructureAwareNavBarModelExtension() {
+    override val language: Language = XQuery
+
     override fun getPresentableText(`object`: Any?): String? = getPresentableText(`object`, false)
 
     override fun getPresentableText(`object`: Any?, forPopup: Boolean): String? {
-        val element = (`object` as? PsiElement)?.takeIf { acceptElement(it) } ?: return null
+        val element = (`object` as? PsiElement)?.takeIf { isAcceptableLanguage(it) } ?: return null
         return when (element) {
             is ItemPresentationEx -> {
                 if (forPopup)
@@ -41,5 +30,5 @@ class XQueryNavBarModelExtension : StructureAwareNavBarModelExtension() {
 
     override fun getParent(psiElement: PsiElement?): PsiElement? = (psiElement?.containingFile as? XQueryModule)
 
-    override fun acceptElement(psiElement: PsiElement): Boolean = XQuery.isKindOf(psiElement.language)
+    override fun isAcceptableLanguage(psiElement: PsiElement?): Boolean = language.isKindOf(psiElement?.language)
 }
